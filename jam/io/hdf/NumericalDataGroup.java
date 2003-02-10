0a45dc455486a856253509a37d590bb07cc6f6a3
Dale Visser <dwvisser@users.sourceforge.net>
2003-02-10 14:29:55 +0000

    Changed Vector references to List references.

--- jam/io/hdf/NumericalDataGroup.java
+++ jam/io/hdf/NumericalDataGroup.java
@@ -12,94 +12,96 @@ import java.io.*;
  */
 public class NumericalDataGroup extends DataObject {
 
-  /**
-   * List of data elements this NDG ties together.
-   */
-  Vector elements;
+	/**
+	 * List of data elements this NDG ties together.
+	 */
+	List elements;
 
-  public NumericalDataGroup(HDFile fi){
-    super(fi, DFTAG_NDG);//sets tag
-    elements=new Vector();
-    try{
-      refreshBytes();
-    } catch (HDFException e) {
-      System.err.println(e);
-    }
-  }
+	public NumericalDataGroup(HDFile fi) {
+		super(fi, DFTAG_NDG); //sets tag
+		elements = new Vector();
+		try {
+			refreshBytes();
+		} catch (HDFException e) {
+			System.err.println(e);
+		}
+	}
 
-  public NumericalDataGroup(HDFile hdf, byte [] data, short reference) {
-    super(hdf,data,reference);
-    tag=DFTAG_NDG;
-  }
+	public NumericalDataGroup(HDFile hdf, byte[] data, short reference) {
+		super(hdf, data, reference);
+		tag = DFTAG_NDG;
+	}
 
-  /**
-   * Should be called whenever a change is made to the contents of the NDG.
-   *
-   * @exception HDFException unrecoverable error
-   */
-  protected void refreshBytes() throws HDFException{
-    int numBytes;
-    ByteArrayOutputStream baos;
-    DataOutputStream dos;
-    Enumeration temp;
-    DataObject ob;
-    try{
-      numBytes=4*elements.size();  //see DFTAG_NDG specification for HDF 4.1r2
-      baos=new ByteArrayOutputStream(numBytes);
-      dos=new DataOutputStream(baos);
-      for (temp = elements.elements() ; temp.hasMoreElements() ;) {
-        ob=(DataObject)(temp.nextElement());
-        dos.writeShort(ob.getTag());
-        dos.writeShort(ob.getRef());
-      }
-      bytes=baos.toByteArray();
-    } catch (IOException e) {
-      throw new HDFException("Problem processing NDG: "+e.getMessage());
-    }
-  }
+	/**
+	 * Should be called whenever a change is made to the contents of the NDG.
+	 *
+	 * @exception HDFException unrecoverable error
+	 */
+	protected void refreshBytes() throws HDFException {
+		int numBytes;
+		ByteArrayOutputStream baos;
+		DataOutputStream dos;
 
-  /**
-   * Implementation of <code>DataObject</code> abstract method.
-   *
-   * @exception HDFException thrown if there is a problem interpreting the bytes
-   */
-  public void interpretBytes() throws HDFException {
-    int numItems,i;
-    short t,r;//item tag/ref
+		try {
+			numBytes = 4 * elements.size();
+			/* see DFTAG_NDG specification for HDF 4.1r2 */
+			baos = new ByteArrayOutputStream(numBytes);
+			dos = new DataOutputStream(baos);
+			for (Iterator temp = elements.iterator(); temp.hasNext();) {
+				DataObject ob = (DataObject) (temp.next());
+				dos.writeShort(ob.getTag());
+				dos.writeShort(ob.getRef());
+			}
+			bytes = baos.toByteArray();
+		} catch (IOException e) {
+			throw new HDFException("Problem processing NDG: " + e.getMessage());
+		}
+	}
 
-    ByteArrayInputStream bais=new ByteArrayInputStream(bytes);
-    DataInputStream dis=new DataInputStream(bais);
-    numItems=bytes.length/4;//2 for each tag, 2 for each ref
-    elements=new Vector(numItems);
-    try{
-      for(i=0;i<numItems;i++){
-        t=dis.readShort();
-        r=dis.readShort();
-        addDataObject(file.getObject(t,r));//look up tag/ref in file and add object
-      }
-    } catch (IOException e) {
-      throw new HDFException ("Problem interpreting NDG: "+e.getMessage());
-    }
-  }
+	/**
+	 * Implementation of <code>DataObject</code> abstract method.
+	 *
+	 * @exception HDFException thrown if there is a problem interpreting the bytes
+	 */
+	public void interpretBytes() throws HDFException {
+		int numItems, i;
+		short t, r; //item tag/ref
 
-  /**
-   * Adds the data element to the NDG.
-   *
-   * @param  data  data element to be added
-   */
-  public void addDataObject(DataObject data){
-    elements.addElement(data);
-    try{
-      refreshBytes();
-    } catch (HDFException e) {
-      System.err.println(e);
-    }
-  }
+		ByteArrayInputStream bais = new ByteArrayInputStream(bytes);
+		DataInputStream dis = new DataInputStream(bais);
+		numItems = bytes.length / 4; //2 for each tag, 2 for each ref
+		elements = new Vector(numItems);
+		try {
+			for (i = 0; i < numItems; i++) {
+				t = dis.readShort();
+				r = dis.readShort();
+				addDataObject(file.getObject(t, r));
+				//look up tag/ref in file and add object
+			}
+		} catch (IOException e) {
+			throw new HDFException(
+				"Problem interpreting NDG: " + e.getMessage());
+		}
+	}
 
-  /**
-   * Passes the internal vector back.
-   */
-  public Vector getObjects(){
-    return elements;
-  }
+	/**
+	 * Adds the data element to the NDG.
+	 *
+	 * @param  data  data element to be added
+	 */
+	public void addDataObject(DataObject data) {
+		elements.add(data);
+		try {
+			refreshBytes();
+		} catch (HDFException e) {
+			System.err.println(e);
+		}
+	}
+
+	/**
+	 * Passes the internal vector back.
+	 */
+	public List getObjects() {
+		return elements;
+	}
 }
