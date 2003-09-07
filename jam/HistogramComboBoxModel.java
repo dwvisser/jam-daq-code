da36c5d14cdecdf893ebd2f8be109f99097f101b
Dale Visser <dwvisser@users.sourceforge.net>
2003-09-07 13:39:29 +0000

    setSelectedItem() does fireContentsChanged()...this seems to fix all the problems I've had with the Histogram chooser since switching to Swing.

--- jam/HistogramComboBoxModel.java
+++ jam/HistogramComboBoxModel.java
@@ -9,61 +9,71 @@ import java.util.List;
  * 
  * @author Dale Visser
  */
-public class HistogramComboBoxModel  extends DefaultComboBoxModel {
+public class HistogramComboBoxModel extends DefaultComboBoxModel {
 
-    private String selection=null;
-    private String NO_HISTS="No Histograms";
-    private JamCommand jc;
+	private String selection = null;
+	private String NO_HISTS = "No Histograms";
+	private JamCommand jc;
 
-    public HistogramComboBoxModel(JamCommand jc){
-        super();
-        this.jc=jc;
-    }
+	public HistogramComboBoxModel(JamCommand jc) {
+		super();
+		this.jc = jc;
+	}
 
-    /**
-     * Needs to be called after every action that changes the list of histograms.
-     * Utility method added so that JamMain could notify when things have changed.
-     */
-    void changeOccured(){
-        fireContentsChanged(this,0,getSize());
-    }
+	/**
+	 * Needs to be called after every action that changes the list of histograms.
+	 * Utility method added so that JamMain could notify when things have changed.
+	 */
+	void changeOccured() {
+		fireContentsChanged(this, 0, getSize());
+	}
 
-    /**
-     * Returns the individual list elements in the histogram chooser.
-     */
-    public Object getElementAt(int index){
-    	Object rval=NO_HISTS; //default value if no histograms
-    	List list=Histogram.getHistogramList(); 
-    	if (index <0 || index >= list.size()) {
-    		System.err.println("WARNING: "+getClass().getName()+
-				".getElementAt("+index+"): index out of range.");  	
-    	} else {
-    		if (numHists()>0){
-				rval = ((Histogram) Histogram.getHistogramList().get(index)).getName();
-			} 
-    	}
-        return rval;
-    }
+	/**
+	 * Returns the individual list elements in the histogram chooser.
+	 */
+	public Object getElementAt(int index) {
+		Object rval = NO_HISTS; //default value if no histograms
+		List list = Histogram.getHistogramList();
+		int size = list.size();
+		if (size > 0) {
+			if (index < 0 || index >= size) {
+				System.err.println(
+					"WARNING: "
+						+ getClass().getName()
+						+ ".getElementAt("
+						+ index
+						+ "): index out of range.");
+			} else {
+				if (numHists() > 0) {
+					rval =
+						((Histogram) Histogram.getHistogramList().get(index))
+							.getName();
+				}
+			}
+		}
+		return rval;
+	}
 
-    /**
-     * Returns the number of list elements in the chooser.
-     */
-    public int getSize(){
-		return Math.max(1,numHists());
-    }
+	/**
+	 * Returns the number of list elements in the chooser.
+	 */
+	public int getSize() {
+		return Math.max(1, numHists());
+	}
 
-    //for ComboBoxModel interface
-    public void setSelectedItem(Object anItem) {
-        selection = (String) anItem;
-    }
+	//for ComboBoxModel interface
+	public void setSelectedItem(Object anItem) {
+		selection = (String) anItem;
+		fireContentsChanged(this, -1, -1);
+	}
 
-    //for ComboBoxModel interface
-    public Object getSelectedItem() {
-        return selection;
-    }
+	//for ComboBoxModel interface
+	public Object getSelectedItem() {
+		return selection;
+	}
 
-    private int numHists(){
-        return  Histogram.getHistogramList().size();
-    }
+	private int numHists() {
+		return Histogram.getHistogramList().size();
+	}
 
 }
