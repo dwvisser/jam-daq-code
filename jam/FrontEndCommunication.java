2c3220ab2b0aa55ce728f826f9909e1a6df8bacc
Dale Visser <dwvisser@users.sourceforge.net>
2003-12-14 16:04:28 +0000

    Removed public modifiers from methods, since they are implicitly public in an interface anyway.

--- jam/FrontEndCommunication.java
+++ jam/FrontEndCommunication.java
@@ -23,19 +23,22 @@
      * Called when Online data taking is setup.
      * @throws JamException when something goes wrong
      */
-    public void setup()  throws JamException;
+    void setup()  throws JamException;
     
     /**
      * Reciever of distributed events
      * can listen for broadcasted event
      * Implementation of Observable interface
+     * 
+     * @param observable source of observed event
+     * @param o communicated message
      */
-    public void update(Observable observable, Object o);
+    void update(Observable observable, Object o);
 
     /**
      * Tell the Front End to start acquistion.
      */
-    public void start();
+    void start();
 
     
     /**
@@ -43,7 +46,7 @@
      * should also flushes out existing data in
      * the front end.
      */
-    public void stop();
+    void stop();
     
     /**
      * Tell the Front End to end a run.
@@ -51,75 +54,65 @@
      * in the front end and put a end of 
      * run marker at its end of the last buffer.
      */
-    public void end();
+    void end();
     
     /**
      * Tell the Front End to fluss out the 
      * data buffer
      */
-    public void flush();
+    void flush();
     
     /**
      * Tell the VME to read the scalers
      * send back to packets the packet with the scaler values
      * and a packet if read OK or ERROR and message
      */
-    public void readScalers();
+    void readScalers();
     
     /**
      * Tell the Front to clear the scalers
      * sends a reply if OK or ERROR
      */
-    public void clearScalers();
+    void clearScalers();
     
     /**
      * Tell the Front to read the counters
      * send back to packets the packet with the counter values
      * and a packet if read OK or ERROR and message
      */
-    public void readCounters();
+    void readCounters();
     
     /**
      * Tell the Font to zero the counters
      * Front end should a reply with a OK or ERROR
      */
-    public void zeroCounters();
+    void zeroCounters();
     
     /**
      * Tells the Front End to reply with verbose status messages, 
      * which can be output for the user to see.
      * @param state TRUE to set the front end in verbose mode     
      */
-     public void verbose(boolean state);
+    void verbose(boolean state);
     
-    /**
-     * Sets whether to receive front end replies and output
-     * the messages to the console.
-     */
-     //public void setDisplayMessages(boolean state);
-     
-     /**
-      * Gets whether front end replies are output to the console.
-      */
-     //public boolean getDisplayMessages();
-     
     /**
      * Tells the Front End that we want it to put it to
      * debug mode.
      * @param state TRUE to set the front end in debug mode
      */
-     public void debug(boolean state);
+     void debug(boolean state);
     
     /**
      * New version uploads CAMAC CNAF commands with udp pakets, and 
      * sets up the camac crate.
      *
      * @param camacCommands object containing CAMAC CNAF commands
+     * @throws JamException if there is a problem setting up
      */
-    public void setupCamac(CamacCommands camacCommands) throws JamException;
+    void setupCamac(CamacCommands camacCommands) throws JamException;
 
-	public void setupVME_Map(VME_Map vmeMap) throws JamException;
-	public void sendScalerInterval(int milliseconds) throws JamException;
+	void setupVME_Map(VME_Map vmeMap) throws JamException;
+	void sendScalerInterval(int milliseconds) throws JamException;
     
     /** 
      * Method that is a deamon for receiving packets from the 
@@ -129,5 +122,5 @@
      * it determines how the packet is to be handled
      *
      */
-    public void run();
+    void run();
 }      
