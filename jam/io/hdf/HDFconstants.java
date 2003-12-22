f7bdfae2bc05165754dbf4467a65d28cb80cd98c
Dale Visser <dwvisser@users.sourceforge.net>
2003-12-22 03:39:16 +0000

    Added HDF_HEADER_NUMBYTES so header reading and skipping would be more understandable.

--- jam/io/hdf/HDFconstants.java
+++ jam/io/hdf/HDFconstants.java
@@ -4,12 +4,13 @@ package jam.io.hdf;
  * Contains constants for the <code>jam.io.hdf</code> package.
  * 
  * @author <a href=mailto:dale@visser.name>Dale Visser</a>
+ * @version 1.0
  */
 public interface HDFconstants{
 
     /**
      * Byte pattern for bytes 0-3 of any standard v4.1r2 HDF file.
      */
-    public static int HDF_HEADER = 0x0e031301;
-    
+    int HDF_HEADER = 0x0e031301; 
+    int HDF_HEADER_NUMBYTES=4;
 }
