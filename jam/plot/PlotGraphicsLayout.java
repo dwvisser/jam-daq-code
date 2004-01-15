b35434698113fe597ce11ef6c7be8eab2e534c6d
Dale Visser <dwvisser@users.sourceforge.net>
2004-01-15 19:27:18 +0000

    Removed static modifiers, since they were already implied. Added font sizes for title. Changed left label offset to something that works better for the new rendering.

--- jam/plot/PlotGraphicsLayout.java
+++ jam/plot/PlotGraphicsLayout.java
@@ -8,51 +8,53 @@ package jam.plot;
 interface PlotGraphicsLayout {
 
     //border outside of plot
-    static final int BORDER_TOP=40;
-    static final int BORDER_LEFT=60;
-    static final int BORDER_BOTTOM=40;
-    static final int BORDER_RIGHT=60;
+    final int BORDER_TOP=40;
+	final int BORDER_LEFT=60;
+	final int BORDER_BOTTOM=40;
+	final int BORDER_RIGHT=60;
 
     //tickmark stuff
-    static final int TICK_SIZE=10;
-    static final int TICK_MINOR=5;
-    static final int TICK_MAJOR=10;
+	final int TICK_SIZE=10;
+	final int TICK_MINOR=5;
+	final int TICK_MAJOR=10;
     
     //title stuff
-    static final int TITLE_OFFSET=10;
-    static final int TITLE_OFFSET_TOP=10;
-    static final int TITLE_OFFSET_BOTTOM=25;
-    static final int TITLE_OFFSET_DATE=25;
+	final int TITLE_OFFSET=10;
+	final int TITLE_OFFSET_TOP=10;
+	final int TITLE_OFFSET_BOTTOM=25;
+	final int TITLE_OFFSET_DATE=25;
     
     //tickmarks
-    static final int LABEL_OFFSET=5;
-    static final int LABEL_OFFSET_TOP=5;
-    static final int LABEL_OFFSET_BOTTOM=3;
-    static final int LABEL_OFFSET_LEFT=3;
+	final int LABEL_OFFSET=5;
+	final int LABEL_OFFSET_TOP=5;
+	final int LABEL_OFFSET_BOTTOM=3;
+	final int LABEL_OFFSET_LEFT=3;
     
     //axis labels
-    static final int AXIS_LABEL_OFFSET_TOP=20;    
-    static final int AXIS_LABEL_OFFSET_BOTTOM=20;
-    static final int AXIS_LABEL_OFFSET_LEFT=50;    
-    static final int AXIS_LABEL_OFFSET_RIGHT=20;        
+	final int AXIS_LABEL_OFFSET_TOP=20;    
+	final int AXIS_LABEL_OFFSET_BOTTOM=20;
+	final int AXIS_LABEL_OFFSET_LEFT=35;    
+	final int AXIS_LABEL_OFFSET_RIGHT=20;        
     
     //stuff for channel marker
-    static final int MARK_MIN_LENGTH=20;    
-    static final int MARK_OFFSET=3;
+	final int MARK_MIN_LENGTH=20;    
+	final int MARK_OFFSET=3;
     //fonts
-    static final int SCREEN_FONT_SIZE=12;
+	final float SCREEN_FONT_SIZE=12;
+	final float TITLE_SCREEN_SIZE=SCREEN_FONT_SIZE+2;
 
     //stuff for two d color scale 
-    static final int COLOR_SCALE_OFFSET=10;	    // distance from right side of plot
-    static final int COLOR_SCALE_LABEL_OFFSET=5;
-    static final int COLOR_SCALE_SIZE=15;	    // size of a color swatch
+	final int COLOR_SCALE_OFFSET=10;	    // distance from right side of plot
+	final int COLOR_SCALE_LABEL_OFFSET=5;
+	final int COLOR_SCALE_SIZE=15;	    // size of a color swatch
 
     // stuff for printing, margins, font
-    static final double MARGIN_TOP=0.5;
-    static final double MARGIN_BOTTOM=0.5;
-    static final double MARGIN_LEFT=0.5;
-    static final double MARGIN_RIGHT=0.5;
+	final double MARGIN_TOP=0.5;
+	final double MARGIN_BOTTOM=0.5;
+	final double MARGIN_LEFT=0.5;
+	final double MARGIN_RIGHT=0.5;
 
-    static final int PRINT_FONT_SIZE=12;
+	final int PRINT_FONT_SIZE=12;
+	final int TILE_PRINT_SIZE=PRINT_FONT_SIZE+2;
     
 }
