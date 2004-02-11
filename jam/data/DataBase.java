a239b4482410f263ee5c64270d88c5746bb5ec94
Dale Visser <dwvisser@users.sourceforge.net>
2004-02-11 12:50:20 +0000

    Now implements the Singleton pattern.

--- jam/data/DataBase.java
+++ jam/data/DataBase.java
@@ -1,25 +1,42 @@
- package jam.data;
+package jam.data;
 
 /**
- * Class that contains a <code>static</code>method to clear the lists of all the data classes.
+ * Class that contains a <code>static</code>method to clear the lists
+ * of all the data classes.
  */
 public class DataBase {
+	
+	private static final DataBase dataBase=new DataBase();
+	
+	private DataBase(){
+		super();
+	}
+	
+	/**
+	 * Get the singleton instance of this class.
+	 * 
+	 * @return the only instance of this class
+	 */
+	static public DataBase getInstance(){
+		return dataBase;
+	}
 
-    /**
-     * Calls the <code>clearList</code> methods of the various data classes.
-     *
-     * @see Histogram#clearList()
-     * @see Gate#clearList()
-     * @see Scaler#clearList()
-     * @see Monitor#clearList()
-     * @see DataParameter#clearList()
-     */
-    public static void  clearAllLists(){
-      Histogram.clearList();
-      Gate.clearList();
-      Scaler.clearList();
-      Monitor.clearList();
-      DataParameter.clearList();
-      System.gc();
-    }
+	/**
+	 * Calls the <code>clearList</code> methods of the various data 
+	 * classes.
+	 *
+	 * @see Histogram#clearList()
+	 * @see Gate#clearList()
+	 * @see Scaler#clearList()
+	 * @see Monitor#clearList()
+	 * @see DataParameter#clearList()
+	 */
+	public void clearAllLists() {
+		Histogram.clearList();
+		Gate.clearList();
+		Scaler.clearList();
+		Monitor.clearList();
+		DataParameter.clearList();
+		System.gc();
+	}
 }
