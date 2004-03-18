6803e115e53168501caffc9662f202c07a9f6b67
Dale Visser <dwvisser@users.sourceforge.net>
2004-03-18 21:36:11 +0000

    1.4 M20040318

--- jam/Version.java
+++ jam/Version.java
@@ -24,7 +24,7 @@ public final class Version {
 	 * Should be empty string if no text for version type is
 	 * desired.
 	 */
-	static final String VERSION_TYPE = "Release Candidate 8";
+	static final String VERSION_TYPE = "M20040318";
 	
 	/**
 	 * @return a string representing the build version of Jam running
