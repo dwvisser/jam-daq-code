2e892b389428e031b0e094200c3971a49f2bb482
Ken Swartz <swartzkb@users.sourceforge.net>
2004-03-31 06:43:44 +0000

    Added check that resources are found, if not found put up dialog and exit.

--- jam/GateListCellRenderer.java
+++ jam/GateListCellRenderer.java
@@ -3,16 +3,20 @@ package jam;
 import jam.data.Gate;
 
 import java.awt.Component;
+import java.awt.Frame;
+import java.net.*;
 
 import javax.swing.ImageIcon;
 import javax.swing.JList;
 import javax.swing.DefaultListCellRenderer;
 import javax.swing.UIManager;
+import javax.swing.*;
+
 
 /**
  * Renders representations for a JComboBox list entry of
  * a <code>jam.data.Gate</code> object.
- * 
+ *
  * @author <a href="mailto:dale@visser.name">Dale Visser</a>
  * @version 17 Dec 2003
  */
@@ -20,24 +24,33 @@ public class GateListCellRenderer
 	extends DefaultListCellRenderer {
 
 	private static final ClassLoader cl = ClassLoader.getSystemClassLoader();
-	private static final ImageIcon stopIcon =
-		new ImageIcon(cl.getResource("jam/stop.png"));
-	private static final ImageIcon goIcon =
-		new ImageIcon(cl.getResource("jam/go.png"));
-	private static final ImageIcon clearIcon = 
-	new ImageIcon(cl.getResource("jam/clear.png"));
-	
+
+	private static ImageIcon stopIcon;
+	private static ImageIcon goIcon;
+	private static ImageIcon clearIcon;
+
+	static{
+		URL urlStop=cl.getResource("jam/stop.png");
+		URL urlGo =cl.getResource("jam/go.png");
+		URL urlClear =cl.getResource("jam/clear.png");
+		if (urlStop==null || urlGo==null || urlClear==null) {
+			JOptionPane.showMessageDialog(null, "Can't load resource: jam/(stop|go|clear).png");
+			System.exit(0);
+		}
+	}
+
 	/**
 	 * Creates a new <code>HistogramListCellRenderer</code>.
 	 */
 	public GateListCellRenderer() {
 		super();
+
 	}
 
 	/**
 	 * Returns a <code>JLabel</code> for the gate, with name and number, and a red
 	 * or green icon indicating if the gate is set.
-	 * 
+	 *
 	 * @see javax.swing.ListCellRenderer#getListCellRendererComponent(javax.swing.JList, java.lang.Object, int, boolean, boolean)
 	 */
 	public Component getListCellRendererComponent(
