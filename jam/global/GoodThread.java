fc3baa547bb7858337cd0efe2d833272375a3536
Dale Visser <dwvisser@users.sourceforge.net>
2004-04-14 16:49:54 +0000

    * created an inner class that is an enumeration of the only possible   states for a GoodThread...this way we avoid   IllegalArgumentException's * created a private lock object to synchronize on...not   necessary, but I like to see what's being locked on explicitly

--- jam/global/GoodThread.java
+++ jam/global/GoodThread.java
@@ -2,27 +2,52 @@ package jam.global;
 import javax.swing.JOptionPane;
 
 /**
- * Extends the thread class to allow proper stopping and suspending of threads,
- * by allowing the object itself to decide when it's appropriate to stop after a
- * stop request has been sent to it.  Based on code given in "Java Design" by
- * Peter Coad and Mark Mayfield.
+ * Extends the thread class to allow proper stopping and suspending
+ * of threads, by allowing the object itself to decide when it's 
+ * appropriate to stop after a stop request has been sent to it. 
+ * Based on code given in "Java Design" by Peter Coad and Mark 
+ * Mayfield.
  *
  * @author <a href="dale@visser.name">Dale Visser</a>
  */
 public class GoodThread extends Thread {
+	
+	public static class State{
+		private final int value;
+		private static final String [] modes = {"RUN","SUSPEND","STOP"};
+		
+		private State (int val){
+			value=val;
+		}
+		
+		public String toString(){
+			return modes[value];
+		}
+		
+		public static final State RUN=new State(0);
+		public static final State SUSPEND=new State(1);
+		public static final State STOP=new State(2);
+	}
 
-    public static final int RUN = 0;
-    public static final int SUSPEND = 1;
-    public static final int STOP = 2;
+    public static final State RUN = State.RUN;
+    public static final State SUSPEND = State.SUSPEND;
+    public static final State STOP = State.STOP;
 
-private static final String [] modes = {"RUN","SUSPEND","STOP","SPECIAL"};
+    private State state = State.RUN;
+    private final Object stateLock=new Object();
 
-    private int state = RUN;
-
+	/**
+	 * Simply calls the superclass's constructor of the same signature.
+	 * We start with the state equal to <code>RUN</code>.
+	 */
     public GoodThread(){
         super();
     }
 
+	/**
+	 * Simply calls the superclass's constructor of the same signature.
+	 * We start with the state equal to <code>RUN</code>.
+	 */
     public GoodThread(Runnable r){
         super(r);
     }
@@ -31,47 +56,49 @@ private static final String [] modes = {"RUN","SUSPEND","STOP","SPECIAL"};
      * To stop, suspend, or restart a GoodThread, call this.
      *
      * @param s <code>STOP, SUSPEND,</code> or <code>RUN</code>
-     * @exception IllegalArgumentException thrown for bad argument
      */
-    public synchronized void setState(int s) {
-        if (s != RUN && s!=SUSPEND && s!=STOP){
-            throw new IllegalArgumentException("GoodThread.setState("+s+"), invalid thread state");
+    public void setState(State s) {
+        synchronized (stateLock){
+			state = s;
+			if (state != State.SUSPEND) {
+				stateLock.notifyAll();//wake up thread
+			} 
         }
-        state = s;
-        if (state != SUSPEND) {
-        	notify();//wake up thread
-        } 
     }
 
     /**
-     * Should be called frequently by the thread's <code>run()</code> method.
-     * Code in the run() method will decide what to do for a <code>STOP</code>
-     * state.  If the state is <code>SUSPEND</code>, <code>checkState()</code> takes 
-     * care of suspending the thread, and it takes a call to <code>setState(int)</code>
-     * with either <code>STOP</code> or <code>RUN</code> to get the thread out of
+     * Should be called frequently by the thread's <code>run()</code>
+     * method. Code in the run() method will decide what to do for a 
+     * <code>STOP</code> state.  If the state is 
+     * <code>SUSPEND</code>, <code>checkState()</code> takes care of
+     * suspending the thread, and it takes a call to 
+     * <code>setState(int)</code> with either <code>STOP</code> or 
+     * <code>RUN</code> to get the thread out of
      * <code>checkState()</code>.
      *
      * @return <code>true</code> if OK to resume, <code>false</code> if state is <code>STOP</code>
      */
-    protected synchronized boolean checkState(){
-        while (state == SUSPEND) {
-            try {
-                wait();
-            } catch (InterruptedException ie) {
-            	JOptionPane.showMessageDialog(null,
-				ie.getMessage(),
-				getClass().getName()+" interrupted while suspended",
-				JOptionPane.ERROR_MESSAGE);
-            }
-        }
-        return (state == RUN);
+    protected boolean checkState(){
+    	synchronized (stateLock){
+			while (state == State.SUSPEND) {
+				try {
+					stateLock.wait();
+				} catch (InterruptedException ie) {
+					JOptionPane.showMessageDialog(null,
+					ie.getMessage(),
+					getClass().getName()+" interrupted while suspended",
+					JOptionPane.ERROR_MESSAGE);
+				}
+			}
+			return (state == State.RUN);
+    	}
     }
 
     public String toString() {
-    	String rval = "Invalid state: "+state;//default
-        if (state >=0 && state < modes.length) {
-        	rval = modes[state];
-        }
-        return rval;
+    	StringBuffer rval=new StringBuffer(super.toString());
+    	rval.append(": state=");
+    	synchronized (stateLock){
+			return rval.append(state).toString();
+    	}
     }
 }
