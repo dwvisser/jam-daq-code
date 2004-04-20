3aa387d608de3bd4a15ec1c0c974bd41784f38d1
Dale Visser <dwvisser@users.sourceforge.net>
2004-04-20 21:13:35 +0000

    Simple JUnit test of RingBuffer class, far from exhaustive.

---
+++ jam/sort/RingBufferTest.java
@@ -0,0 +1,61 @@
+package jam.sort;
+
+import junit.framework.TestCase;
+
+/**
+ * JUnit tests for <code>jam.data.Histogram</data>.
+ * 
+ * @author <a href="mailto:dale@visser.name">Dale Visser</a>
+ * @see Histogram
+ * @see AllTests
+ */
+public class RingBufferTest extends TestCase {
+
+	private RingBuffer ring=new RingBuffer();
+	final byte [] buffer=new byte[RingBuffer.BUFFER_SIZE];
+
+	/**
+	 * Constructor for HistogramTest.
+	 * 
+	 * @param arg0
+	 */
+	public RingBufferTest(String arg0) {
+		super(arg0);
+	}
+
+	/**
+	 * Initialize local variables for the tests.
+	 * 
+	 * @see TestCase#setUp()
+	 */
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	/**
+	 * Test for <code>putBuffer()</code>.
+	 *
+	 * @see RingBuffer#putBuffer()
+	 */
+	public void testPut() {
+		for (int i=0; i<RingBuffer.NUMBER_BUFFERS/2; i++){
+			try{
+				ring.putBuffer(buffer);
+			} catch (RingFullException re){
+				System.err.println(re);
+			}
+			ring.getBuffer();
+		}
+		assertTrue(ring.empty());
+		final int most=RingBuffer.NUMBER_BUFFERS*3/4;
+		for (int i=0; i<most; i++){
+			try{
+				ring.putBuffer(buffer);
+			} catch (RingFullException re){
+				System.err.println(re);
+			}
+		}
+	}
+	
+
+}
