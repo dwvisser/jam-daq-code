a507fcfff132e15401da52b3f4701c525d20df21
Ken Swartz <swartzkb@users.sourceforge.net>
2004-05-02 19:55:08 +0000

    Show dialog for new histogram

---
+++ jam/commands/ShowDialogNewHistogramCmd.java
@@ -0,0 +1,36 @@
+package jam.commands;
+import jam.data.control.*;
+import java.awt.Frame;
+//import jam.global.Broadcaster;
+//import jam.global.MessageHandler;
+//import jam.global.JamStatus;
+/**
+ * Show the histogram dialog
+ * 
+ * @author Ken
+ *
+ */
+public class ShowDialogNewHistogramCmd extends AbstractCommand{
+
+	//Null constructor needed
+	public ShowDialogNewHistogramCmd(){
+		
+	}
+	
+	/**
+	 * Execute the command
+	 */
+	public void execute(Object [] cmdParams){
+		
+		HistogramControl histogramControl= new HistogramControl((Frame)status.getFrame(), broadcaster, msghdlr);
+		histogramControl.showNew();		
+	}
+	
+	/**
+	 * Execute the command
+	 */
+	public void executeStrParam(String [] cmdParams){
+		execute(null);
+	}
+	
+}
