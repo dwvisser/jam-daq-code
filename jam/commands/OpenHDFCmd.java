2db7dc93648e189adc4c1ee3b70e4ef636320dca
Ken Swartz <swartzkb@users.sourceforge.net>
2004-05-09 06:28:55 +0000

    Allows the input of a file name

--- jam/commands/OpenHDFCmd.java
+++ jam/commands/OpenHDFCmd.java
@@ -1,11 +1,14 @@
 package jam.commands;
 
 import javax.swing.JFrame;
+import java.io.File;
 
 import jam.io.hdf.HDFIO;
 import jam.data.control.DataControl;
 import jam.io.FileOpenMode;
 import jam.global.BroadcastEvent;
+import jam.global.JamStatus;
+
 
 /**
  * Open a hdf file
@@ -20,22 +23,30 @@ public class OpenHDFCmd extends AbstractCommand implements Commandable {
 	 */
 	public void execute(Object[] cmdParams) {
 		
+		JFrame frame = status.getFrame();						
+		final HDFIO	hdfio = new HDFIO(frame, msghdlr);
+		
+		boolean isFileRead =false; 
+		
 		//No file given		
 		if ( cmdParams==null) {		
-
-			JFrame frame = status.getFrame();
-							
-			final HDFIO	hdfio = new HDFIO(frame, msghdlr);
 						
 			if (hdfio.readFile(FileOpenMode.OPEN)) { //true if successful
-				status.setSortModeFile(hdfio.getFileNameOpen());
-				DataControl.setupAll();
-				broadcaster.broadcast(BroadcastEvent.HISTOGRAM_ADD);
-				frame.repaint();
-				//KBS still needed 
-				//setSaveEnabled(true);
+				isFileRead=true;
 			}
-		}		
+			
+		//File given	
+		} else {
+			if (hdfio.readFile(FileOpenMode.OPEN, (File)cmdParams[0])) {
+				isFileRead=true;
+			}
+		}
+
+		//File was read in
+		if (isFileRead)		
+			notifyApp(hdfio.getFileNameOpen());
+		
+		
 	}
 
 	/* (non-Javadoc)
@@ -43,9 +54,25 @@ public class OpenHDFCmd extends AbstractCommand implements Commandable {
 	 */
 	public void executeParse(String[] cmdTokens) {		
 		
+		Object [] cmdParams = new Object[1]; 
 		if (cmdTokens.length==0) {
 			execute(null);
+		} else {
+			File file = new File(cmdTokens[0]); 
+			cmdParams[0]=file;
+			execute(cmdParams);
 		}
 	}
+
+	private void notifyApp(String fileName) {
+		 
+		JFrame frame = status.getFrame();				
+				
+		status.setSortMode(JamStatus.FILE, fileName);
+		DataControl.setupAll();
+		broadcaster.broadcast(BroadcastEvent.HISTOGRAM_ADD);
+		frame.repaint();
+		
+	}							
 
 }
