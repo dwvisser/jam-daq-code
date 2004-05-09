89ca998ebbe2726efc0b9e3a92ec35eb139421d9
Dale Visser <dwvisser@users.sourceforge.net>
2004-05-09 14:39:24 +0000

    Fixed up javadoc comments.

--- jam/global/SortMode.java
+++ jam/global/SortMode.java
@@ -12,34 +12,11 @@ package jam.global;
  * @version May 9, 2004
  */
 public class SortMode {
-	/**
-	 * Sort Mode--No sort file loaded.
-	 */
 	static private final int noSort = 0;
-
-	/**
-	 * Sort Mode--Set to sort online data to disk.
-	 */
 	static private final int onlineDisk = 1;
-
-	/**
-	 * Sort Mode--Set to sort online data to tape.
-	 */
 	static private final int onlineNoDisk = 2;
-
-	/**
-	 * Sort Mode--Set to sort offline data from disk.
-	 */
 	static private final int offline = 3;
-
-	/**
-	 * Sort Mode--Acting as a client to a remote Jam process.
-	 */
 	static private final int remote = 5;
-
-	/**
-	 * Sort Mode--Just read in a data file.
-	 */
 	static private final int file = 6; //we have read in a file
 	
 	private final int mode;
@@ -48,11 +25,33 @@ public class SortMode {
 		mode=i;
 	}
 
+	/**
+	 * Not sorting, and no file loaded.
+	 */
 	static public final SortMode NO_SORT=new SortMode(noSort);
+	
+	/**
+	 * Sorting online data and storing events to disk.
+	 */
 	static public final SortMode ONLINE_DISK=new SortMode(onlineDisk);
+	
+	/**
+	 * Sort online data without storing events.
+	 */
 	static public final SortMode ONLINE_NO_DISK=new SortMode(onlineNoDisk);
-	static public final SortMode OFFLINE=new SortMode(offline);
-	static public final SortMode REMOTE=new SortMode(remote);
-	static public final SortMode FILE=new SortMode(file);
 
+	/**
+	 * Sorting data from disk, that is, sorting offline.
+	 */
+	static public final SortMode OFFLINE=new SortMode(offline);
+	
+	/**
+	 * Acting as a client to a remote Jam process.
+	 */
+	static public final SortMode REMOTE=new SortMode(remote);
+	
+	/**
+	 * Looking at data that was read in from a file.
+	 */
+	static public final SortMode FILE=new SortMode(file);
 }
