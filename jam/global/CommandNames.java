7947c9b7127083c86bae27c32533b50f47c2fc97
Ken Swartz <swartzkb@users.sourceforge.net>
2004-05-11 04:53:50 +0000

    Has the command names used internally and avaliable from the console

---
+++ jam/global/CommandNames.java
@@ -0,0 +1,18 @@
+package jam.global;
+
+/**
+ * The command names, used internally and also avaliable from
+ * the console
+ *   
+ * @author Ken Swartz
+ *
+ */
+public interface CommandNames {
+	
+	/** Open a hdf file */
+	public static final String OPENHDF = "open";
+	
+	/** clear all data */
+	public static final String NEWCLEAR ="clear";
+
+}
