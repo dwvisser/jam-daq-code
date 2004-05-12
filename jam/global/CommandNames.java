b79eb41b529ee9e2dad1ebc948654656a3de89db
Ken Swartz <swartzkb@users.sourceforge.net>
2004-05-12 05:08:46 +0000

    Added more names

--- jam/global/CommandNames.java
+++ jam/global/CommandNames.java
@@ -8,9 +8,18 @@ package jam.global;
  *
  */
 public interface CommandNames {
+
+	/** clear all data */
+	public static final String NEWCLEAR ="clear";
 	
 	/** Open a hdf file */
 	public static final String OPEN_HDF = "open";
+	
+	/** Reload hdf file */
+	public static final String RELOAD_HDF = "reload";
+
+	/** Add hdf file */
+	public static final String ADD_HDF = "addfile";
 
 	/** Save to hdf file */
 	public static final String SAVE_HDF = "save";
@@ -18,7 +27,7 @@ public interface CommandNames {
 	/** Save as to a hdf */ 
 	public static final String SAVE_AS_HDF = "saveas";	
 	
-	/** clear all data */
-	public static final String NEWCLEAR ="clear";
+	/** Open selected histograms in hdf */ 
+	public static final String OPEN_SELECTED = "opensel";	
 
 }
