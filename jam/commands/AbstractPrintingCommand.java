4d279d27c608c6215f09bde346001bdc4afccb1c
Dale Visser <dwvisser@users.sourceforge.net>
2004-06-03 19:04:56 +0000

    Moved mPageFormat initialization code from MainMenuBar to here.

--- jam/commands/AbstractPrintingCommand.java
+++ jam/commands/AbstractPrintingCommand.java
@@ -1,7 +1,10 @@
 package jam.commands;
 
-import java.awt.print.PrinterJob;
+import jam.plot.PlotGraphicsLayout;
+
 import java.awt.print.PageFormat;
+import java.awt.print.Paper;
+import java.awt.print.PrinterJob;
 
 /**
  * Command for Page Setup 
@@ -10,6 +13,22 @@ import java.awt.print.PageFormat;
  */
 abstract class AbstractPrintingCommand extends AbstractCommand {
 
-	protected static PageFormat mPageFormat = PrinterJob.getPrinterJob().defaultPage();
+	protected static PageFormat mPageFormat = 
+	PrinterJob.getPrinterJob().defaultPage();
 	
+	static {
+		final double inchesToPica=72.0;
+		final double top=PlotGraphicsLayout.MARGIN_TOP*inchesToPica;
+		final double bottom=mPageFormat.getHeight()-
+		PlotGraphicsLayout.MARGIN_BOTTOM*inchesToPica;
+		final double height=bottom-top;
+		final double left=PlotGraphicsLayout.MARGIN_LEFT*inchesToPica;
+		final double right=mPageFormat.getWidth()-
+		PlotGraphicsLayout.MARGIN_RIGHT*inchesToPica;
+		final double width=right-left;
+		final Paper paper=mPageFormat.getPaper();
+		paper.setImageableArea(top,left,width,height);
+		mPageFormat.setPaper(paper);
+		mPageFormat.setOrientation(PageFormat.LANDSCAPE);
+	}
 }
