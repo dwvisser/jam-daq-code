05dfc3d5506172e644c21ed0935f0cbfc72768ad
Dale Visser <dwvisser@users.sourceforge.net>
2004-06-03 21:31:05 +0000

    Deleted commented-out code.

--- jam/commands/SaveAsHDFCmd.java
+++ jam/commands/SaveAsHDFCmd.java
@@ -18,32 +18,20 @@ final class SaveAsHDFCmd extends AbstractCommand implements Commandable {
 	}
 
 	/**
-	 * Save to a hdf, prompt for overwrite
+	 * Save to a hdf, prompt for overwrite.
+	 * 
 	 * @see jam.commands.AbstractCommand#execute(java.lang.Object[])
 	 */
 	protected void execute(Object[] cmdParams) throws CommandException {
 		final HDFIO hdfio = new HDFIO(status.getFrame(), msghdlr);		
 		if ( cmdParams==null) {//No file given		
-			//int write = hdfio.writeFile();
 			hdfio.writeFile();
-			//if (write==JFileChooser.APPROVE_OPTION){
-				//KBS FIXME setSaveEnabled(true);
-				//??Is this the right instead of setSaveEnabled 
-				//status.setSortMode(file);
-			//}
-			//throw new CommandException("Error executing command save");				
 		} else {//File name given	
-			//KBS check for valid file if not done in hdfio
 			hdfio.writeFile((File)cmdParams[0]);			
 		}			
 	}
 
-	/* 
-	 * @see jam.commands.AbstractCommand#executeParse(java.lang.String[])
-	 */
 	protected void executeParse(String[] cmdTokens) throws CommandListenerException {
-		
-		
 		try {
 			Object [] cmdParams = new Object[1]; 
 			if (cmdTokens.length==0) {
