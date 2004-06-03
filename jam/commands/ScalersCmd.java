fd21538b6bff454e96c3824254b32a206cd6bb36
Dale Visser <dwvisser@users.sourceforge.net>
2004-06-03 19:09:14 +0000

    Edited "whitespace" mostly.

--- jam/commands/ScalersCmd.java
+++ jam/commands/ScalersCmd.java
@@ -1,24 +1,19 @@
 package jam.commands;
 
 import jam.global.BroadcastEvent;
+
 /**
  *  Command for scalers
  * 
  * @author Ken Swartz
- *
- * To change the template for this generated type comment go to
- * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
  */
-final class ScalersCmd extends AbstractCommand implements Commandable {
+final class ScalersCmd extends AbstractCommand {
 
-	protected static int READ =1;
-	protected static int ZERO =2;
-	/* 
-	 * @see jam.commands.AbstractCommand#execute(java.lang.Object[])
-	 */
+	protected static final int READ =1;
+	protected static final int ZERO =2;
+
 	protected void execute(Object[] cmdParams) {
-		int param =((Integer)cmdParams[0]).intValue();
-		
+		final int param =((Integer)cmdParams[0]).intValue();
 		if (param==READ) {
 			readScalers();
 		}else if (param==ZERO) {
@@ -27,29 +22,20 @@ final class ScalersCmd extends AbstractCommand implements Commandable {
 			msghdlr.errorOutln(
 			"Incomplete command: need 'scaler zero' or 'scaler read'.");	
 		}
-		
 	}
 
-	/* (non-Javadoc)
-	 * @see jam.commands.AbstractCommand#executeParse(java.lang.String[])
-	 */
 	protected void executeParse(String[] cmdTokens) {
-		
-		Object [] params = new Object[1];
-		
+		final Object [] params = new Object[1];		
 		if (cmdTokens[0].equals("read")) {			
-			params[0]= new Integer(READ);
-			
+			params[0]= new Integer(READ);			
 		}else if (cmdTokens[0].equals("zero")) {
 			params[0]= new Integer(ZERO);
-		}
-		 	
+		}		 	
 		execute(params);	
-
 	}
+	
 	/**
-	 * Does the scaler zeroing
-	 *
+	 * Does the scaler zeroing.
 	 */
 	private void zeroScalers() {
 		broadcaster.broadcast(BroadcastEvent.SCALERS_CLEAR);
@@ -57,13 +43,11 @@ final class ScalersCmd extends AbstractCommand implements Commandable {
 	}
 	
 	/**
-	 * Does the scaler reading
-	 *
+	 * Does the scaler reading.
 	 */
 	private void readScalers() {
 		if (status.isOnLine()){
 			broadcaster.broadcast(BroadcastEvent.SCALERS_READ);
 		} 		
 	}
-
 }
