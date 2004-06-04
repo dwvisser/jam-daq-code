15991dc8902f45bca4cc6eccfa2e9df7d4c41c09
Dale Visser <dwvisser@users.sourceforge.net>
2004-06-04 21:44:28 +0000

    * copied idea of AbstractShowDataControlCmd into AbstractShowDialog * created ShowBatchExport for showing batch export dialog

---
+++ jam/commands/AbstractShowDialog.java
@@ -0,0 +1,27 @@
+package jam.commands;
+
+import jam.global.CommandListenerException;
+
+import javax.swing.JDialog;
+
+
+/**
+ * Commands that are for showing DataControl dialogs simply extend this
+ * and assign a reference to <code>dataControl</control> in their constructor.
+ * 
+ * @author Ken Swartz
+ */
+public class AbstractShowDialog
+	extends AbstractCommand {
+
+	protected JDialog dialog;
+	
+	protected void execute(Object[] cmdParams) {
+		dialog.show();
+	}
+
+	protected void executeParse(String[] cmdTokens)
+		throws CommandListenerException {
+		execute(null);
+	}
+}
---
+++ jam/commands/ShowBatchExport.java
@@ -0,0 +1,20 @@
+/*
+ * Created on Jun 4, 2004
+ *
+ * To change the template for this generated file go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+package jam.commands;
+
+/**
+ * 
+ * @author <a href="mailto:dale@visser.name">Dale Visser</a>
+ * @version Jun 4, 2004
+ */
+public class ShowBatchExport extends AbstractShowDialog {
+	
+	public ShowBatchExport(){
+		putValue(NAME, "Batch Export\u2026");
+		dialog=new jam.io.BatchExport(msghdlr);
+	}
+}
