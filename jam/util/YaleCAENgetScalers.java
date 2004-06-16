e2d121a4ebdf6112eab061d03ff7a657b3b6afa7
Ken Swartz <swartzkb@users.sourceforge.net>
2004-06-16 06:35:47 +0000

    Usable from a command now

--- jam/util/YaleCAENgetScalers.java
+++ jam/util/YaleCAENgetScalers.java
@@ -25,10 +25,8 @@ package jam.util;
 import jam.global.JamProperties;
 import jam.global.JamStatus;
 import jam.global.MessageHandler;
-import jam.io.ExtensionFileFilter;
 
 import java.awt.Frame;
-import java.awt.event.ActionEvent;
 import java.io.BufferedInputStream;
 import java.io.DataInputStream;
 import java.io.EOFException;
@@ -36,14 +34,13 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 
-import javax.swing.AbstractAction;
-import javax.swing.Action;
-import javax.swing.JFileChooser;
-
 public class YaleCAENgetScalers {
 
 	private final Frame frame;
 	private final MessageHandler console;
+	private String strScalerText;
+	private String fileName;
+	private String strError;
 
 	public YaleCAENgetScalers() {
 		JamStatus js=JamStatus.instance();
@@ -51,6 +48,11 @@ public class YaleCAENgetScalers {
 		console = js.getMessageHandler();
 	}
 
+	public void display() {
+
+		new TextDisplayDialog(frame, fileName, false, strScalerText);
+	
+	}
 	/**
 	 * Takes an event file, searches for scaler blocks in it and 
 	 * returns tab-delimited text listing each scaler block on one
@@ -59,10 +61,15 @@ public class YaleCAENgetScalers {
 	 * @param events the file to search
 	 * @return text showing the scaler values in the event file
 	 */
-	public String processEventFile(File events) throws IOException {
-		final StringBuffer rval = new StringBuffer();
+	public boolean processEventFile(File events)  {
+		
+		boolean rtnState=true;
+		final StringBuffer strBuff = new StringBuffer();
 		final int SCALER_HEADER = 0x01cccccc;
 		DataInputStream dis = null;
+		
+		strError="";
+		
 		try {
 			dis =
 				new DataInputStream(
@@ -75,91 +82,36 @@ public class YaleCAENgetScalers {
 					blockNum++;
 					int numScalers = dis.readInt();
 					for (int i = 1; i <= numScalers; i++) {
-						rval.append(dis.readInt());
+						strBuff.append(dis.readInt());
 						if (i < numScalers) {
-							rval.append('\t');
+							strBuff.append('\t');
 						}
 					}
-					rval.append('\n');
+					strBuff.append('\n');
 				}
 			}
-		} catch (EOFException e) {
+		//End of file reached	
+		} catch (EOFException eofe) {
 			if (dis != null) {
-				dis.close();
+				//Bury close exception
+				try {dis.close();}catch(Exception e){};
 			}
-			return rval.toString();
-		}
-	}
-
-	public class GetScalersAction extends AbstractAction {
-		
-		public GetScalersAction(){
-			super("Display scaler values from a YaleCAEN event file...");
+			strError="";
+			rtnState = true;
+		} catch (IOException ioe) {
+			strError=",reading file: "+ioe.getMessage();
+			rtnState = false;						
 		}
 		
-		public void actionPerformed(ActionEvent ae) {
-			final File file = getFile();
-			if (file != null) {
-				String name = file.getName();
-				boolean display = true;
-				String text = null;
-				try {
-					text = processEventFile(file);
-				} catch (IOException e) {
-					console.errorOutln(e.getMessage());
-					display = false;
-				}
-				if (display) {
-					new TextDisplayDialog(frame,name,false,text);
-					/*final JDialog jd = new JDialog(frame, name, false);
-					final Container contents=jd.getContentPane();
-					contents.setLayout(new BorderLayout());
-					final JScrollPane jsp = new JScrollPane(text);
-					text.setToolTipText("Use select, cut and paste to export the text.");
-					contents.add(jsp, BorderLayout.CENTER);
-					jd.pack();
-					final Dimension screenSize = 
-					Toolkit.getDefaultToolkit().getScreenSize();
-					final int del=25;
-					final int x=frame.getX()+del;
-					final int y=frame.getY()+del;
-					final Dimension initSize=jd.getSize();
-					final int sizex=Math.min(initSize.width,
-					screenSize.width-del-x);	
-					final int sizey=Math.min(initSize.height,
-					screenSize.height-del-y);	
-					jd.setLocation(x,y);
-					jd.setSize(sizex,sizey);
-					jd.show();*/
-				}
-			}
-		}
+		fileName=events.getName();		
+		strScalerText=strBuff.toString();		
+		return rtnState;		
 	}
 	
-	public Action getAction(){
-		return new GetScalersAction();
+	public String getErrorTxt() {
+		return strError;
 	}
-
 	private File lastFile =
 		new File(JamProperties.getPropString(JamProperties.EVENT_INPATH));
 
-	/**
-	 * Get a *.evn file from a JFileChooser.
-	 *
-	 * @return	a <code>File</code> chosen by the user, null if dialog cancelled
-	 */
-	protected File getFile() {
-		File file = null;
-		int option;
-		JFileChooser jfile = new JFileChooser(lastFile);
-		jfile.setDialogTitle("Select an Event File");
-		jfile.setFileFilter(new ExtensionFileFilter("evn"));
-		option = jfile.showOpenDialog(null);
-		/* don't do anything if it was cancel */
-		if (option == JFileChooser.APPROVE_OPTION) {
-			file = jfile.getSelectedFile();
-			lastFile = file;
-		}
-		return file;
-	}
 }
