214abd4dbeee3fdbdbdc74fd6e1c132cf35db573
Dale Visser <dwvisser@users.sourceforge.net>
2004-06-23 19:49:50 +0000

    Now takes a file argument if given.

--- jam/commands/SaveGatesCmd.java
+++ jam/commands/SaveGatesCmd.java
@@ -1,7 +1,9 @@
 package jam.commands;
 
-import jam.io.hdf.HDFIO;
 import jam.global.CommandListenerException;
+import jam.io.hdf.HDFIO;
+
+import java.io.File;
 
 /** 
  * Save gates and scalers 
@@ -9,32 +11,49 @@ import jam.global.CommandListenerException;
  * @author Ken Swartz
  *
  */
-public class SaveGatesCmd extends AbstractCommand implements Commandable {
+final class SaveGatesCmd extends AbstractCommand implements Commandable {
+
+	public void initCommand() {
+		putValue(NAME, "Save gates, scalers & parameters as HDF\u2026");
+	}
 
 	/**
-	 * Constructor
-	 *
-	 */
-	SaveGatesCmd(){
-		putValue(NAME,"Save gates, scalers & parameters as HDF\u2026");
-	}
-
-	/* (non-Javadoc)
+	 * Save to an hdf file.
+	 * 
+	 * @param cmdParams empty array or <code>null</code> to use a 
+	 * file dialog, or an array with a <code>File</code> as the first
+	 * element
 	 * @see jam.commands.AbstractCommand#execute(java.lang.Object[])
+	 * @see java.io.File
 	 */
-	protected void execute(Object[] cmdParams){
+	protected void execute(Object[] cmdParams) {
 		final HDFIO hdfio = new HDFIO(status.getFrame(), msghdlr);
-		hdfio.writeFile(false,true,true,true);
-
+		if (cmdParams == null || cmdParams.length==0) { //No file given		
+			hdfio.writeFile(false, true, true, true);
+		} else { //File name given	
+			hdfio.writeFile(false, true, true, true, (File) cmdParams[0]);
+		}
 	}
 
-	/* (non-Javadoc)
+	/**
+	 * Save to an hdf file.
+	 * 
+	 * @param cmdParams empty array or <code>null</code> to use a 
+	 * file dialog, or the name of a <code>File</code> as the first
+	 * element
 	 * @see jam.commands.AbstractCommand#executeParse(java.lang.String[])
+	 * @see java.io.File
 	 */
 	protected void executeParse(String[] cmdTokens)
 		throws CommandListenerException {
+		if (cmdTokens == null || cmdTokens.length == 0) {
 			execute(null);
-
+		} else {
+			final Object[] cmdParams = new Object[1];
+			final File file = new File((String) cmdTokens[0]);
+			cmdParams[0] = file;
+			execute(cmdParams);
+		}
 	}
 
 }
