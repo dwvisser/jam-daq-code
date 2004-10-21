66e9c7ac2ef8361d46bf871ab43904f64824136c
Dale Visser <dwvisser@users.sourceforge.net>
2004-10-21 18:10:40 +0000

    Changed buttons to use anonymous ActionListener's.

--- jam/PeakFindDialog.java
+++ jam/PeakFindDialog.java
@@ -24,7 +24,7 @@ import javax.swing.border.EmptyBorder;
  * 
  * @author <a href="mailto:dale@visser.name">Dale W Visser</a>
  */
-public class PeakFindDialog extends JDialog implements ActionListener {
+public class PeakFindDialog extends JDialog {
 
 	private final Display display;
 	private final MessageHandler console;
@@ -75,35 +75,32 @@ public class PeakFindDialog extends JDialog implements ActionListener {
 		pbutton.add(pb);
 		pb.setLayout(new GridLayout(1, 3,5,5));
 
-		JButton bok = new JButton("OK");
+		final JButton bok = new JButton("OK");
 		pb.add(bok);
-		bok.setActionCommand("ok");
-		bok.addActionListener(this);
-		
-		JButton bapply = new JButton("Apply");
+		bok.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				setPeakFindProperties();
+				setVisible(false);
+			}
+		});
+		final JButton bapply = new JButton("Apply");
 		pb.add(bapply);
-		bapply.setActionCommand("apply");
-		bapply.addActionListener(this);
-		
-		JButton bcancel = new JButton(" Cancel ");
+		bapply.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				setPeakFindProperties();
+			}
+		});
+		final JButton bcancel = new JButton(" Cancel ");
 		pb.add(bcancel);
-		bcancel.setActionCommand("cancel");
-		bcancel.addActionListener(this);
-		
+		bcancel.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				setVisible(false);
+			}
+		});		
 		setResizable(false);
 		pack();
 	}
 
-	public synchronized void actionPerformed(ActionEvent e) {
-		String text=e.getActionCommand();
-		if (text.equals("ok") || text.equals("apply")){
-			setPeakFindProperties();
-		    if (text.equals("ok")) this.hide();
-		} else if (text.equals("cancel")){
-			this.hide();
-		}
-	}
-	
 	private void setPeakFindProperties(){
 		double w = Double.parseDouble(width.getText().trim());
 		double s = Double.parseDouble(sensitivity.getText().trim());
