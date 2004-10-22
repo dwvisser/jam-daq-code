6aa8a5fdded761818671aaf9fd01f8a423089e0d
Dale Visser <dwvisser@users.sourceforge.net>
2004-10-22 21:11:14 +0000

    formatting

--- jam/sort/RingFullException.java
+++ jam/sort/RingFullException.java
@@ -1,17 +1,16 @@
-/*
- */
 package jam.sort;
+
 /**
  * Exception that is thrown if <code>RingBuffer</code> is full.
- *
+ * 
  * @author Ken Swartz
  * @version 0.9
  * @see RingBuffer
  * @since JDK1.1
  */
-class RingFullException extends Exception{
+class RingFullException extends Exception {
 
-    public RingFullException(String msg){
-	super(msg);
-    }
+	public RingFullException(String msg) {
+		super(msg);
+	}
 }
--- jam/sort/RingInputStream.java
+++ jam/sort/RingInputStream.java
@@ -1,21 +1,17 @@
-/*
- */
 package jam.sort;
 
 /**
- * This class takes a buffer and creates from it a InputStream.
- * The buffers are not copied but referenced so you must make
- * sure not to remove the buffer while this class has a reference
- * to it.
- *
- * This class is not re-entrant (multi-thread ready)
- * so that it can be fast, synchronize locks take time.
- *
- * Adapted from java.io.ByteInputstream difference are
- * that you don't construct a new class each time and methods
- * are not synchronized.
- *
- * @author	Ken Swartz
+ * This class takes a buffer and creates from it a InputStream. The buffers are
+ * not copied but referenced so you must make sure not to remove the buffer
+ * while this class has a reference to it.
+ * 
+ * This class is not re-entrant (multi-thread ready) so that it can be fast,
+ * synchronize locks take time.
+ * 
+ * Adapted from <code>java.io.ByteInputstream</code>. A difference is that
+ * you don't construct a new class each time and methods are not synchronized.
+ * 
+ * @author Ken Swartz
  */
 public final class RingInputStream extends java.io.InputStream {
 
@@ -37,7 +33,8 @@ public final class RingInputStream extends java.io.InputStream {
 	/**
 	 * Load a buffer to read.
 	 * 
-	 * @param bufferIn	the input buffer (not copied)     
+	 * @param bufferIn
+	 *            the input buffer (not copied)
 	 */
 	public void setBuffer(byte[] bufferIn) {
 		buf = bufferIn;
@@ -47,9 +44,13 @@ public final class RingInputStream extends java.io.InputStream {
 
 	/**
 	 * Load a buffer to read but read only specified bytes
-	 * @param buf	The input buffer (not copied)
-	 * @param offset    The offset of the first byte to read
-	 * @param length	The number of bytes to read     
+	 * 
+	 * @param buf
+	 *            The input buffer (not copied)
+	 * @param offset
+	 *            The offset of the first byte to read
+	 * @param length
+	 *            The number of bytes to read
 	 */
 	public void setBuffer(byte buf[], int offset, int length) {
 		this.buf = buf;
@@ -59,8 +60,8 @@ public final class RingInputStream extends java.io.InputStream {
 
 	/**
 	 * Reads a byte of data.
-	 * @return 	the byte read, or -1 if the end of the
-	 *		stream is reached.
+	 * 
+	 * @return the byte read, or -1 if the end of the stream is reached.
 	 */
 	public int read() {
 		return (pos < count) ? (buf[pos++] & 0xff) : -1;
@@ -68,11 +69,15 @@ public final class RingInputStream extends java.io.InputStream {
 
 	/**
 	 * Reads into an array of bytes.
-	 * @param b	the buffer into which the data is read
-	 * @param off the start offset of the data
-	 * @param len the maximum number of bytes read
-	 * @return  the actual number of bytes read; -1 is
-	 * 		returned when the end of the stream is reached.
+	 * 
+	 * @param b
+	 *            the buffer into which the data is read
+	 * @param off
+	 *            the start offset of the data
+	 * @param len
+	 *            the maximum number of bytes read
+	 * @return the actual number of bytes read; -1 is returned when the end of
+	 *         the stream is reached.
 	 */
 	public int read(byte b[], final int off, final int len) {
 		int rval = 0; //by default, read nothing
@@ -81,8 +86,9 @@ public final class RingInputStream extends java.io.InputStream {
 			rval = -1;
 		} else {
 			if (pos + len > count) {
-				/* check we can read all the bytes asked 
-				 * otherwise read all we can 
+				/*
+				 * check we can read all the bytes asked otherwise read all we
+				 * can
 				 */
 				rval = count - pos;
 			}
@@ -97,8 +103,9 @@ public final class RingInputStream extends java.io.InputStream {
 	/**
 	 * Skips n bytes of input.
 	 * 
-	 * @param n the number of bytes to be skipped
-	 * @return	the actual number of bytes skipped.
+	 * @param n
+	 *            the number of bytes to be skipped
+	 * @return the actual number of bytes skipped.
 	 */
 	public long skip(long n) {
 		long rval = n;
