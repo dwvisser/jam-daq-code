0b82c6dd6fa33f900924136597e461aec323f2c4
Dale Visser <dwvisser@users.sourceforge.net>
2004-10-22 21:09:41 +0000

    Added more complete unit tests for RingBuffer behavior.

--- jam/sort/RingBufferTest.java
+++ jam/sort/RingBufferTest.java
@@ -1,5 +1,7 @@
 package jam.sort;
 
+import java.util.Arrays;
+
 import junit.framework.TestCase;
 
 /**
@@ -10,8 +12,9 @@ import junit.framework.TestCase;
  */
 public class RingBufferTest extends TestCase {
 
-	private RingBuffer ring=new RingBuffer();
-	final byte [] buffer=new byte[RingBuffer.BUFFER_SIZE];
+	private final RingBuffer ring=new RingBuffer();
+	private final byte [] buffer=new byte[RingBuffer.BUFFER_SIZE];
+	private final byte [] out=new byte[RingBuffer.BUFFER_SIZE];
 
 	/**
 	 * Constructor for HistogramTest.
@@ -38,23 +41,47 @@ public class RingBufferTest extends TestCase {
 	 */
 	public void testPut() {
 		for (int i=0; i<RingBuffer.NUMBER_BUFFERS/2; i++){
+			Arrays.fill(buffer,(byte)i);
 			try{
 				ring.putBuffer(buffer);
 			} catch (RingFullException re){
 				System.err.println(re);
 			}
-			ring.getBuffer();
+			ring.getBuffer(out);
 		}
 		assertTrue(ring.isEmpty());
-		final int most=RingBuffer.NUMBER_BUFFERS*3/4;
-		for (int i=0; i<most; i++){
+		assertFalse(ring.isFull());
+		/* Next expression true, since we did get right after each put. */
+		assertTrue(Arrays.equals(buffer,out));
+		ring.clear();
+		for (int i=0; i<RingBuffer.NUMBER_BUFFERS; i++){
+			Arrays.fill(buffer,(byte)(i+1));
 			try{
 				ring.putBuffer(buffer);
 			} catch (RingFullException re){
 				System.err.println(re);
 			}
 		}
+		assertFalse(ring.isEmpty());
+		assertTrue(ring.isFull());
+		ring.getBuffer(out);
+		/* Next expression is false because buffer if FIFO. */
+		assertFalse(Arrays.equals(buffer,out));
+		assertFalse(ring.isFull());
+		ring.clear();
+		assertTrue(ring.isEmpty());
 	}
 	
-
+	public void testGetAvailableBuffers(){
+		System.out.println("Number of buffers: "+RingBuffer.NUMBER_BUFFERS);
+		assertEquals(RingBuffer.NUMBER_BUFFERS,ring.getAvailableBuffers());
+		for (int i=0; i<RingBuffer.NUMBER_BUFFERS; i++){
+			try{
+				ring.putBuffer(buffer);
+			} catch (RingFullException re){
+				System.err.println(re);
+			}
+		}
+		assertEquals(0,ring.getAvailableBuffers());
+	}	
 }
