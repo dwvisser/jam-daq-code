36588b2970f660c07211c9faebe0de128c02885a
Dale Visser <dwvisser@users.sourceforge.net>
2004-10-29 15:28:26 +0000

    * receiveLoop() calls setEmptyBefore(true) instead of warning to console * setEmptyBefore(true) when emptyBefore=false (initial state) pops up warning which   encourages user to end run, but allows them to continue with no more warnings if   they want * beginRun() in RunControl now calls setEmptyBefore(false) to reset emptyBefore at   the beginning of each run

--- jam/sort/NetDaemon.java
+++ jam/sort/NetDaemon.java
@@ -1,5 +1,7 @@
 package jam.sort;
+
 import jam.global.GoodThread;
+import jam.global.JamStatus;
 import jam.global.MessageHandler;
 
 import java.io.IOException;
@@ -10,216 +12,253 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 import javax.swing.Action;
+import javax.swing.JButton;
 import javax.swing.JOptionPane;
 
 /**
- * <code>NetDeamon</code> receives packets from the network
- * and sends the data data into one or two pipes.
- * One pipe to a <code>SortDeamon</code>, and the other is
- * a <code>TapeDaemon</code> or <code>DiskDaemon</code> if one is activated.
- *
+ * <code>NetDeamon</code> receives packets from the network and sends the data
+ * data into one or two pipes. One pipe to a <code>SortDeamon</code>, and the
+ * other is a <code>TapeDaemon</code> or <code>DiskDaemon</code> if one is
+ * activated.
+ * 
  * @author Ken Swartz
  * @author Dale Visser
  * @version 0.5
  * @since JDK 1.1
  */
-public  class NetDaemon extends GoodThread {
-    
-    private final MessageHandler msgHandler;
-    private final DatagramSocket dataSocket;
-    
-    /**
-     * ring buffers for passing events to sorting
-     */
-    private final RingBuffer sortingRing;
-    
-    /**
-     * ring buffers for passing events to storage
-     */
-    private final RingBuffer storageRing;
-    
-    /**
-     * set if we want data to be put is writing pipe
-     * writting out data
-     */
-    private boolean writerOn=false;
-    
-    /**
-     *set if we sorting data
-     */
-    private boolean sorterOn=true;
-    
-    /**
-     * number of packets counter
-     */
-    private int packetCount=0;	    //number of packets received
-    private int notSortCount=0;	    //number of packets not sorted
-    private int notStorCount=0;//number of packets not stored
-    
-    /**
-     * Constructor passed both storage and sorting pipes.
-     *
-     * @param sortingRing buffer the events are sent to for sorting
-     * @param storageRing buffer the events are sent to for storage
-     * @param msgHandler where messages can be sent to the console
-     * @exception SortException thrown if there's a problem setting up the pipes
-     */
-    public NetDaemon( RingBuffer sortingRing, RingBuffer storageRing, MessageHandler msgHandler,
-    String host, int port)
-    throws SortException {
-        this.sortingRing=sortingRing;
-        this.storageRing=storageRing;
-        this.msgHandler=msgHandler;
-        try {//ceate a port listener
-            final InetAddress dataAddress =InetAddress.getByName(host);
-            dataSocket=new DatagramSocket(port,dataAddress);            
-        } catch (UnknownHostException e) {
-            throw new SortException(getClass().getName()+": The host, "+host+", is unknown.");
-        } catch(BindException be){
-            throw new SortException(getClass().getName()+": Could not bind to data socket. Are other copies of Jam running?");
-        } catch (IOException e) {
-            throw new SortException(getClass().getName()+": Could not create data socket.");
-        }
-        setPriority(ThreadPriorities.NET);
-        setDaemon(true);//the user doesn't interact with this thread
-        setName("UDP Data Receiver");
-    }
-    
-    /**
-     * Calls <code>receiveLoop</code>.
-     *
-     * @see #receiveLoop()
-     */
-    public void run(){
-    	final StringBuffer message=new StringBuffer(
-   		getClass().getName()).append(
-		"--communication with acquisition halted, because of ");
-        try {
-            receiveLoop();
-        } catch (Exception e){
-        	message.append(e.getClass().getName()).append(':').append(
-        	e.getMessage());
-            msgHandler.warningOutln(message.toString());
-        }
-    }
-    
-	private boolean emptyBefore=false;
-	private synchronized void setEmptyBefore(boolean state){
-		if (!emptyBefore && state){
-			final Object[] options=new Object[1];
-			options[0]=endAction;
-			JOptionPane.showInputDialog(null,"The ring buffer has ","Ring Buffer Full",
-					JOptionPane.WARNING_MESSAGE,null,options,options[0]);
+public class NetDaemon extends GoodThread {
+
+	private final MessageHandler msgHandler;
+
+	private final DatagramSocket dataSocket;
+
+	/**
+	 * ring buffers for passing events to sorting
+	 */
+	private final RingBuffer sortingRing;
+
+	/**
+	 * ring buffers for passing events to storage
+	 */
+	private final RingBuffer storageRing;
+
+	/**
+	 * set if we want data to be put is writing pipe writting out data
+	 */
+	private boolean writerOn = false;
+
+	/**
+	 * set if we sorting data
+	 */
+	private boolean sorterOn = true;
+
+	/**
+	 * number of packets counter
+	 */
+	private int packetCount = 0; //number of packets received
+
+	private int notSortCount = 0; //number of packets not sorted
+
+	private int notStorCount = 0;//number of packets not stored
+
+	/**
+	 * Constructor passed both storage and sorting pipes.
+	 * 
+	 * @param sortRing
+	 *            buffer the events are sent to for sorting
+	 * @param storeRing
+	 *            buffer the events are sent to for storage
+	 * @param msgHandle
+	 *            where messages can be sent to the console
+	 * @exception SortException
+	 *                thrown if there's a problem setting up the pipes
+	 */
+	public NetDaemon(RingBuffer sortRing, RingBuffer storeRing,
+			MessageHandler msgHandle, String host, int port)
+			throws SortException {
+		sortingRing = sortRing;
+		storageRing = storeRing;
+		msgHandler = msgHandle;
+		try {//ceate a port listener
+			final InetAddress dataAddress = InetAddress.getByName(host);
+			dataSocket = new DatagramSocket(port, dataAddress);
+		} catch (UnknownHostException e) {
+			throw new SortException(getClass().getName() + ": The host, "
+					+ host + ", is unknown.");
+		} catch (BindException be) {
+			throw new SortException(
+					getClass().getName()
+							+ ": Could not bind to data socket. Are other copies of Jam running?");
+		} catch (IOException e) {
+			throw new SortException(getClass().getName()
+					+ ": Could not create data socket.");
 		}
-		emptyBefore=state;
+		setPriority(ThreadPriorities.NET);
+		setDaemon(true);//the user doesn't interact with this thread
+		setName("UDP Data Receiver");
 	}
-	
-	private synchronized boolean isEmptyBefore(){
+
+	/**
+	 * Calls <code>receiveLoop</code>.
+	 * 
+	 * @see #receiveLoop()
+	 */
+	public void run() {
+		final StringBuffer message = new StringBuffer(getClass().getName())
+				.append("--communication with acquisition halted, because of ");
+		try {
+			receiveLoop();
+		} catch (Exception e) {
+			message.append(e.getClass().getName()).append(':').append(
+					e.getMessage());
+			msgHandler.warningOutln(message.toString());
+		}
+	}
+
+	private boolean emptyBefore = false;
+
+	public synchronized void setEmptyBefore(boolean state) {
+		if (!emptyBefore && state) {
+			final StringBuffer mesg = new StringBuffer();
+			mesg.append("The sorting process lost a buffer. Click 'OK' to\n");
+			mesg
+					.append("end the current run, 'Cancel' to have Jam attempt to\n");
+			mesg
+					.append("continue automatically sampling events in order to keep\n");
+			mesg.append("up with the acquisition with no further warnings.\n");
+			final boolean confirmed = JOptionPane.showConfirmDialog(JamStatus
+					.instance().getFrame(), mesg.toString(),
+					"Buffer lost. End run?", JOptionPane.OK_CANCEL_OPTION,
+					JOptionPane.WARNING_MESSAGE) == JOptionPane.OK_OPTION;
+			if (confirmed) {
+				new JButton(endAction).doClick();
+			} 
+		}
+		emptyBefore = state;
+	}
+
+	synchronized boolean isEmptyBefore() {
 		return emptyBefore;
 	}
 
 	/**
-     * Runs in an infinite loop receiving data from the local net
-     * and stuffing it into a couple of pipes.
-     *
-     * @exception IOException if there's a problem storing the data
-     * @exception SortException if there's a problem sorting the data
-     */
-    public  void receiveLoop() throws IOException, SortException {
-        if (dataSocket==null) {
-            throw new SortException("Could not start netDeamon, socket null {NetDaemon]");
-        }
-		final byte [] bufferOut=RingBuffer.freshBuffer();
-		final DatagramPacket dataIn = new DatagramPacket(bufferOut, bufferOut.length);
-        while(this.checkState()){//loop as long as state is RUN
-            /* wait for packet */
-            dataSocket.receive(dataIn);
-            if (checkState()) {
-                dataIn.getData();//data goes to bufferOut
-                packetCount++;
-                /* Put buffer into to sorting ring with sample fraction */
-                if( sorterOn ){
-                    try {
-                        sortingRing.putBuffer(bufferOut);
-                    } catch (RingFullException rfe){
-                        notSortCount++;
-                        setEmptyBefore(true);
-                    }
-                }
-                /* put buffer into to storage ring */
-                if(writerOn){
-                    try {
-                        storageRing.putBuffer(bufferOut);
-                    } catch (RingFullException rfe) {
-                        notStorCount++;
-                        msgHandler.errorOutln("Storage Buffer "+rfe.getMessage());
-                    }
-                }
-            } else {//received a packet while thread state not RUN
-                msgHandler.warningOutln("Warning: recevied buffer while NetDaemon thread"+
-                " state was not RUN: state="+this);
-            }
-        }//end RUN loop
-    }
-    
-    /**
-     * Sets whether to write out events to the storage pipe.
-     *
-     * @param writerOn <code>true</code> if write events, <code>false</code> if not
-     */
-    public void setWriter(boolean writerOn){
-        if(storageRing!=null){
-            this.writerOn=writerOn;
-        } else {
-            msgHandler.warningOutln("Can't write out events no ring buffer [NetDaemon].");
-        }
-    }
-    
-    /**
-     * Closes the network connection.
-     */
-    public void closeNet() {
-        if(dataSocket!=null){
-            dataSocket.close();
-        }
-    }
-    
-    /**
-     * Returns the total packets sent.
-     *
-     * @return the total number of packets sent
-     */
-    public int getPacketCount() {
-        return packetCount;
-    }
-    
-    /**
-     * Sets the packet count.
-     *
-     * @param count the total number of packets sent
-     */
-    public void setPacketCount(int count) {
-        packetCount=count;
-    }
-    
-    public int getStoredPackets(){
-    	return packetCount-notStorCount;
-    }
-    
-    public int getSortedBuffers(){
-    	return packetCount-notSortCount;
-    }
-    
-    public void resetCounters(){
-    	synchronized(sortingRing){
-    		packetCount=0;
-    		notStorCount=0;
-    		notSortCount=0;
-    	}
-    }
-    
-    private Action endAction;
-    public void setEndRunAction(Action a){
-    	endAction=a;
-    }
+	 * Runs in an infinite loop receiving data from the local net and stuffing
+	 * it into a couple of pipes.
+	 * 
+	 * @exception IOException
+	 *                if there's a problem storing the data
+	 * @exception SortException
+	 *                if there's a problem sorting the data
+	 */
+	public void receiveLoop() throws IOException, SortException {
+		if (dataSocket == null) {
+			throw new SortException(
+					"Could not start netDeamon, socket null {NetDaemon]");
+		}
+		final byte[] bufferOut = RingBuffer.freshBuffer();
+		final DatagramPacket dataIn = new DatagramPacket(bufferOut,
+				bufferOut.length);
+		while (this.checkState()) {//loop as long as state is RUN
+			/* wait for packet */
+			dataSocket.receive(dataIn);
+			if (checkState()) {
+				dataIn.getData();//data goes to bufferOut
+				packetCount++;
+				/* Put buffer into to sorting ring with sample fraction */
+				if (sorterOn) {
+					try {
+						sortingRing.putBuffer(bufferOut);
+					} catch (RingFullException rfe) {
+						notSortCount++;
+						setEmptyBefore(true);
+					}
+				}
+				/* put buffer into to storage ring */
+				if (writerOn) {
+					try {
+						storageRing.putBuffer(bufferOut);
+					} catch (RingFullException rfe) {
+						notStorCount++;
+						msgHandler.errorOutln("Storage Buffer "
+								+ rfe.getMessage());
+					}
+				}
+			} else {//received a packet while thread state not RUN
+				msgHandler
+						.warningOutln("Warning: recevied buffer while NetDaemon thread"
+								+ " state was not RUN: state=" + this);
+			}
+		}//end RUN loop
+	}
+
+	/**
+	 * Sets whether to write out events to the storage pipe.
+	 * 
+	 * @param writerOn
+	 *            <code>true</code> if write events, <code>false</code> if
+	 *            not
+	 */
+	public void setWriter(boolean writerOn) {
+		if (storageRing != null) {
+			this.writerOn = writerOn;
+		} else {
+			msgHandler
+					.warningOutln("Can't write out events no ring buffer [NetDaemon].");
+		}
+	}
+
+	/**
+	 * Closes the network connection.
+	 */
+	public void closeNet() {
+		if (dataSocket != null) {
+			dataSocket.close();
+		}
+	}
+
+	/**
+	 * Returns the total packets sent.
+	 * 
+	 * @return the total number of packets sent
+	 */
+	public int getPacketCount() {
+		return packetCount;
+	}
+
+	/**
+	 * Sets the packet count.
+	 * 
+	 * @param count
+	 *            the total number of packets sent
+	 */
+	public void setPacketCount(int count) {
+		packetCount = count;
+	}
+
+	public int getStoredPackets() {
+		return packetCount - notStorCount;
+	}
+
+	public int getSortedBuffers() {
+		return packetCount - notSortCount;
+	}
+
+	public void resetCounters() {
+		synchronized (sortingRing) {
+			packetCount = 0;
+			notStorCount = 0;
+			notSortCount = 0;
+		}
+	}
+
+	private Action endAction;
+	
+	/**
+	 * Called by RunControl to give NetDaemon access to the "end run" action.
+	 * 
+	 * @param a the action which ends the current run
+	 */
+	public void setEndRunAction(Action a) {
+		endAction = a;
+	}
 }
