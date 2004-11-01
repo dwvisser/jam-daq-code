ca7c67304cf2e33011c086a9a827344bdb4f2381
Dale Visser <dwvisser@users.sourceforge.net>
2004-11-01 19:37:44 +0000

    Simplified by removing the integer 'type' field, and simply have one field 'source' which has it's class type queried to determine how the monitor gets its value. Also, if a null source is passed to the constructor, it fails with an IllegalArgumentException now, giving a useful message on Jam's console.

--- jam/data/Monitor.java
+++ jam/data/Monitor.java
@@ -1,4 +1,5 @@
 package jam.data;
+
 import jam.global.Sorter;
 
 import java.applet.AudioClip;
@@ -10,12 +11,11 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
-
 /**
- * This class is for monitoring the status of data acquisition.  Monitors can show the
- * status of
- * things like event rates, beam current, rate of growth in a histogram, etc.
- *
+ * This class is for monitoring the status of data acquisition. Monitors can
+ * show the status of things like event rates, beam current, rate of growth in a
+ * histogram, etc.
+ * 
  * @author Ken Swartz
  * @author Dale Visser
  * @version 0.5,0.9
@@ -23,291 +23,296 @@ import java.util.Map;
  */
 public final class Monitor implements Serializable {
 
-    /**
-     * Lookup table for all monitors.
-     */
-    public static Map monitorTable=Collections.synchronizedMap(new HashMap());
+	/**
+	 * Lookup table for all monitors.
+	 */
+	public static Map monitorTable = Collections.synchronizedMap(new HashMap());
 
-    /**
-     * List of all monitors.
-     */
-    public static List monitorList=Collections.synchronizedList(new ArrayList());
+	/**
+	 * List of all monitors.
+	 */
+	public static List monitorList = Collections
+			.synchronizedList(new ArrayList());
 
-    /**
-     * Monitor based on value of a scaler.
-     */
-    public static final int SCALER=0;
+	/** The update interval */
+	private static int interval;
 
-    /**
-     * Monitor based on area in a gate.
-     */
-    public static final int GATE=1;
+	private final String name; //name
 
-    /**
-     * Sortfile defined monitor.
-     */
-    public static final int SORT=2;
-    /** The update interval */
-    private static int interval;
+	private final Object source;
 
-    private final String name;  //name
-    private final int type;    //monitor type gate, scaler ...
+	private double threshold;
 
-    private double threshold;
-    private double maximum;
+	private double maximum;
 
-    private boolean alarm;
-    private java.applet.AudioClip audioClip;
+	private boolean alarm;
 
-    private final Sorter sortClass;
-    private final Scaler scaler;
-    private final Gate gate;
+	private java.applet.AudioClip audioClip;
 
-    private double valueNew;  //the newest value set
-    private double valueOld;  //the previous value set
-    private double value;    //value for testing
+	private double valueNew; //the newest value set
 
-    /**
-     * Constructor which gives type <code>SORT</code>.
-     *
-     * @param n name of the monitor for display in dialog
-     * @param sort the sort routine which produces the monitor values
-     * @see #SORT
-     */
-    public Monitor(String n, Sorter sort) {
-        name=n;
-        type=SORT;
-        sortClass=sort;
-        gate=null;
-        scaler=null;
-        addToCollections();
-    }
-    
-    private final void addToCollections(){
-		monitorTable.put(name,this);
-		monitorList.add(this);    	
-    }
+	private double valueOld; //the previous value set
 
-    /**
-     * Constructor which gives type <code>SCALER</code>.
-     *
-     * @param n name of the monitor for display in dialog
-     * @param s the scaler which is monitored
-     * @see #SCALER
-     */
-    public Monitor(String n, Scaler s)  {
-        name=n;
-        scaler=s;
-        gate=null;
-        sortClass=null;
-        type=SCALER;
-        addToCollections();
-    }
+	private double value; //value for testing
 
-    /**
-     * Constructor which gives type <code>GATE</code>.
-     *
-     * @param n name of the monitor for display in dialog
-     * @param g the gate whose area is monitored
-     * @see #GATE
-     */
-    public Monitor(String n, Gate g)  {
-        name=n;
-        gate=g;
-        scaler=null;
-        sortClass=null;
-        type=GATE;
-        addToCollections();
-    }
+	/**
+	 * Constructor which gives type <code>SORT</code>.
+	 * 
+	 * @param n
+	 *            name of the monitor for display in dialog
+	 * @param sort
+	 *            the sort routine which produces the monitor values
+	 * @see #SORT
+	 */
+	public Monitor(String n, Sorter sort) {
+		name = n;
+		source = sort;
+		if (source==null){
+			throw new IllegalArgumentException("Monitor \""+n+"\": source must be non-null.");
+		}
+		addToCollections();
+	}
 
-    /**
-     * Set the interval in seconds at which updates occur.
-     *
-     * @param intervalIn interval in seconds
-     */
-    public synchronized static void setInterval(int intervalIn){
-        interval=intervalIn;
-    }
+	private final void addToCollections() {
+		monitorTable.put(name, this);
+		monitorList.add(this);
+	}
 
-    /**
-     * Gets the interval in seconds at which updates occur.
-     *
-     * @return interval in seconds
-     */
-    public synchronized static int getInterval(){
-        return interval;
-    }
+	/**
+	 * Constructor which gives type <code>SCALER</code>.
+	 * 
+	 * @param n
+	 *            name of the monitor for display in dialog
+	 * @param s
+	 *            the scaler which is monitored
+	 * @see #SCALER
+	 */
+	public Monitor(String n, Scaler s) {
+		name = n;
+		source = s;
+		if (source==null){
+			throw new IllegalArgumentException("Monitor \""+n+"\": source must be non-null.");
+		}
+		addToCollections();
+	}
 
-    /**
-     * Returns the list of monitors.
-     *
-     * @return the list of monitors
-     */
-    public static List getMonitorList() {
-        return Collections.unmodifiableList(monitorList);
-    }
+	/**
+	 * Constructor which gives type <code>GATE</code>.
+	 * 
+	 * @param n
+	 *            name of the monitor for display in dialog
+	 * @param g
+	 *            the gate whose area is monitored
+	 * @see #GATE
+	 */
+	public Monitor(String n, Gate g) {
+		name = n;
+		source = g;
+		if (source==null){
+			throw new IllegalArgumentException("Monitor \""+n+"\": source must be non-null.");
+		}
+		addToCollections();
+	}
 
-    /**
-     * Sets the list of monitor objects.
-     *
-     * @param inMonList must contain all <code>Monitor</code> objects
-     */
-    public static void setMonitorList(List inMonList){
-        //clear current lists
-        clearList();
-        //loop for all histograms
-        for(Iterator allMonitors=inMonList.iterator(); allMonitors.hasNext();) {
-            Monitor monitor=(Monitor)allMonitors.next();
-            String name=monitor.getName();
-            monitorTable.put(name, monitor);
-            monitorList.add(monitor);
-        }
-    }
+	/**
+	 * Set the interval in seconds at which updates occur.
+	 * 
+	 * @param intervalIn
+	 *            interval in seconds
+	 */
+	public synchronized static void setInterval(int intervalIn) {
+		interval = intervalIn;
+	}
 
-    /**
-     * Clears the list of monitors.
-     */
-    public static void  clearList(){
-        monitorTable.clear();
-        monitorList.clear();
-        //run garbage collector
-        System.gc();
-    }
+	/**
+	 * Gets the interval in seconds at which updates occur.
+	 * 
+	 * @return interval in seconds
+	 */
+	public synchronized static int getInterval() {
+		return interval;
+	}
 
-    /**
-     * Returns this monitor's name. The name is used in
-     * display and to retrieve the monitor.
-     *
-     * @return this montor's name
-     */
-    public String getName(){
-        return name;
-    }
+	/**
+	 * Returns the list of monitors.
+	 * 
+	 * @return the list of monitors
+	 */
+	public static List getMonitorList() {
+		return Collections.unmodifiableList(monitorList);
+	}
 
-    /**
-     * Returns this monitor's current value.
-     *
-     * @return this monitor's current value
-     */
-    public synchronized double getValue(){
-        return value;
-    }
+	/**
+	 * Sets the list of monitor objects.
+	 * 
+	 * @param inMonList
+	 *            must contain all <code>Monitor</code> objects
+	 */
+	public static void setMonitorList(List inMonList) {
+		clearList();
+		for (final Iterator allMonitors = inMonList.iterator(); allMonitors
+				.hasNext();) {
+			final Monitor monitor = (Monitor) allMonitors.next();
+			final String name = monitor.getName();
+			monitorTable.put(name, monitor);
+			monitorList.add(monitor);
+		}
+	}
 
-    /**
-     * Sets this monitor's latest value.
-     *
-     * @param valueIn the new value
-     */
-    public synchronized void setValue(int valueIn){
-        valueNew=valueIn;
-    }
+	/**
+	 * Clears the list of monitors.
+	 */
+	public static void clearList() {
+		monitorTable.clear();
+		monitorList.clear();
+		/* run garbage collector */
+		System.gc();
+	}
 
-    /**
-     * Sets this monitor's value to zero.
-     */
-    public synchronized void reset() {
-        value=0;
-    }
+	/**
+	 * Returns this monitor's name. The name is used in display and to retrieve
+	 * the monitor.
+	 * 
+	 * @return this montor's name
+	 */
+	public String getName() {
+		return name;
+	}
 
-    /**
-     * Updates this monitor, calculating the latest monitor values.
-     * Keeps the most recent value, too, for rate determination.
-     */
-    public void update(){
-        if(type==SCALER){
-            valueNew=scaler.getValue();
-            value=(valueNew-valueOld)/interval;
-            valueOld=valueNew;
-        } else if(type==GATE) {
-            valueNew=((double)gate.getArea());
-            value=(valueNew-valueOld)/interval;
-            valueOld=valueNew;
-        } else if(type==SORT) {
-            value=sortClass.monitor(name);
-        }
-    }
+	/**
+	 * Returns this monitor's current value.
+	 * 
+	 * @return this monitor's current value
+	 */
+	public synchronized double getValue() {
+		return value;
+	}
 
-    /**
-     * Sets the threshold value, which is the minimum value for a monitor to have without
-     * <code>MonitorControl</code> issuing a warning beep.
-     *
-     * @param inThreshold the new minimum
-     * @see jam.data.control.MonitorControl
-     */
-    public synchronized void setThreshold(double inThreshold){
-        threshold=inThreshold;
-    }
+	/**
+	 * Sets this monitor's latest value.
+	 * 
+	 * @param valueIn
+	 *            the new value
+	 */
+	public synchronized void setValue(int valueIn) {
+		valueNew = valueIn;
+	}
 
-    /**
-     * Returns the threshold value for this monitor.
-     *
-     * @return the threshold value
-     */
-    public synchronized double getThreshold(){
-        return threshold;
-    }
+	/**
+	 * Sets this monitor's value to zero.
+	 */
+	public synchronized void reset() {
+		value = 0;
+	}
 
-    /**
-     * Sets the maximum value, which is the maximum value for a monitor to have without
-     * <code>MonitorControl</code> issuing a warning beep.
-     *
-     * @param inMaximum the new maximum
-     * @see jam.data.control.MonitorControl
-     */
-    public synchronized void setMaximum(double inMaximum){
-        maximum=inMaximum;
-    }
+	/**
+	 * Updates this monitor, calculating the latest monitor values. Keeps the
+	 * most recent value, too, for rate determination.
+	 */
+	public void update() {
+		if (source instanceof Scaler) {
+			valueNew = ((Scaler)source).getValue();
+			value = (valueNew - valueOld) / interval;
+			valueOld = valueNew;
+		} else if (source instanceof Gate) {
+			valueNew = ((Gate)source).getArea();
+			value = (valueNew - valueOld) / interval;
+			valueOld = valueNew;
+		} else if (source instanceof Sorter) {
+			value = ((Sorter)source).monitor(name);
+		}
+	}
 
-    /**
-     * Returns the maximum value for this monitor.
-     *
-     * @return the maximum value for this monitor
-     */
-    public synchronized double getMaximum(){
-        return maximum;
-    }
+	/**
+	 * Sets the threshold value, which is the minimum value for a monitor to
+	 * have without <code>MonitorControl</code> issuing a warning beep.
+	 * 
+	 * @param inThreshold
+	 *            the new minimum
+	 * @see jam.data.control.MonitorControl
+	 */
+	public synchronized void setThreshold(double inThreshold) {
+		threshold = inThreshold;
+	}
 
-    /**
-     * Sets whether the alarm is activated. If the alarm is not activated, <code>MonitorControl</code>
-     * simply turns the indicator bar red when the value is below threshold or above the maximum.  If
-     * it is activated, an alarm sound is issued too.
-     *
-     * @param inAlarm <code>true</code> if an audible alarm is desired, <code>false</code> if not
-     */
-    public synchronized void setAlarm(boolean inAlarm){
-        alarm=inAlarm;
-    }
+	/**
+	 * Returns the threshold value for this monitor.
+	 * 
+	 * @return the threshold value
+	 */
+	public synchronized double getThreshold() {
+		return threshold;
+	}
 
-    /**
-     * Returns whether alarm is activated or not.
-     *
-     * @return <code>true</code> if an audible alarm is desired, <code>false</code> if not
-     */
-    public synchronized boolean getAlarm(){
-        return alarm;
-    }
+	/**
+	 * Sets the maximum value, which is the maximum value for a monitor to have
+	 * without <code>MonitorControl</code> issuing a warning beep.
+	 * 
+	 * @param inMaximum
+	 *            the new maximum
+	 * @see jam.data.control.MonitorControl
+	 */
+	public synchronized void setMaximum(double inMaximum) {
+		maximum = inMaximum;
+	}
 
-    /**
-     * NOT YET IMPLEMENTED, Sets an <code>AudioClip</code> object to be played for alarms if the alarm is enabled.
-     * Currently, the plan is to fully implement this when the JDK 1.2 <code>javax.media</code> packeage is
-     * available.
-     */
-    public synchronized void setAudioClip(AudioClip ac){
-        audioClip=ac;
-    }
+	/**
+	 * Returns the maximum value for this monitor.
+	 * 
+	 * @return the maximum value for this monitor
+	 */
+	public synchronized double getMaximum() {
+		return maximum;
+	}
 
-    /**
-     * NOT YET IMPLEMENTED, Gets the current <code>AudioClip</code> object to be played for alarms if the alarm is enabled.
-     * Currently, the plan is to fully implement this when the JDK 1.2 <code>javax.media</code> packeage is
-     * available.
-     *
-     * @return the sound clip for this monitor's alarm, <code>null</code> indicates that a default system beep is desired
-     */
-    public synchronized AudioClip getAudioClip(){
-        return audioClip;
-    }
+	/**
+	 * Sets whether the alarm is activated. If the alarm is not activated,
+	 * <code>MonitorControl</code> simply turns the indicator bar red when the
+	 * value is below threshold or above the maximum. If it is activated, an
+	 * alarm sound is issued too.
+	 * 
+	 * @param inAlarm
+	 *            <code>true</code> if an audible alarm is desired,
+	 *            <code>false</code> if not
+	 */
+	public synchronized void setAlarm(boolean inAlarm) {
+		alarm = inAlarm;
+	}
 
-	public synchronized boolean isAcceptable(){
-		return value>maximum || value < threshold;
+	/**
+	 * Returns whether alarm is activated or not.
+	 * 
+	 * @return <code>true</code> if an audible alarm is desired,
+	 *         <code>false</code> if not
+	 */
+	public synchronized boolean getAlarm() {
+		return alarm;
+	}
+
+	/**
+	 * NOT YET IMPLEMENTED, Sets an <code>AudioClip</code> object to be played
+	 * for alarms if the alarm is enabled. Currently, the plan is to fully
+	 * implement this when the JDK 1.2 <code>javax.media</code> packeage is
+	 * available.
+	 */
+	public synchronized void setAudioClip(AudioClip ac) {
+		audioClip = ac;
+	}
+
+	/**
+	 * NOT YET IMPLEMENTED, Gets the current <code>AudioClip</code> object to
+	 * be played for alarms if the alarm is enabled. Currently, the plan is to
+	 * fully implement this when the JDK 1.2 <code>javax.media</code> packeage
+	 * is available.
+	 * 
+	 * @return the sound clip for this monitor's alarm, <code>null</code>
+	 *         indicates that a default system beep is desired
+	 */
+	public synchronized AudioClip getAudioClip() {
+		return audioClip;
+	}
+
+	public synchronized boolean isAcceptable() {
+		return value > maximum || value < threshold;
 	}
 }
