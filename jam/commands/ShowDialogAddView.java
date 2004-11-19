ca15ca32856f45b23ac56eac9dc0de32e64d17e9
Dale Visser <dwvisser@users.sourceforge.net>
2004-11-19 20:49:54 +0000

    Moved View dialogs to inner classes of the ShowDialog*View commands.

--- jam/commands/ShowDialogAddView.java
+++ jam/commands/ShowDialogAddView.java
@@ -1,31 +1,141 @@
 package jam.commands;
 
-import jam.global.CommandListenerException;
-import jam.plot.ViewNew;
+import jam.global.BroadcastEvent;
+import jam.plot.View;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
 
 /**
  * Command to add view.
  * 
  * @author Ken Swartz
  */
-public class ShowDialogAddView extends AbstractCommand {
+public class ShowDialogAddView extends AbstractShowDialog {
 
-	
-	private ViewNew viewNew;
-	
-	public void initCommand(){
+	public void initCommand() {
 		putValue(NAME, "New\u2026");
-		viewNew = new ViewNew();		
-	}
-	
-	protected void execute(Object[] cmdParams) throws CommandException {
-		viewNew.showView();
-
+		dialog = new ViewNew();
 	}
 
-	protected void executeParse(String[] cmdTokens)
-			throws CommandListenerException {
-			//NOP
-	}
+	/**
+	 * Create a view dialog
+	 * 
+	 * @author Ken Swartz
+	 */
+	private class ViewNew extends JDialog {
 
+		private final String[] DEFAULT_NUMBERS = { "1", "2", "3", "4",
+				"5", "7", "8" };
+
+		private final JTextField textName;
+
+		private final JComboBox comboRows;
+
+		private final JComboBox comboCols;
+
+		public ViewNew() {
+			setTitle("New View");
+			setModal(false);
+			final Container cdnew = getContentPane();
+			setResizable(false);
+			cdnew.setLayout(new BorderLayout(5, 5));
+			setLocation(20, 50);
+			/* Labels on the left */
+			final JPanel pLabels = new JPanel(new GridLayout(0, 1, 5, 5));
+			pLabels.setBorder(new EmptyBorder(10, 10, 0, 0));
+			cdnew.add(pLabels, BorderLayout.WEST);
+			final JLabel ln = new JLabel("Name", JLabel.RIGHT);
+			pLabels.add(ln);
+			final JLabel lrw = new JLabel("Rows", JLabel.RIGHT);
+			pLabels.add(lrw);
+			final JLabel lcl = new JLabel("Columns", JLabel.RIGHT);
+			pLabels.add(lcl);
+			/* Entries */
+			final JPanel pEntires = new JPanel(new GridLayout(0, 1, 5, 5));
+			pEntires.setBorder(new EmptyBorder(10, 0, 0, 10));
+			cdnew.add(pEntires, BorderLayout.CENTER);
+			/* Name field */
+			final JPanel pName = new JPanel(new FlowLayout(FlowLayout.LEFT, 0,
+					0));
+			pEntires.add(pName);
+			textName = new JTextField("");
+			textName.setColumns(15);
+			pName.add(textName);
+			/* Rows Combo */
+			final JPanel pRows = new JPanel(new FlowLayout(FlowLayout.LEFT, 0,
+					0));
+			pEntires.add(pRows);
+			comboRows = new JComboBox(DEFAULT_NUMBERS);
+			pRows.add(comboRows);
+			/* Cols Combo */
+			final JPanel pCols = new JPanel(new FlowLayout(FlowLayout.LEFT, 0,
+					0));
+			pEntires.add(pCols);
+			comboCols = new JComboBox(DEFAULT_NUMBERS);
+			pCols.add(comboCols);
+
+			/* panel for buttons */
+			final JPanel pbutton = new JPanel(new FlowLayout(FlowLayout.CENTER));
+			cdnew.add(pbutton, BorderLayout.SOUTH);
+			final JPanel pbnew = new JPanel();
+			pbnew.setLayout(new GridLayout(1, 0, 5, 5));
+			pbutton.add(pbnew, BorderLayout.SOUTH);
+			final JButton bok = new JButton("OK");
+			bok.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent ae) {
+					makeView();
+					dispose();
+
+				}
+			});
+			pbnew.add(bok);
+			final JButton bapply = new JButton("Apply");
+			bapply.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent ae) {
+					makeView();
+				}
+			});
+			pbnew.add(bapply);
+			final JButton bcancel = new JButton("Cancel");
+			bcancel.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent ae) {
+					dispose();
+				}
+			});
+			pbnew.add(bcancel);
+			pack();
+		}
+
+		/**
+		 * Make a new view
+		 */
+		private void makeView() {
+			String name = textName.getText();
+			View viewNew;
+			int nRows = Integer.parseInt(((String) comboRows.getSelectedItem())
+					.trim());
+			int nCols = Integer.parseInt(((String) comboCols.getSelectedItem())
+					.trim());
+			/* Check for blank name */
+			if (name.trim().equals("")) {
+				name = "View " + nRows + "x" + nCols;
+			}
+			viewNew = new View(name, nRows, nCols);
+			broadcaster.broadcast(BroadcastEvent.Command.VIEW_NEW);
+			status.getDisplay().setView(viewNew);
+		}
+	}
 }
--- jam/commands/ShowDialogDeleteView.java
+++ jam/commands/ShowDialogDeleteView.java
@@ -1,31 +1,140 @@
 package jam.commands;
 
-import jam.global.CommandListenerException;
-import jam.plot.ViewDelete;
+import jam.global.BroadcastEvent;
+import jam.global.Broadcaster;
+import jam.plot.View;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.swing.AbstractButton;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
 
 /**
  * Command to delete view.
  * 
  * @author Ken Swartz
  */
-public class ShowDialogDeleteView extends AbstractCommand {
-
-	
-	private ViewDelete viewDelete;
+class ShowDialogDeleteView extends AbstractShowDialog {
 	
 	public void initCommand(){
 		putValue(NAME, "Delete\u2026");
-		viewDelete = new ViewDelete();		
+		dialog = new ViewDelete();		
 	}
 	
-	protected void execute(Object[] cmdParams) throws CommandException {
-		viewDelete.showView();
+	private class ViewDelete extends JDialog {
 
+		private final JComboBox comboNames;
+
+		private static final String CHOOSE_NAME = "Choose Name";
+		
+		private final AbstractButton bok=new JButton("OK");
+		private final AbstractButton bapply=new JButton("Apply");
+
+		public ViewDelete() {
+			setTitle("Delete View");
+			setModal(false);
+			final Container cdnew = getContentPane();
+			setResizable(false);
+			cdnew.setLayout(new BorderLayout(5, 5));
+			setLocation(20, 50);
+			final JPanel pNames = new JPanel(
+					new FlowLayout(FlowLayout.LEFT, 20, 20));
+			pNames.setBorder(new EmptyBorder(0, 10, 0, 10));
+			cdnew.add(pNames, BorderLayout.CENTER);
+			final JLabel ln = new JLabel("Name", JLabel.RIGHT);
+			pNames.add(ln);
+			/* Name combo box */
+			comboNames = new JComboBox();
+			Dimension dim = comboNames.getPreferredSize();
+			dim.width = 200;
+			comboNames.setPreferredSize(dim);
+			pNames.add(comboNames);
+			/* panel for buttons */
+			final JPanel pbutton = new JPanel(new FlowLayout(FlowLayout.CENTER));
+			cdnew.add(pbutton, BorderLayout.SOUTH);
+			final JPanel pbnew = new JPanel();
+			pbnew.setLayout(new GridLayout(1, 0, 5, 5));
+			pbutton.add(pbnew, BorderLayout.SOUTH);
+			bok.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent ae) {
+					deleteView();
+					dispose();
+				}
+			});
+			pbnew.add(bok);
+			bapply.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent ae) {
+					deleteView();
+				}
+			});
+			pbnew.add(bapply);
+			final JButton bcancel = new JButton("Cancel");
+			bcancel.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent ae) {
+					dispose();
+				}
+			});
+			pbnew.add(bcancel);
+			addWindowListener(new WindowAdapter(){
+				public void windowActivated(WindowEvent e){
+					updateViewNames();
+				}
+				
+				public void windowOpened(WindowEvent e){
+					updateViewNames();
+				}
+			});
+			pack();
+		}
+
+		private void updateViewNames() {
+			final List namesList = new ArrayList();
+			namesList.add(CHOOSE_NAME);
+			namesList.addAll(View.getNameList());
+			comboNames.removeAllItems();
+			final Iterator iter=namesList.iterator();
+			while (iter.hasNext()){
+				final Object next=iter.next();
+				if (!next.equals(View.SINGLE.getName())){
+					comboNames.addItem(next);
+				}
+			}
+			final boolean enable = comboNames.getModel().getSize()>1;
+			comboNames.setEnabled(enable);
+			bok.setEnabled(enable);
+			bapply.setEnabled(enable);
+		}
+
+		/**
+		 * Delete a view.
+		 */
+		private void deleteView() {
+			final String name = (String) comboNames.getSelectedItem();
+			if (!(name.equals(CHOOSE_NAME) || name.equals(View.SINGLE.getName()))) {
+				View.removeView(name);
+			}
+			Broadcaster.getSingletonInstance().broadcast(
+					BroadcastEvent.Command.VIEW_NEW);
+			status.getDisplay().setView(View.SINGLE);
+			updateViewNames();
+		}
 	}
-
-	protected void executeParse(String[] cmdTokens)
-			throws CommandListenerException {
-			//NOP
-	}
-
+	
 }
