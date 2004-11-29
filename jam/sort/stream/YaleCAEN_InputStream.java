8f21e7bbb2a2f8dce24cb6e5c8c717c4619f7500
Dale Visser <dwvisser@users.sourceforge.net>
2004-11-29 21:54:44 +0000

    internal_status -> internalStatus

--- jam/sort/stream/YaleCAEN_InputStream.java
+++ jam/sort/stream/YaleCAEN_InputStream.java
@@ -21,7 +21,7 @@ public class YaleCAEN_InputStream extends L002HeaderReader implements L002Parame
     static final int NUM_V7X5_UNITS=19;//20 slots in a VME crate - 1 controller slot
     static final int NUM_CHANNELS = NUM_V7X5_UNITS*32;
     static final int NUM_EVENTS_TO_STORE = 100;
-    private BufferStatus internal_status = BufferStatus.FIFO_FILLING;
+    private BufferStatus internalStatus = BufferStatus.FIFO_FILLING;
     
     static final int STOP_PAD = 0x01DDDDDD;
     static final int END_PAD = 0x01EEEEEE;
@@ -89,7 +89,7 @@ public class YaleCAEN_InputStream extends L002HeaderReader implements L002Parame
         System.arraycopy(zeros,0,fifo[posPut],0,zeros.length);
         eventNumberTable.put(new Integer(eventNumber), new Integer(posPut));
         incrementPut();
-        if (fifoFull()) internal_status=BufferStatus.FIFO_FULL;
+        if (fifoFull()) internalStatus=BufferStatus.FIFO_FULL;
     }
     
     private boolean fifoFull() {
@@ -106,12 +106,12 @@ public class YaleCAEN_InputStream extends L002HeaderReader implements L002Parame
         int [] rval = fifo[posGet];
         System.arraycopy(rval,0,data,0,data.length);
         incrementGet();
-        if (!inFlushState()) internal_status = BufferStatus.FIFO_FILLING;
+        if (!inFlushState()) internalStatus = BufferStatus.FIFO_FILLING;
     }
     
     private boolean inFlushState(){
-        return internal_status==BufferStatus.FIFO_FLUSH ||
-        internal_status==BufferStatus.FIFO_ENDRUN_FLUSH;
+        return internalStatus==BufferStatus.FIFO_FLUSH ||
+        internalStatus==BufferStatus.FIFO_ENDRUN_FLUSH;
     }
         
     /**
@@ -140,7 +140,7 @@ public class YaleCAEN_InputStream extends L002HeaderReader implements L002Parame
         try {
             /* internal_status may also be in a "flush" mode in which case
              * we skip this read loop and go straight to flushing out another event */ 
-            while(internal_status==BufferStatus.FIFO_FILLING){
+            while(internalStatus==BufferStatus.FIFO_FILLING){
             /* this loop may finish if status changes to "fifo full" mode 
              * when an event index gets added below */
                 int header = dataInput.readInt();
@@ -192,24 +192,24 @@ public class YaleCAEN_InputStream extends L002HeaderReader implements L002Parame
                     }
                 	Scaler.update(tval);
                     rval=EventInputStatus.SCALER_VALUE;
-                    internal_status=BufferStatus.SCALER;
+                    internalStatus=BufferStatus.SCALER;
                 } else if (header==END_OF_BUFFER){//return end of buffer to SortDaemon
                     /* no need to flush here */
                     rval=EventInputStatus.END_BUFFER;
-                    internal_status=BufferStatus.PADDING;
+                    internalStatus=BufferStatus.PADDING;
                 } else if (header==BUFFER_PAD) {
                     rval=EventInputStatus.IGNORE;
-                    internal_status=BufferStatus.PADDING;
+                    internalStatus=BufferStatus.PADDING;
                 } else if (header==STOP_PAD) {
-                    internal_status = BufferStatus.FIFO_FLUSH;
+                    internalStatus = BufferStatus.FIFO_FLUSH;
                 } else if (header==END_PAD) {
-                    internal_status = BufferStatus.FIFO_ENDRUN_FLUSH;
+                    internalStatus = BufferStatus.FIFO_ENDRUN_FLUSH;
                     showMessage("Scaler blocks in file ="+nscalerblock);
                     nscalerblock=0;
                 } else {
                     /* using IGNORE since UNKNOWN WORD causes annoying beeps */
                     rval = EventInputStatus.IGNORE;
-                    internal_status=BufferStatus.PADDING;
+                    internalStatus=BufferStatus.PADDING;
                 }
             }// end of while loop
             /* We've dropped out of the while loop, which means either that 
@@ -221,21 +221,21 @@ public class YaleCAEN_InputStream extends L002HeaderReader implements L002Parame
                     getFirstEvent(data);
                     rval=EventInputStatus.EVENT;
                 } else {//all events flushed, make ready for next event
-                    if (internal_status==BufferStatus.FIFO_FLUSH){
+                    if (internalStatus==BufferStatus.FIFO_FLUSH){
                         rval = EventInputStatus.END_BUFFER;
                     } else {//internal status must be "endrun flush"
                         rval = EventInputStatus.END_RUN;
                     }
-                    internal_status=BufferStatus.FIFO_FILLING;
+                    internalStatus=BufferStatus.FIFO_FILLING;
                 }
             /* The other possibility is that the FIFO is full and we need to 
              * output an event. */
-            } else if (internal_status==BufferStatus.FIFO_FULL) {
+            } else if (internalStatus==BufferStatus.FIFO_FULL) {
                 getFirstEvent(data);//routine retrieves data and updates tracking variables
                 rval = EventInputStatus.EVENT;
             } else {//internal status=SCALER or PADDING
                 /* set to FIFO_FILLING so next call will enter loop */
-                internal_status=BufferStatus.FIFO_FILLING;
+                internalStatus=BufferStatus.FIFO_FILLING;
             }
         } catch (EOFException eofe) {// we got to the end of a file or stream
             rval=EventInputStatus.END_FILE;
