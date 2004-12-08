9f12dd32c662d2f5dba2aaebe9c3b467d4df9fd2
Dale Visser <dwvisser@users.sourceforge.net>
2004-12-08 19:17:11 +0000

    Added javadoc comment.

--- jam/plot/PlotSelectListener.java
+++ jam/plot/PlotSelectListener.java
@@ -4,11 +4,16 @@ package jam.plot;
 
 /**
  * 
- * Called when a plot is selected
+ * Called when a plot is selected.
  * 
  * @author Ken Swartz
  */
 interface PlotSelectListener {
 
+    /**
+     * Called when a plot is selected.
+     * 
+     * @param plot the selection
+     */
 	void plotSelected(Object plot);
 }
