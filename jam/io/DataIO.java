374be66f77427cd5c613ef00c41b01d48c9d36ca
Dale Visser <dwvisser@users.sourceforge.net>
2004-12-15 16:00:48 +0000

    * changed writeFile() signatures * edited javadocs

--- jam/io/DataIO.java
+++ jam/io/DataIO.java
@@ -1,5 +1,7 @@
 package jam.io;
 import java.io.File;
+
+import javax.swing.JFileChooser;
 /**
  * Interface to read in and write out Jam data
  * package
@@ -7,36 +9,64 @@ import java.io.File;
  */
 public interface  DataIO {
     /**
-     *	write out to a given file all options true
-     */ 			        
-    public void writeFile(boolean hist, boolean gate, boolean scaler, File file);
-    
-    /**
-     *	write out to a file, prompted for file
-     *  name all options true
-     */ 			            
-    public int writeFile(boolean hist, boolean gate, boolean scaler);
-    
-    /**
-     *	write out to a given file all options true
-     */ 			        
-    public void writeFile(File file);
-    
-    /**
-     *	write out to a file, prompted for file
-     *  name all options true
-     */ 			            
-    public int writeFile() ;
-    
-    /**
      *	write out to a given file
      *
+     * @param hist whether to write histograms
+     * @param gate whether to write gates
+     * @param scaler whether to write scalers
+     * @param params whether to write parameters
+     * @param file to write to
      */ 			        
-    public boolean readFile(FileOpenMode mode);
+    void writeFile(boolean hist, boolean gate, boolean scaler, boolean params, File file);
     
     /**
-     *	write out to a given file
-     * @param mode Do you 
+     *	write out to a file, prompted for file
+     * 
+     * @param hist whether to write histograms
+     * @param gate whether to write gates
+     * @param scaler whether to write scalers
+     * @param params whether to write parameters
+	 * @return <code>JFileChooser.APPROVE_OPTION</code> or
+	 *         <code>JFileChooser.CANCEL_OPTION</code>
+	 * @see JFileChooser#APPROVE_OPTION
+	 * @see JFileChooser#CANCEL_OPTION
+     */ 			            
+    int writeFile(boolean hist, boolean gate, boolean scaler, boolean params);
+    
+    /**
+     *	write out to a given file all options true
+     *
+     * @param file to write to
      */ 			        
-    public boolean readFile(FileOpenMode mode, File file);
+    void writeFile(File file);
+    
+    /**
+     *	write out to a file, prompted for file
+     *  name all options true
+	 * @return <code>JFileChooser.APPROVE_OPTION</code> or
+	 *         <code>JFileChooser.CANCEL_OPTION</code>
+	 * @see JFileChooser#APPROVE_OPTION
+	 * @see JFileChooser#CANCEL_OPTION
+     */ 			            		            
+    int writeFile() ;
+    
+    /**
+     * Prompt for a file with a file chooser and read it in.
+     *
+     * @param mode open or reload
+     * @see FileOpenMode#OPEN
+     * @see FileOpenMode#RELOAD
+     * @return <code>true</code> if successful
+     */ 			        
+    boolean readFile(FileOpenMode mode);
+    
+    /**
+     * Read a file in.
+     * @param mode open or reload
+     * @param file to read in
+     * @see FileOpenMode#OPEN
+     * @see FileOpenMode#RELOAD
+     * @return <code>true</code> if successful
+     */ 			        
+    boolean readFile(FileOpenMode mode, File file);
 }    
