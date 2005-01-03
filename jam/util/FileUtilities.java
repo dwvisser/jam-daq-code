afdcdfd3b34e63a6500ca570e587250bba578e29
Dale Visser <dwvisser@users.sourceforge.net>
2005-01-03 15:33:41 +0000

    Javadoc edits+changed routine to only have one exit point.

--- jam/util/FileUtilities.java
+++ jam/util/FileUtilities.java
@@ -7,23 +7,37 @@ package jam.util;
  */
 public class FileUtilities {
 
+    /**
+     * Take any existing extension and replace it with the given one.
+     */
 	public static final int FORCE = 0;
+	
+	/**
+	 * Append the given extension regardless.
+	 */
 	public static final int APPEND_ONLY = 1;
+	
+	/**
+	 * Don't change the given filename.
+	 */
 	public static final int NO_CHANGE = 2;
 
 	/**
 	  * Takes a file name and extension and makes the file name to have the extension.
 	  * If the filename has no extension, the extension is appended.  If it does have an
-	  * extension, it is replaced.  By extension, we mean the usual <code>DOX/Unix</code>
+	  * extension, it is replaced.  By extension, we mean the usual <code>DOS/Unix</code>
 	  * filenaming convention:<P>
 	  * <code><i>filename</i>.ext</code><br>
 	  * where <code>ext</code> is usually three characters indicating filetype.
 	  * 
 	  * @param	fileName    with or without an extension already
 	  * @param	extension   file extension; everything up to any decimal is thrown away
-	  * @param   mode	    FORCE if force a change, APPEND_ONLY if only append and not replace, NO_CHANGE if ignore
+	  * @param   mode	    <code>FORCE</code>, <code>APPEND_ONLY</code> or <code>NO_CHANGE</code>
 	  * @return		    a filename with the desired extension
 	  * @throws IllegalArgumentException if an unrecognized mode is given
+	  * @see #APPEND_ONLY
+	  * @see #FORCE
+	  * @see #NO_CHANGE
 	  */
 	public static String setExtension(
 		String fileName,
@@ -32,23 +46,24 @@ public class FileUtilities {
 		String extNoPeriod = extension;
 		String rval=fileName;
 
-		if (mode == NO_CHANGE)
-			return fileName;
-		if (mode != FORCE && mode != APPEND_ONLY && mode != NO_CHANGE) {
-			throw new IllegalArgumentException("Invalid call to setExtension().");
-		}
-		int index = extension.indexOf('.');
-		if (index >= 0) { //period occurs in extension
-			extNoPeriod = extension.substring(index + 1);
-		}
-		index = fileName.indexOf('.');
-		if (index >= 0) { //period occurs in filename
-			if (mode == FORCE) {
-				rval = fileName.substring(0, index + 1) + extNoPeriod;
-			} 
-		} else { //add period
-			rval = fileName + '.' + extNoPeriod;
-		}
-		return rval;
-	}
+		if (mode != NO_CHANGE) {
+            if (mode != FORCE && mode != APPEND_ONLY) {
+                throw new IllegalArgumentException(
+                        "Invalid call to setExtension().");
+            }
+            int index = extension.indexOf('.');
+            if (index >= 0) { //period occurs in extension
+                extNoPeriod = extension.substring(index + 1);
+            }
+            index = fileName.indexOf('.');
+            if (index >= 0) { //period occurs in filename
+                if (mode == FORCE) {
+                    rval = fileName.substring(0, index + 1) + extNoPeriod;
+                }
+            } else { //add period
+                rval = fileName + '.' + extNoPeriod;
+            }
+        }
+        return rval;
+    }
 }
