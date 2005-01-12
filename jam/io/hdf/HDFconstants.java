cb8184bda7af0bcba80642ce9908b08241a5a867
Dale Visser <dwvisser@users.sourceforge.net>
2005-01-12 19:38:25 +0000

    Reduced visibility and added a javadoc comment.

--- jam/io/hdf/HDFconstants.java
+++ jam/io/hdf/HDFconstants.java
@@ -6,11 +6,15 @@ package jam.io.hdf;
  * @author <a href=mailto:dale@visser.name>Dale Visser</a>
  * @version 1.0
  */
-public interface HDFconstants{
+interface HDFconstants{
 
     /**
      * Byte pattern for bytes 0-3 of any standard v4.1r2 HDF file.
      */
     int HDF_HEADER = 0x0e031301; 
+    
+    /**
+     * Number of bytes in hdf header word.
+     */
     int HEAD_NBYTES=4;
 }
