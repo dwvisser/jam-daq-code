fa763a6299ac350e5a90db41895a669ebbd390f6
Dale Visser <dwvisser@users.sourceforge.net>
2005-01-12 19:39:00 +0000

    Added a chaining constructor, made constructors package private.

--- jam/io/hdf/HDFException.java
+++ jam/io/hdf/HDFException.java
@@ -2,16 +2,19 @@
  */
  package jam.io.hdf;
 /**
- * Exception that can be thrown by classes in io.hdf package
+ * Exception that can be thrown by classes in <code>jam.io.hdf</code> package
  *
  * @author Dale Visser
  * @author Ken Swartz
  */ 
 public class HDFException extends Exception {
 
-    public HDFException(String errorMessage) {
-	super(errorMessage);
+    HDFException(String errorMessage) {
+        super(errorMessage);
     }
 
+    HDFException(String msg, Throwable thrown){
+        super(msg,thrown);
+    }
 
 }
