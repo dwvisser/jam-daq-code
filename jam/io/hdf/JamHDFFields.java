77ae7cfab90aee65e956260d222ba022a560cd22
Dale Visser <dwvisser@users.sourceforge.net>
2005-01-12 22:01:27 +0000

    * Removed 2 unused constants. * Added javadoc comments.

--- jam/io/hdf/JamHDFFields.java
+++ jam/io/hdf/JamHDFFields.java
@@ -1,46 +1,76 @@
 package jam.io.hdf;
 
 /**
- * Contains field names used in HDF files produced by
- * the <code>jam.io.hdf</code> package.
+ * Contains field names used in HDF files produced by the
+ * <code>jam.io.hdf</code> package.
  * 
- * @author <a href=mailto:dale@visser.name>Dale Visser</a>
+ * @author <a href=mailto:dale@visser.name>Dale Visser </a>
  */
 public interface JamHDFFields {
+    
+    /**
+     * The section name for the whole file.
+     */
+    String FILE_SECTION_NAME = "Jam File Section";
 
-    
-     String FILE_SECTION_NAME="Jam File Section";
+    /**
+     * The section name for the histograms.
+     */
+    String HIST_SECTION_NAME = "Histograms";
 
-     String HIST_SECTION_NAME = "Histograms";
-    
-     String HIST_TYPE_NAME = "Histogram";
-    
-     String [] HIST_1D_DIMENSION_LABEL = {"ADC Channel + 1"};
+    /**
+     * The type name for the histograms?
+     */
+    String HIST_TYPE_NAME = "Histogram";
 
-     String [] HIST_2D_DIMENSION_LABEL = {"X Channel + 1","Y Channel + 1"};
-        
-     String GATE_SECTION_NAME = "Gates";
-    
-     String GATE_1D_TYPE_NAME = "1-d Gate";
-    
-     String GATE_2D_TYPE_NAME = "2-d Banana Gate";
-    
-     String [] GATE_1D_NAMES = {"Lower Limit","Upper Limit"};
+    /**
+     * The section name for the gates.
+     */
+    String GATE_SECTION_NAME = "Gates";
 
-     String [] GATE_2D_NAMES = {"X Coordinate","Y Coordinate"};
-    
-     String SCALER_SECTION_NAME = "Scalers";
+    /**
+     * The type name for the 1d gates?
+     */
+    String GATE_1D_TYPE_NAME = "1-d Gate";
 
-     String SCALER_TYPE_NAME = "Scaler Value";
-    
-     String [] SCALER_COLUMN_NAMES = {"Number","Name","Value"};
-    
-     String PARAMETER_SECTION_NAME = "Parameters";
+    /**
+     * The type name for the 2d gates?
+     */
+    String GATE_2D_TYPE_NAME = "2-d Banana Gate";
 
-     String PARAMETER_TYPE_NAME = "Parameter Value";
-    
-     String [] PARAMETER_COLUMN_NAMES = {"Name","Value"};
-    
-     String ERROR_LABEL="Errors";
-    
+    /**
+     * Names for lower and upper limits of 1d gates.
+     */
+    String[] GATE_1D_NAMES = { "Lower Limit", "Upper Limit" };
+
+    /**
+     * Names for x- and y- coordinates lists for 2d gates.
+     */
+    String[] GATE_2D_NAMES = { "X Coordinate", "Y Coordinate" };
+
+    /**
+     * The section name for the scaler values.
+     */
+    String SCALER_SECTION_NAME = "Scalers";
+
+    /**
+     * The type name for the scaler values?
+     */
+    String SCALER_TYPE_NAME = "Scaler Value";
+
+    /** Column names for scaler table. */
+    String[] SCALER_COLUMN_NAMES = { "Number", "Name", "Value" };
+
+    /** The section names for the parameter values. */
+    String PARAMETER_SECTION_NAME = "Parameters";
+
+    /** The type name for the parameter values? */
+    String PARAMETER_TYPE_NAME = "Parameter Value";
+
+    /** The column names for the parameter table. */
+    String[] PARAMETER_COLUMN_NAMES = { "Name", "Value" };
+
+    /** The label for the error bar numerical data groups. */
+    String ERROR_LABEL = "Errors";
+
 }
