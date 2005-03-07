869fd89457129b00608f6a07e2d429c7dee428ad
Dale Visser <dwvisser@users.sourceforge.net>
2005-03-07 16:41:58 +0000

    Now extends AbstractLoaderHDF.

--- jam/commands/AddHDFCmd.java
+++ jam/commands/AddHDFCmd.java
@@ -1,20 +1,19 @@
 package jam.commands;
 
+import jam.data.Group;
 import jam.global.BroadcastEvent;
 import jam.global.SortMode;
 import jam.io.FileOpenMode;
-import jam.data.Group;
 
 import java.io.File;
 import java.util.Observable;
-import java.util.Observer;
 
 /**
  * Add counts to histograms in memory from histograms in an HDF file.
  * 
  * @author Ken Swartz
  */
-final class AddHDFCmd extends LoaderHDF implements Observer {
+final class AddHDFCmd extends AbstractLoaderHDF {
 
 	public void initCommand(){
 		putValue(NAME,"Add counts\u2026");
@@ -22,27 +21,27 @@ final class AddHDFCmd extends LoaderHDF implements Observer {
 	}
 	
 	protected final void execute(final Object[] cmdParams) {
-		File file=null;
-		Group loadGroup =Group.getCurrentGroup();
-		
-		//Parse commad parameters if given
-		if (cmdParams!=null) {
-			if (cmdParams.length>0)
-				file =(File)cmdParams[0];
-			if (cmdParams.length>1)
-				loadGroup=(Group)cmdParams[1];
-		}		
-				
-		loadGroup =Group.getCurrentGroup();
-		loadHDFFile(file, loadGroup);
-	}
+        File file = null;
+        Group loadGroup = Group.getCurrentGroup();
+        //Parse commad parameters if given
+        if (cmdParams != null) {
+            if (cmdParams.length > 0) {
+                file = (File) cmdParams[0];
+            }
+            if (cmdParams.length > 1) {
+                loadGroup = (Group) cmdParams[1];
+            }
+        }
+        loadGroup = Group.getCurrentGroup();
+        loadHDFFile(file, loadGroup);
+    }
 	
-	public void update(Observable observe, Object obj){
-		final BroadcastEvent be=(BroadcastEvent)obj;
-		final BroadcastEvent.Command command=be.getCommand();
-		if (command==BroadcastEvent.Command.SORT_MODE_CHANGED){
-			final SortMode mode=STATUS.getSortMode();
-			setEnabled(mode != SortMode.REMOTE);
-		}
-	}
+	public void update(Observable observe, Object obj) {
+        final BroadcastEvent event = (BroadcastEvent) obj;
+        final BroadcastEvent.Command command = event.getCommand();
+        if (command == BroadcastEvent.Command.SORT_MODE_CHANGED) {
+            final SortMode mode = STATUS.getSortMode();
+            setEnabled(mode != SortMode.REMOTE);
+        }
+    }
 }
