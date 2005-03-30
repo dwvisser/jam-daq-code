efa89a951e0d0fbeab575522c4cc31892fe01f6a
Dale Visser <dwvisser@users.sourceforge.net>
2005-03-30 23:51:50 +0000

    * field names more descriptive * all assertions now include informational messages

--- jam/data/GateTest.java
+++ jam/data/GateTest.java
@@ -7,81 +7,86 @@ import junit.framework.TestCase;
 /**
  * JUnit tests for <code>jam.data.Gate</code>.
  * 
- * @author <a href="mailto:dale@visser.name">Dale Visser</a>
+ * @author <a href="mailto:dale@visser.name">Dale Visser </a>
  * @see Gate
  * @see AllTests
  */
 public class GateTest extends TestCase {
 
-	Gate g1, g2; //1d and 2d, respectively
-	Histogram h1; //1d
-	Histogram h2; //2d
+    Gate gate1, gate2; //1d and 2d, respectively
 
-	Polygon box; //box for 2d gate
-	int[] xpoints = { 10, 50, 50, 10 };
-	int[] ypoints = { 10, 10, 50, 50 };
-	int npoints = 4;
+    Histogram hist1; //1d
 
-	/**
-	 * Constructor for GateTest.
-	 * 
-	 * @param arg0
-	 */
-	public GateTest(String arg0) {
-		super(arg0);
-	}
+    Histogram hist2; //2d
 
-	/**
-	 * Initialize local variables for the tests.
-	 * 
-	 * @see TestCase#setUp()
-	 */
-	protected void setUp() throws Exception {
-		super.setUp();
-		Group group = Group.createGroup("TestGateGroup", Group.Type.FILE);
-		h1 = Histogram.createHistogram(group, new int[100],"h1");
-		h2 = Histogram.createHistogram(group, new int[100][100], "h2");
-		g1 = new Gate("g1", h1);
-		g1.setLimits(10, 50);
-		g2 = new Gate("g2", h2);
-		box = new Polygon(xpoints, ypoints, 4);
-		g2.setLimits(box);
-	}
+    Polygon box; //box for 2d gate
 
-	/**
-	 * Test for boolean inGate(int).
-	 * 
-	 * @see Gate#inGate(int)
-	 */
-	public void testInGateI() {
-		boolean assertion1 = g1.inGate(20);
-		boolean assertion2 = !g1.inGate(5);
-		boolean assertion3 = !g1.inGate(60);
-		assertTrue(assertion1);
-		assertTrue(assertion2);
-		assertTrue(assertion3);
-	}
+    int[] xpoints = { 10, 50, 50, 10 };
 
-	/**
-	 * Test for boolean inGate(int, int).
-	 * 
-	 * @see Gate#inGate(int,int)
-	 */
-	public void testInGateII() {
-		boolean assertion1 = g2.inGate(20, 20);
-		boolean assertion2 = !g2.inGate(5, 20);
-		boolean assertion3 = !g2.inGate(60, 20);
-		boolean assertion4 = !g2.inGate(20, 5);
-		boolean assertion5 = !g2.inGate(5, 5);
-		boolean assertion6 = !g2.inGate(60, 60);
-		boolean assertion7 = !g2.inGate(20, 60);
-		assertTrue(assertion1);
-		assertTrue(assertion2);
-		assertTrue(assertion3);
-		assertTrue(assertion4);
-		assertTrue(assertion5);
-		assertTrue(assertion6);
-		assertTrue(assertion7);
-	}
+    int[] ypoints = { 10, 10, 50, 50 };
+
+    int npoints = 4;
+
+    /**
+     * Constructor for GateTest.
+     * 
+     * @param arg0
+     */
+    public GateTest(String arg0) {
+        super(arg0);
+    }
+
+    /**
+     * Initialize local variables for the tests.
+     * 
+     * @see TestCase#setUp()
+     */
+    protected void setUp() throws Exception {
+        super.setUp();
+        final Group group = Group.createGroup("TestGateGroup", Group.Type.FILE);
+        hist1 = Histogram.createHistogram(group, new int[100], "h1");
+        hist2 = Histogram.createHistogram(group, new int[100][100], "h2");
+        gate1 = new Gate("g1", hist1);
+        gate1.setLimits(10, 50);
+        gate2 = new Gate("g2", hist2);
+        box = new Polygon(xpoints, ypoints, 4);
+        gate2.setLimits(box);
+    }
+
+    /**
+     * Test for boolean inGate(int).
+     * 
+     * @see Gate#inGate(int)
+     */
+    public void testInGateI() {
+        final boolean assertion1 = gate1.inGate(20);
+        final boolean assertion2 = !gate1.inGate(5);
+        final boolean assertion3 = !gate1.inGate(60);
+        assertTrue("20 not in gate.", assertion1);
+        assertTrue("5 in gate.", assertion2);
+        assertTrue("60 in gate.", assertion3);
+    }
+
+    /**
+     * Test for boolean inGate(int, int).
+     * 
+     * @see Gate#inGate(int,int)
+     */
+    public void testInGateII() {
+        final boolean assertion1 = gate2.inGate(20, 20);
+        final boolean assertion2 = !gate2.inGate(5, 20);
+        final boolean assertion3 = !gate2.inGate(60, 20);
+        final boolean assertion4 = !gate2.inGate(20, 5);
+        final boolean assertion5 = !gate2.inGate(5, 5);
+        final boolean assertion6 = !gate2.inGate(60, 60);
+        final boolean assertion7 = !gate2.inGate(20, 60);
+        assertTrue("20,20 not in gate.", assertion1);
+        assertTrue("5,20 in gate.", assertion2);
+        assertTrue("60,20 in gate.", assertion3);
+        assertTrue("20,5 in gate.", assertion4);
+        assertTrue("5,5 in gate.", assertion5);
+        assertTrue("60,60 in gate.", assertion6);
+        assertTrue("20,60 in gate.", assertion7);
+    }
 
 }
