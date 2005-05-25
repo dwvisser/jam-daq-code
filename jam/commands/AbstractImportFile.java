e3a87bbd5ac66b09bd30571e20c2b2fe8841dc97
Dale Visser <dwvisser@users.sourceforge.net>
2005-05-25 17:15:58 +0000

    local 'final' declaration

--- jam/commands/AbstractImportFile.java
+++ jam/commands/AbstractImportFile.java
@@ -8,61 +8,59 @@ import jam.io.ImpExpException;
 import java.io.File;
 
 /**
- * Export data to file. Full implementations must assign an 
- * <code>ImpExp</code> object.
- *
+ * Export data to file. Full implementations must assign an <code>ImpExp</code>
+ * object.
+ * 
  * @author Ken Swartz
  */
 class AbstractImportFile extends AbstractImportExport {
 
-	/**
-	 * Loads the given file, or opens a load dialog if given 
-	 * <code>null</code>.
-	 * 
-	 * @param cmdParams <code>null</code> or 1-element array with a file
-	 * reference
-	 * @see jam.commands.AbstractCommand#execute(java.lang.Object[])
-	 * @see java.io.File
-	 */
-	protected final void execute(Object[] cmdParams) throws CommandException {
-		try {
-			if (cmdParams == null) { //No file given		
-				if (importExport.openFile(null)) {
-					STATUS.setOpenFile(importExport.getLastFile());
-					AbstractControl.setupAll();
-					BROADCASTER.broadcast(BroadcastEvent.Command.HISTOGRAM_ADD);
-				}
-			} else { //File given
-				File file = (File) cmdParams[0];
-				importExport.openFile(file);
-			}
-		} catch (ImpExpException iee) {
-			throw new CommandException(iee.getMessage());
-		}
-	}
+    /**
+     * Loads the given file, or opens a load dialog if given <code>null</code>.
+     * 
+     * @param cmdParams
+     *            <code>null</code> or 1-element array with a file reference
+     * @see jam.commands.AbstractCommand#execute(java.lang.Object[])
+     * @see java.io.File
+     */
+    protected final void execute(Object[] cmdParams) throws CommandException {
+        try {
+            if (cmdParams == null) { //No file given
+                if (importExport.openFile(null)) {
+                    STATUS.setOpenFile(importExport.getLastFile());
+                    AbstractControl.setupAll();
+                    BROADCASTER.broadcast(BroadcastEvent.Command.HISTOGRAM_ADD);
+                }
+            } else { //File given
+                final File file = (File) cmdParams[0];
+                importExport.openFile(file);
+            }
+        } catch (ImpExpException iee) {
+            throw new CommandException(iee.getMessage());
+        }
+    }
 
-	/**
-	 * Loads the given file, or opens a load dialog if given 
-	 * <code>null</code>.
-	 * 
-	 * @param cmdTokens 0- or 1-element array with a file
-	 * reference
-	 * @see jam.commands.AbstractCommand#execute(java.lang.Object[])
-	 * @see java.io.File
-	 */
-	protected final void executeParse(String[] cmdTokens)
-		throws CommandListenerException {
-		try {
-			if (cmdTokens.length == 0) {
-				execute(null);
-			} else {
-				final Object[] cmdParams = new Object[1];
-				final File file = new File(cmdTokens[0]);
-				cmdParams[0] = file;
-				execute(cmdParams);
-			}
-		} catch (CommandException ce) {
-			throw new CommandListenerException(ce.getMessage(),ce);
-		}
-	}
+    /**
+     * Loads the given file, or opens a load dialog if given <code>null</code>.
+     * 
+     * @param cmdTokens
+     *            0- or 1-element array with a file reference
+     * @see jam.commands.AbstractCommand#execute(java.lang.Object[])
+     * @see java.io.File
+     */
+    protected final void executeParse(String[] cmdTokens)
+            throws CommandListenerException {
+        try {
+            if (cmdTokens.length == 0) {
+                execute(null);
+            } else {
+                final Object[] cmdParams = new Object[1];
+                final File file = new File(cmdTokens[0]);
+                cmdParams[0] = file;
+                execute(cmdParams);
+            }
+        } catch (CommandException ce) {
+            throw new CommandListenerException(ce.getMessage(), ce);
+        }
+    }
 }
