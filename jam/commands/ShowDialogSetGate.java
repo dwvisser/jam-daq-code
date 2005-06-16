0f08a30a02f1a9d2d3d50f42811031cb72e30572
Dale Visser <dwvisser@users.sourceforge.net>
2005-06-16 19:08:40 +0000

    removed unused field, gateList

--- jam/commands/ShowDialogSetGate.java
+++ jam/commands/ShowDialogSetGate.java
@@ -1,11 +1,9 @@
 package jam.commands;
 
-import jam.data.Gate;
 import jam.data.Histogram;
 import jam.data.control.GateSet;
 import jam.global.BroadcastEvent;
 
-import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
 
@@ -16,34 +14,29 @@ import javax.swing.Icon;
  * Show the scalers dialog box
  * 
  * @author Ken Swartz
- *
+ * 
  */
-final class ShowDialogSetGate extends AbstractShowDialog implements
-Observer {
+final class ShowDialogSetGate extends AbstractShowDialog implements Observer {
 
-	private final List gateList=Gate.getGateList();
-	
-	public void initCommand(){
-		putValue(NAME,"Set\u2026");
-		dialog=new GateSet();		
-	   final Icon iGateSet = loadToolbarIcon("jam/ui/GateSet.png");
-	   putValue(Action.SMALL_ICON, iGateSet);
-	   putValue(Action.SHORT_DESCRIPTION, "Set Gate.");	    
-		
-
+	public void initCommand() {
+		putValue(NAME, "Set\u2026");
+		dialog = new GateSet();
+		final Icon iGateSet = loadToolbarIcon("jam/ui/GateSet.png");
+		putValue(Action.SMALL_ICON, iGateSet);
+		putValue(Action.SHORT_DESCRIPTION, "Set Gate.");
 	}
 
-	public void update(Observable observe, Object obj){
-		final BroadcastEvent be=(BroadcastEvent)obj;
-		final BroadcastEvent.Command command=be.getCommand();
-		if ( (command==BroadcastEvent.Command.GROUP_SELECT) || 
-			 (command==BroadcastEvent.Command.ROOT_SELECT) ) {
-			setEnabled(false);			
-		} else if ( (command==BroadcastEvent.Command.HISTOGRAM_SELECT) || 
-				    (command==BroadcastEvent.Command.GATE_SELECT) ) {
+	public void update(Observable observe, Object obj) {
+		final BroadcastEvent be = (BroadcastEvent) obj;
+		final BroadcastEvent.Command command = be.getCommand();
+		if ((command == BroadcastEvent.Command.GROUP_SELECT)
+				|| (command == BroadcastEvent.Command.ROOT_SELECT)) {
+			setEnabled(false);
+		} else if ((command == BroadcastEvent.Command.HISTOGRAM_SELECT)
+				|| (command == BroadcastEvent.Command.GATE_SELECT)) {
 			Histogram hist = STATUS.getCurrentHistogram();
-			if (hist!=null)
+			if (hist != null)
 				setEnabled(!hist.getGates().isEmpty());
 		}
-	}		
+	}
 }
