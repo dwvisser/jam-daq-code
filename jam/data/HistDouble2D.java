9f5e7bca109f766a68b3c091ebef637bd69a26cd
Dale Visser <dwvisser@users.sourceforge.net>
2005-06-17 12:47:05 +0000

    javadoc fixes

--- jam/data/HistDouble2D.java
+++ jam/data/HistDouble2D.java
@@ -5,145 +5,162 @@ package jam.data;
 
 import java.util.Arrays;
 
-
 /**
  * @author <a href="mailto:dale@visser.name">Dale W Visser</a>
  */
 final class HistDouble2D extends AbstractHist2D {
-	
-	/* array to hold counds for 2d double */
-	private transient double[][] counts2dD;
 
-	/**
-	 * Create a new 2-d histogram with counts known and automatically give it a
-	 * number
-	 * 
-	 * @param name
-	 *            unique name of histogram, should be limited to
-	 *            <code>NAME_LENGTH</code> characters, used in both .jhf and
-	 *            .hdf files as the unique identifier for reloading the
-	 *            histogram
-	 * @param title
-	 *            lengthier title of histogram, displayed on plot
-	 * @param countsIn
-	 *            array of counts to initialize with, must be square
-	 */
-	HistDouble2D(Group group, String name, String title, double[][] countsIn) {
-		super(group, name, Type.TWO_D_DOUBLE, countsIn.length, countsIn[0].length,
-				title);
-		initCounts(countsIn);
-	}
-	
-	HistDouble2D(Group group, String name, String title, String axisLabelX, String axisLabelY,
-			double[][] countsIn) {
-		super(group, name, Type.TWO_D_DOUBLE, countsIn.length, countsIn[0].length,
-				title, axisLabelX, axisLabelY);
-		initCounts(countsIn);
-	}
+    /* array to hold counds for 2d double */
+    private transient double[][] counts2dD;
 
-	private void initCounts(double [][] countsIn){
-		counts2dD=new double[getSizeX()][getSizeY()];
-		for (int i = 0; i < countsIn.length; i++) { //copy arrays
-			System.arraycopy(countsIn[i], 0, counts2dD[i], 0,
-					countsIn[0].length);
-		}
-	}
+    /**
+     * Create a new 2-d histogram with counts known and automatically give it a
+     * number
+     * 
+     * @param name
+     *            unique name of histogram, should be limited to
+     *            <code>NAME_LENGTH</code> characters, used in both .jhf and
+     *            .hdf files as the unique identifier for reloading the
+     *            histogram
+     * @param title
+     *            lengthier title of histogram, displayed on plot
+     * @param countsIn
+     *            array of counts to initialize with, must be square
+     * @param group
+     *            that this histogram belongs to
+     */
+    HistDouble2D(Group group, String name, String title, double[][] countsIn) {
+        super(group, name, Type.TWO_D_DOUBLE, countsIn.length,
+                countsIn[0].length, title);
+        initCounts(countsIn);
+    }
 
-	/* (non-Javadoc)
-	 * @see jam.data.AbstractHist2D#getCounts(int, int)
-	 */
-	public double getCounts(int chX, int chY) {
-		return counts2dD[chX][chY];
-	}
+    HistDouble2D(Group group, String name, String title, String axisLabelX,
+            String axisLabelY, double[][] countsIn) {
+        super(group, name, Type.TWO_D_DOUBLE, countsIn.length,
+                countsIn[0].length, title, axisLabelX, axisLabelY);
+        initCounts(countsIn);
+    }
 
-	/* (non-Javadoc)
-	 * @see jam.data.AbstractHist2D#setCounts(int, int, double)
-	 */
-	public void setCounts(int chX, int chY, double counts) {
-		counts2dD[chX][chY]=counts;
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see jam.data.Histogram#addCounts(java.lang.Object)
+     */
+    public void addCounts(Object countsIn) {
+        if (Type.getArrayType(countsIn) != getType()) {
+            throw new IllegalArgumentException("Expected array for type "
+                    + getType());
+        }
+        addCountsArray((double[][]) countsIn);
+    }
 
-	/* (non-Javadoc)
-	 * @see jam.data.Histogram#clearCounts()
-	 */
-	void clearCounts() {
-		counts2dD=null;
-	}
+    private synchronized void addCountsArray(final double[][] countsIn) {
+        final int maxX = Math.min(getSizeX(), countsIn.length) - 1;
+        final int maxY = Math.min(getSizeY(), countsIn[0].length) - 1;
+        for (int x = maxX; x >= 0; x--) {
+            for (int y = maxY; y >= 0; y--) {
+                counts2dD[x][y] += countsIn[x][y];
+            }
+        }
+    }
 
-	/* (non-Javadoc)
-	 * @see jam.data.Histogram#getCounts()
-	 */
-	public Object getCounts() {
-		return counts2dD;
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see jam.data.Histogram#clearCounts()
+     */
+    void clearCounts() {
+        counts2dD = null;
+    }
 
-	public double getCount() {
-		return getArea();
-	}
-	/* (non-Javadoc)
-	 * @see jam.data.Histogram#setZero()
-	 */
-	public void setZero() {
-		final int size=getSizeX();
-		for (int i = 0; i < size; i++) {
-			Arrays.fill(counts2dD[i],0);
-		}
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see jam.data.Histogram#getArea()
+     */
+    public double getArea() {
+        final int size = getSizeX();
+        double sum = 0.0;
+        for (int i = 0; i < size; i++) {
+            final int sizeY = getSizeY();
+            for (int j = 0; j < sizeY; j++) {
+                sum += counts2dD[i][j];
+            }
+        }
+        return sum;
+    }
 
-	/* (non-Javadoc)
-	 * @see jam.data.Histogram#setCounts(java.lang.Object)
-	 */
-	public void setCounts(Object countsIn) {
-		if (Type.getArrayType(countsIn) != getType()) {
-			throw new IllegalArgumentException("Expected array for type "
-					+ getType());
-		}
-		setCountsArray((double[][]) countsIn);
-	}
+    public double getCount() {
+        return getArea();
+    }
 
-	/* (non-Javadoc)
-	 * @see jam.data.Histogram#addCounts(java.lang.Object)
-	 */
-	public void addCounts(Object countsIn) {
-		if (Type.getArrayType(countsIn) != getType()) {
-			throw new IllegalArgumentException("Expected array for type "
-					+ getType());
-		}
-		addCountsArray((double[][]) countsIn);
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see jam.data.Histogram#getCounts()
+     */
+    public Object getCounts() {
+        return counts2dD;
+    }
 
-	private synchronized void addCountsArray(final double[][] countsIn) {
-		final int maxX = Math.min(getSizeX(), countsIn.length) - 1;
-		final int maxY = Math.min(getSizeY(), countsIn[0].length) - 1;
-		for (int x = maxX; x >= 0; x--) {
-			for (int y = maxY; y >= 0; y--) {
-				counts2dD[x][y] += countsIn[x][y];
-			}
-		}
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see jam.data.AbstractHist2D#getCounts(int, int)
+     */
+    public double getCounts(int chX, int chY) {
+        return counts2dD[chX][chY];
+    }
 
-	/* (non-Javadoc)
-	 * @see jam.data.Histogram#getArea()
-	 */
-	public double getArea() {
-		final int size=getSizeX();
-		double sum=0.0;
-		for (int i = 0; i < size; i++) {
-			final int sizeY=getSizeY();
-			for (int j = 0; j < sizeY; j++) {
-				sum += counts2dD[i][j];
-			}
-		}
-		return sum;
-	}
-	
-	private synchronized void setCountsArray(double[][] countsIn) {
-		final int loopLen = Math.min(countsIn.length, counts2dD.length);
-		for (int i = 0; i < loopLen; i++) {
-			System.arraycopy(countsIn[i], 0, counts2dD[i], 0, Math.min(
-					countsIn[i].length, counts2dD[i].length));
-		}
-	}
+    private void initCounts(double[][] countsIn) {
+        counts2dD = new double[getSizeX()][getSizeY()];
+        for (int i = 0; i < countsIn.length; i++) { // copy arrays
+            System.arraycopy(countsIn[i], 0, counts2dD[i], 0,
+                    countsIn[0].length);
+        }
+    }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see jam.data.AbstractHist2D#setCounts(int, int, double)
+     */
+    public void setCounts(int chX, int chY, double counts) {
+        counts2dD[chX][chY] = counts;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see jam.data.Histogram#setCounts(java.lang.Object)
+     */
+    public void setCounts(Object countsIn) {
+        if (Type.getArrayType(countsIn) != getType()) {
+            throw new IllegalArgumentException("Expected array for type "
+                    + getType());
+        }
+        setCountsArray((double[][]) countsIn);
+    }
+
+    private synchronized void setCountsArray(double[][] countsIn) {
+        final int loopLen = Math.min(countsIn.length, counts2dD.length);
+        for (int i = 0; i < loopLen; i++) {
+            System.arraycopy(countsIn[i], 0, counts2dD[i], 0, Math.min(
+                    countsIn[i].length, counts2dD[i].length));
+        }
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see jam.data.Histogram#setZero()
+     */
+    public void setZero() {
+        final int size = getSizeX();
+        for (int i = 0; i < size; i++) {
+            Arrays.fill(counts2dD[i], 0);
+        }
+    }
 
 }
--- jam/data/HistInt1D.java
+++ jam/data/HistInt1D.java
@@ -5,213 +5,220 @@ package jam.data;
 
 import java.util.Arrays;
 
-
-
 /**
  * The 1-dimensional histogram class to use for online and offline sorting.
  * 
  * @author <a href="mailto:dale@visser.name">Dale W Visser </a>
  */
 public final class HistInt1D extends AbstractHist1D {
-	private static final int [] EMPTY_INT=new int[0];
+    private static final int[] EMPTY_INT = new int[0];
 
-	private int counts[]; // array to hold counts for 1d int
+    private int counts[]; // array to hold counts for 1d int
 
-	/**
-	 * Create a new 1-d <code>Histogram</code> with the counts known and with
-	 * axes labeled.
-	 * 
-	 * @param name
-	 *            unique name of histogram, should be limited to
-	 *            <code>NAME_LENGTH</code> characters, used in both .jhf and
-	 *            .hdf files as the unique identifier for reloading the
-	 *            histogram
-	 * @param title
-	 *            lengthier title of histogram, displayed on plot
-	 * @param axisLabelX
-	 *            label displayed for x-axis on plot
-	 * @param axisLabelY
-	 *            label displayed for y-axis on plot
-	 * @param countsIn
-	 *            array of counts to initialize with
-	 */
-	HistInt1D(Group group, String name, String title, String axisLabelX, String axisLabelY,
-			int[] countsIn) {
-		super(group, name, Type.ONE_DIM_INT, countsIn.length, title, axisLabelX,
-				axisLabelY);
-		initCounts(countsIn);
-	}
+    /**
+     * Given an array of counts, create a new 1-d <code>Histogram</code> and
+     * give it a number.
+     * 
+     * @param name
+     *            unique name of histogram, should be limited to
+     *            <code>NAME_LENGTH</code> characters, used in both .jhf and
+     *            .hdf files as the unique identifier for reloading the
+     *            histogram
+     * @param title
+     *            lengthier title of histogram, displayed on plot
+     * @param countsIn
+     *            array of counts to initialize with
+     * @param group
+     *            that this histogram belongs to
+     */
+    HistInt1D(Group group, String name, String title, int[] countsIn) {
+        super(group, name, Type.ONE_DIM_INT, countsIn.length, title);
+        initCounts(countsIn);
+    }
 
-	/**
-	 * Given an array of counts, create a new 1-d <code>Histogram</code> and
-	 * give it a number.
-	 * 
-	 * @param name
-	 *            unique name of histogram, should be limited to
-	 *            <code>NAME_LENGTH</code> characters, used in both .jhf and
-	 *            .hdf files as the unique identifier for reloading the
-	 *            histogram
-	 * @param title
-	 *            lengthier title of histogram, displayed on plot
-	 * @param countsIn
-	 *            array of counts to initialize with
-	 */
-	HistInt1D(Group group, String name, String title, int[] countsIn) {
-		super(group, name, Type.ONE_DIM_INT, countsIn.length, title);
-		initCounts(countsIn);
-	}
-	
-	private void initCounts(int [] countsIn){
-		counts=new int[getSizeX()];
-		System.arraycopy(countsIn, 0, counts, 0, countsIn.length);		
-	}
+    /**
+     * Create a new 1-d <code>Histogram</code> with the counts known and with
+     * axes labeled.
+     * 
+     * @param name
+     *            unique name of histogram, should be limited to
+     *            <code>NAME_LENGTH</code> characters, used in both .jhf and
+     *            .hdf files as the unique identifier for reloading the
+     *            histogram
+     * @param title
+     *            lengthier title of histogram, displayed on plot
+     * @param axisLabelX
+     *            label displayed for x-axis on plot
+     * @param axisLabelY
+     *            label displayed for y-axis on plot
+     * @param countsIn
+     *            array of counts to initialize with
+     * @param group
+     *            that this histogram belongs to
+     */
+    HistInt1D(Group group, String name, String title, String axisLabelX,
+            String axisLabelY, int[] countsIn) {
+        super(group, name, Type.ONE_DIM_INT, countsIn.length, title,
+                axisLabelX, axisLabelY);
+        initCounts(countsIn);
+    }
 
-	/**
-	 * Returns the counts in the histogram as an array of the appropriate type.
-	 * It is necessary to cast the returned array with <code>(int [])</code>.
-	 * 
-	 * @return <code>int []</code>
-	 */
-	public synchronized Object getCounts() {
-		return counts;
-	}
+    /**
+     * Adds the given counts to this histogram.
+     * 
+     * @param add
+     *            1d array of <code>int</code>'s
+     * @throws IllegalArgumentException
+     *             if the parameter is the wrong type
+     */
+    public synchronized void addCounts(Object add) {
+        if (Type.getArrayType(add) != getType()) {
+            throw new IllegalArgumentException("Expected array for type "
+                    + getType());
+        }
+        addCountsArray((int[]) add);
+    }
 
-	public double getCount() {
-		return getArea();
-	}
-	
-	/**
-	 * Returns the number of counts in the given channel.
-	 * 
-	 * @param channel that we are interested in
-	 * @return number of counts
-	 */
-	public synchronized double getCounts(int channel) {
-		return counts[channel];
-	}
-
-	/**
-	 * Zeroes all the counts in this histogram.
-	 */
-	public synchronized void setZero() {
-		Arrays.fill(counts,0);
-		unsetErrors();
-	}
-
-	/**
-	 * Set the counts array using the given <code>int []</code>.
-	 * 
-	 * @param countsIn
-	 *            <code>int []</code>
-	 * @throws IllegalArgumentException
-	 *             if countsIn is the wrong type.
-	 */
-	public synchronized void setCounts(Object countsIn) {
-		if (Type.getArrayType(countsIn)!=getType()){
-			throw new IllegalArgumentException("Expected array for type "+getType());
-		}
-		final int inLength = ((int[]) countsIn).length;
-		System.arraycopy(countsIn, 0, counts, 0, Math.min(inLength, getSizeX()));
-	}
-
-	/**
-	 * Sets the counts in the given channel to the specified
-	 * number of counts.
-	 * 
-	 * @param channel to change
-	 * @param count to be in the channel, rounded to <code>int</code>, if
-	 * necessary
-	 */
-	public synchronized void setCounts(int channel, double count) {
-		counts[channel] = (int) Math.round(count);
-	}
-
-	private synchronized void addCountsArray(int[] countsIn) {
+    private synchronized void addCountsArray(int[] countsIn) {
         final int max = Math.min(countsIn.length, getSizeX()) - 1;
         for (int i = max; i >= 0; i--) {
             counts[i] += countsIn[i];
         }
     }
 
+    synchronized void clearCounts() {
+        counts = EMPTY_INT;
+        unsetErrors();
+        setCalibration(null);
+    }
 
-	/**
-	 * Increments the counts by one in the given channel. Must be a histogram of
-	 * type <code>ONE_DIM_INT</code>.
-	 * 
-	 * @param dataWord
-	 *            the channel to be incremented
-	 * @exception UnsupportedOperationException
-	 *                thrown if method called for inappropriate type of
-	 *                histogram
-	 */
-	public void inc(int dataWord) {
-		final int size=getSizeX();
-		int incCh = dataWord;
-		/* check for overflow */
-		if (incCh >= size) {
-			incCh = size - 1;
-		} else if (dataWord < 0) {
-			incCh = 0;
-		}
-		synchronized (this) {
-			counts[incCh]++;
-		}
-	}
+    /**
+     * Returns the total number of counts in the histogram.
+     * 
+     * @return area under the counts in the histogram
+     */
+    public synchronized double getArea() {
+        final int size = getSizeX();
+        double sum = 0.0;
+        for (int i = 0; i < size; i++) {
+            sum += counts[i];
+        }
+        return sum;
+    }
 
-	/**
-	 * Returns the array of error bars, possibly estimated.
-	 *  
-	 * @return 1-sigma error bars
-	 */
-	public synchronized double[] getErrors() {
-		final int length = counts.length;
-		if (errors == null) {
-			errors = new double[length];
-			for (int i = 0; i < length; i++) {
-				if (counts[i] == 0) {
-					/* set errors according to Poisson with error = 1 */
-					errors[i] = 1.0;
-				} else {
-					errors[i] = Math.sqrt(counts[i]);
-				}
-			}
-		}
-		return errors;
-	}
+    public double getCount() {
+        return getArea();
+    }
 
-	/**
-	 * Returns the total number of counts in the histogram.
-	 * 
-	 * @return area under the counts in the histogram
-	 */
-	public synchronized double getArea() {
-		final int size=getSizeX();
-		double sum = 0.0;
-		for (int i = 0; i < size; i++) {
-			sum += counts[i];
-		}
-		return sum;
-	}
-	
-	/**
-	 * Adds the given counts to this histogram.
-	 * 
-	 * @param add
-	 *            1d array of <code>int</code>'s
-	 * @throws IllegalArgumentException
-	 *             if the parameter is the wrong type
-	 */
-	public synchronized void addCounts(Object add){
-		if (Type.getArrayType(add)!=getType()){
-			throw new IllegalArgumentException("Expected array for type "+getType());
-		}
-		addCountsArray((int[]) add);
-	}
-	
-	synchronized void clearCounts(){
-		counts=EMPTY_INT;
-		unsetErrors();
-		setCalibration(null);
-	}
+    /**
+     * Returns the counts in the histogram as an array of the appropriate type.
+     * It is necessary to cast the returned array with <code>(int [])</code>.
+     * 
+     * @return <code>int []</code>
+     */
+    public synchronized Object getCounts() {
+        return counts;
+    }
+
+    /**
+     * Returns the number of counts in the given channel.
+     * 
+     * @param channel
+     *            that we are interested in
+     * @return number of counts
+     */
+    public synchronized double getCounts(int channel) {
+        return counts[channel];
+    }
+
+    /**
+     * Returns the array of error bars, possibly estimated.
+     * 
+     * @return 1-sigma error bars
+     */
+    public synchronized double[] getErrors() {
+        final int length = counts.length;
+        if (errors == null) {
+            errors = new double[length];
+            for (int i = 0; i < length; i++) {
+                if (counts[i] == 0) {
+                    /* set errors according to Poisson with error = 1 */
+                    errors[i] = 1.0;
+                } else {
+                    errors[i] = Math.sqrt(counts[i]);
+                }
+            }
+        }
+        return errors;
+    }
+
+    /**
+     * Increments the counts by one in the given channel. Must be a histogram of
+     * type <code>ONE_DIM_INT</code>.
+     * 
+     * @param dataWord
+     *            the channel to be incremented
+     * @exception UnsupportedOperationException
+     *                thrown if method called for inappropriate type of
+     *                histogram
+     */
+    public void inc(int dataWord) {
+        final int size = getSizeX();
+        int incCh = dataWord;
+        /* check for overflow */
+        if (incCh >= size) {
+            incCh = size - 1;
+        } else if (dataWord < 0) {
+            incCh = 0;
+        }
+        synchronized (this) {
+            counts[incCh]++;
+        }
+    }
+
+    private void initCounts(int[] countsIn) {
+        counts = new int[getSizeX()];
+        System.arraycopy(countsIn, 0, counts, 0, countsIn.length);
+    }
+
+    /**
+     * Sets the counts in the given channel to the specified number of counts.
+     * 
+     * @param channel
+     *            to change
+     * @param count
+     *            to be in the channel, rounded to <code>int</code>, if
+     *            necessary
+     */
+    public synchronized void setCounts(int channel, double count) {
+        counts[channel] = (int) Math.round(count);
+    }
+
+    /**
+     * Set the counts array using the given <code>int []</code>.
+     * 
+     * @param countsIn
+     *            <code>int []</code>
+     * @throws IllegalArgumentException
+     *             if countsIn is the wrong type.
+     */
+    public synchronized void setCounts(Object countsIn) {
+        if (Type.getArrayType(countsIn) != getType()) {
+            throw new IllegalArgumentException("Expected array for type "
+                    + getType());
+        }
+        final int inLength = ((int[]) countsIn).length;
+        System
+                .arraycopy(countsIn, 0, counts, 0, Math.min(inLength,
+                        getSizeX()));
+    }
+
+    /**
+     * Zeroes all the counts in this histogram.
+     */
+    public synchronized void setZero() {
+        Arrays.fill(counts, 0);
+        unsetErrors();
+    }
 
 }
