e36698c27bdf5ff6ae38cd4a379fe502abc5ab4d
Dale Visser <dwvisser@users.sourceforge.net>
2005-06-23 11:33:20 +0000

    type parameter javadocs

--- jam/util/CollectionsUtil.java
+++ jam/util/CollectionsUtil.java
@@ -27,12 +27,14 @@ public final class CollectionsUtil {
     /**
      * Used for conditional add of elements in a collection.
      * @author <a href="mailto:dale@visser.name">Dale W Visser</a>
+     * @param <T> type of element in the collection
      */
     public interface Condition<T> {
         
         /**
          * Gives whether to accept an object or not.
          * @param object to decide about
+         * @param <T> type of element in the collection
          * @return <code>true</code> if acceptable
          */
         <T> boolean accept(T object);
@@ -44,6 +46,7 @@ public final class CollectionsUtil {
      * @param source of objects to add
      * @param destination destination to add to
      * @param condition makes decision whether to add an item
+     * @param <T> element type of the collections
      */
     public <T> void addConditional(Collection<T> source, Collection<T> destination,
             Condition<T> condition) {
