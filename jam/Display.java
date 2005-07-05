c1f34ccb19cf1836c64e67e2564a26d3a4889ff4
Dale Visser <dwvisser@users.sourceforge.net>
2005-07-05 11:50:43 +0000

    * renamed layout field * refactored update() method

--- jam/Display.java
+++ jam/Display.java
@@ -2,6 +2,7 @@ package jam;
 
 import jam.global.BroadcastEvent;
 import jam.global.Broadcaster;
+import jam.global.BroadcastEvent.Command;
 
 import java.awt.CardLayout;
 import java.util.Observable;
@@ -13,56 +14,60 @@ import javax.swing.JPanel;
  * Display to show plots or table
  * 
  * @author Ken Swartz
- *
+ * 
  */
-public class Display extends JPanel implements Observer{
+public class Display extends JPanel implements Observer {
 
-		private static final String KEY_PLOT="plot";
-		
-		private static final String KEY_TABLE="table";
-		
-		private transient final Broadcaster broadcaster = Broadcaster.getSingletonInstance();
-		
-		private final CardLayout displaySwapPanelLayout;
-	
-        /**
-         * Constructor.
-         * 
-         * @param plotDisplay plot panel
-         * @param summaryTable summary panel
-         */
-		public Display(JPanel plotDisplay, JPanel summaryTable) {
-			displaySwapPanelLayout = new CardLayout();
-			setLayout(displaySwapPanelLayout);
-			add(KEY_PLOT, plotDisplay);
-			add(KEY_TABLE, summaryTable);
-			/* Initial show plot. */
-			displaySwapPanelLayout.show(this, KEY_PLOT);
-			broadcaster.addObserver(this);
-		}
-        
-		/**
-		 * Show plots in the main panel
-		 */
-		public void showPlot() {
-			displaySwapPanelLayout.show(this, KEY_PLOT);			
-		}
-		/**
-		 * Show a table in the main panel
-		 */		
-		public void showTable() {
-			displaySwapPanelLayout.show(this, KEY_TABLE);
-		}
-		public void update(Observable observable, Object o) {
-			BroadcastEvent be = (BroadcastEvent) o;
-			if	( (be.getCommand() == BroadcastEvent.Command.GROUP_SELECT) ||
-			      (be.getCommand() == BroadcastEvent.Command.ROOT_SELECT) ){
-				showTable();
-			}else if ( (be.getCommand() == BroadcastEvent.Command.HISTOGRAM_SELECT)||
-					   (be.getCommand() == BroadcastEvent.Command.GATE_SELECT) ){
-				showPlot();
-			}
-			
-		}
-		
+    private static final String KEY_PLOT = "plot";
+
+    private static final String KEY_TABLE = "table";
+
+    private transient final Broadcaster broadcaster = Broadcaster
+            .getSingletonInstance();
+
+    private transient final CardLayout cardLayout;
+
+    /**
+     * Constructor.
+     * 
+     * @param plotDisplay
+     *            plot panel
+     * @param summaryTable
+     *            summary panel
+     */
+    public Display(JPanel plotDisplay, JPanel summaryTable) {
+        cardLayout = new CardLayout();
+        setLayout(cardLayout);
+        add(KEY_PLOT, plotDisplay);
+        add(KEY_TABLE, summaryTable);
+        /* Initial show plot. */
+        cardLayout.show(this, KEY_PLOT);
+        broadcaster.addObserver(this);
+    }
+
+    /**
+     * Show plots in the main panel
+     */
+    public void showPlot() {
+        cardLayout.show(this, KEY_PLOT);
+    }
+
+    /**
+     * Show a table in the main panel
+     */
+    public void showTable() {
+        cardLayout.show(this, KEY_TABLE);
+    }
+
+    public void update(Observable observable, Object object) {
+        final BroadcastEvent event = (BroadcastEvent) object;
+        final Command command = event.getCommand();
+        if ((command == BroadcastEvent.Command.GROUP_SELECT)
+                || (command == BroadcastEvent.Command.ROOT_SELECT)) {
+            showTable();
+        } else if ((command == BroadcastEvent.Command.HISTOGRAM_SELECT)
+                || (command == BroadcastEvent.Command.GATE_SELECT)) {
+            showPlot();
+        }
+    }
 }
