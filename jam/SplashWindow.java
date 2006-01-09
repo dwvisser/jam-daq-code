8dc90c65b6ad7f2c26a23fc6122c38b02a6aa722
Dale Visser <dwvisser@users.sourceforge.net>
2006-01-09 22:00:53 +0000

    added logger call

--- jam/SplashWindow.java
+++ jam/SplashWindow.java
@@ -1,4 +1,5 @@
 package jam;
+
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Container;
@@ -11,6 +12,8 @@ import java.awt.Toolkit;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
@@ -22,32 +25,40 @@ import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
 
 /**
- * Generates the "splash" window that displays while Jam
- * is launching.  The window disappears after a specified
- * timeout period or when the user clicks on it.
+ * Generates the "splash" window that displays while Jam is launching. The
+ * window disappears after a specified timeout period or when the user clicks on
+ * it.
  */
 class SplashWindow extends JWindow {
 
 	private transient final URL urlNukeIcon;
+
 	private transient final URL urlOSIGif;
+
+	private static final Logger LOGGER = Logger.getLogger("jam");
+
 	/**
-	 * Creates the splash window which will exist for as long
-	 * as the specified wait time in milliseconds.
-	 *
-	 * @param frame parent frame
-	 * @param waitTime time in milliseconds after which the window disappears
+	 * Creates the splash window which will exist for as long as the specified
+	 * wait time in milliseconds.
+	 * 
+	 * @param frame
+	 *            parent frame
+	 * @param waitTime
+	 *            time in milliseconds after which the window disappears
 	 */
 	public SplashWindow(Frame frame, int waitTime) {
 		super(frame);
 		ClassLoader classLoader = getClass().getClassLoader();
-		urlNukeIcon= classLoader.getResource("jam/nukeicon.png");
-		if (urlNukeIcon==null) {
-			JOptionPane.showMessageDialog(frame, "Can't load resource: jam/nukeicon.png");
+		urlNukeIcon = classLoader.getResource("jam/nukeicon.png");
+		if (urlNukeIcon == null) {
+			JOptionPane.showMessageDialog(frame,
+					"Can't load resource: jam/nukeicon.png");
 			System.exit(0);
 		}
 		urlOSIGif = classLoader.getResource("jam/OSI.png");
-		if (urlOSIGif==null) {
-			JOptionPane.showMessageDialog(frame, "Can't load resource: jam/OSI.gif");
+		if (urlOSIGif == null) {
+			JOptionPane.showMessageDialog(frame,
+					"Can't load resource: jam/OSI.gif");
 			System.exit(0);
 		}
 		drawWindow();
@@ -70,7 +81,11 @@ class SplashWindow extends JWindow {
 					Thread.sleep(pause);
 					SwingUtilities.invokeAndWait(closerRunner);
 				} catch (Exception e) {
-					e.printStackTrace();
+					LOGGER
+							.log(
+									Level.SEVERE,
+									"Exception occured trying to display splash window.",
+									e);
 					// can catch InvocationTargetException
 					// can catch InterruptedException
 				}
@@ -84,52 +99,60 @@ class SplashWindow extends JWindow {
 	private void drawWindow() {
 		final Container contents = getContentPane();
 		final JPanel west = new JPanel(new FlowLayout());
-		west.setBackground(Color.white);
-		west.setBorder(BorderFactory.createMatteBorder(1,1,0,0,Color.black));
+		west.setBackground(Color.WHITE);
+		west
+				.setBorder(BorderFactory.createMatteBorder(1, 1, 0, 0,
+						Color.BLACK));
 
-		final ImageIcon nukeicon=new ImageIcon(urlNukeIcon);
-		final int sizexy=80;
-		nukeicon.setImage(nukeicon.getImage().getScaledInstance(
-		sizexy,sizexy,Image.SCALE_SMOOTH));
+		final ImageIcon nukeicon = new ImageIcon(urlNukeIcon);
+		final int sizexy = 80;
+		nukeicon.setImage(nukeicon.getImage().getScaledInstance(sizexy, sizexy,
+				Image.SCALE_SMOOTH));
 		west.add(new JLabel(nukeicon));
-		final JPanel center = new JPanel(new GridLayout(0,1));
-		center.setBackground(Color.white);
-		center.setBorder(BorderFactory.createMatteBorder(1,0,0,0,Color.black));
-		center.add(new JLabel(
-				"Jam: Data Acquisition for Nuclear Physics"));
+		final JPanel center = new JPanel(new GridLayout(0, 1));
+		center.setBackground(Color.WHITE);
+		center.setBorder(BorderFactory.createMatteBorder(1, 0, 0, 0,
+				Color.BLACK));
+		center.add(new JLabel("Jam: Data Acquisition for Nuclear Physics"));
 		center.add(new JLabel("\u00a9 2002 Yale University"));
-		center.add(new JLabel("University of Illinois/NCSA Open Source License"));
+		center
+				.add(new JLabel(
+						"University of Illinois/NCSA Open Source License"));
 
 		center.add(new JLabel("See Help|License... for license text."));
-		final JPanel east=new JPanel(new FlowLayout());
-		east.setBackground(Color.white);
-		east.setBorder(BorderFactory.createMatteBorder(1,0,0,1,Color.black));
-		final JLabel osi = new JLabel(
-		new ImageIcon(urlOSIGif));
-		osi.setToolTipText("Open Source Initiative. See http://www.opensource.org/");
+		final JPanel east = new JPanel(new FlowLayout());
+		east.setBackground(Color.WHITE);
+		east
+				.setBorder(BorderFactory.createMatteBorder(1, 0, 0, 1,
+						Color.BLACK));
+		final JLabel osi = new JLabel(new ImageIcon(urlOSIGif));
+		osi
+				.setToolTipText("Open Source Initiative. See http://www.opensource.org/");
 		east.add(osi);
 		final JPanel panelSouth = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-		final JLabel versionLabel = new JLabel("v. " + Version.getInstance().getName(),SwingConstants.RIGHT);
+		final JLabel versionLabel = new JLabel("v. "
+				+ Version.getInstance().getName(), SwingConstants.RIGHT);
 		panelSouth.add(versionLabel);
-		panelSouth.setBackground(Color.cyan);
-		panelSouth.setBorder(
-			BorderFactory.createMatteBorder(0, 1, 1, 1, Color.black));
+		panelSouth.setBackground(Color.CYAN);
+		panelSouth.setBorder(BorderFactory.createMatteBorder(0, 1, 1, 1,
+				Color.BLACK));
 		contents.add(panelSouth, BorderLayout.SOUTH);
-		contents.add(west,BorderLayout.WEST);
-		contents.add(center,BorderLayout.CENTER);
-		contents.add(east,BorderLayout.EAST);
+		contents.add(west, BorderLayout.WEST);
+		contents.add(center, BorderLayout.CENTER);
+		contents.add(east, BorderLayout.EAST);
 		pack();
-		final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		final Dimension screenSize = Toolkit.getDefaultToolkit()
+				.getScreenSize();
 		final Dimension labelSize = getSize();
-		setLocation(
-			screenSize.width / 2 - (labelSize.width / 2),
-			screenSize.height / 2 - (labelSize.height / 2));
+		setLocation(screenSize.width / 2 - (labelSize.width / 2),
+				screenSize.height / 2 - (labelSize.height / 2));
 	}
 
 	/**
 	 * Displays this window for up to a minute.
 	 * 
-	 * @param args ignored
+	 * @param args
+	 *            ignored
 	 */
 	static public void main(final String[] args) {
 		new SplashWindow(null, 60000);
