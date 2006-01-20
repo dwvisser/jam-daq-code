6a0311572b3258a50f89c0d42b8f04d32f921a0b
Dale Visser <dwvisser@users.sourceforge.net>
2006-01-20 11:50:09 +0000

    * packet types from interface now * System.out replaced by logger calls * PMD warnings eliminated

--- jam/simulator/SimulateFrontEnd.java
+++ jam/simulator/SimulateFrontEnd.java
@@ -1,13 +1,8 @@
 package jam.simulator;
 
-
-import jam.data.Scaler;
-import jam.global.BroadcastEvent;
+import jam.PacketTypes;
 import jam.global.GoodThread;
 
-import java.io.ByteArrayInputStream;
-import java.io.DataInput;
-import java.io.DataInputStream;
 import java.io.IOException;
 import java.net.BindException;
 import java.net.DatagramPacket;
@@ -16,165 +11,123 @@ import java.net.InetAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.nio.ByteBuffer;
-import java.util.logging.Level;
-
-import javax.swing.JOptionPane;
 
 /**
- * Simulator of front end
- *  start up with "java jam.simulator.SimulateFrontEnd"
+ * Simulator of front end start up with "java jam.simulator.SimulateFrontEnd"
  * 
  * @author Ken Swartz
  */
-public final class SimulateFrontEnd extends GoodThread {
-	
+public final class SimulateFrontEnd extends GoodThread implements PacketTypes {
+
 	private static final int MAX_MESSAGE_SIZE = 80;
-	
+
 	private static final byte STRING_NULL = (byte) 0x0;
-	
-	/**
-	 * Standard informational message.
-	 */
-	final int OK_MESSAGE=0;//standard message
-	
-	/**
-	 * Error message.
-	 */
-	final int ERROR=1;//message indicating error condition
-	
-	/**
-	 * Message containing scaler values.
-	 */
-	final int SCALER=2;//received from VME, contains scaler values
-	
-	/**
-	 * Message containing CNAF commands.
-	 */
-	final int CNAF=3;//sent to VME, contains CNAF commands
-	
-	/**
-	 * Message containing front end event and buffer counters.
-	 */
-	final int COUNTER=4;
-	
-	/**
-	 * Message containing CAEN VME electronics configuration info.
-	 */
-	final int VME_ADDRESS=5;//sent to VME, contains VME addressing information
-	
-	/**
-	 * Message containing the interval in seconds at which to 
-	 * insert scalers in the event stream.
-	 */
-	final int INTERVAL=6;//sent to VME, contains interval to insert scalers in event stream
-	
 
 	private static final int MAX_PACKET_SIZE = 1024;
-	
-	//final String LOCAL_HOST="localhost";
-	
-	final String HOST_IP="localhost";
-	final String FRONTEND_IP="localhost"; 
-	final int PORT_RECIEVE=6002;
-	final int PORT_SEND=6003;
-	final int PORT_SEND_HOST=5002;
-	final int PORT_DATA=6005;	
-	final int PORT_DATA_HOST=10205;
-	
-	private InetAddress addressHost=null;
-	private InetAddress addressFrontEnd=null;	
 
-	private transient DatagramSocket socketSend; 
+	final static String HOST_IP = "localhost";
+
+	final static String FRONTEND_IP = "localhost";
+
+	final static int PORT_RECIEVE = 6002;
+
+	final static int PORT_SEND = 6003;
+
+	final static int PORT_SEND_HOST = 5002;
+
+	final static int PORT_DATA = 6005;
+
+	final static int PORT_DATA_HOST = 10205;
+
+	private transient InetAddress addressHost = null;//NOPMD
+
+	private transient InetAddress addressFrontEnd = null;//NOPMD
+
+	private transient DatagramSocket socketSend;
+
 	private transient DatagramSocket socketReceive;
-	
-	private Object isRun= new Object();
-	private boolean runState=false;
-	
 
-	public SimulateFrontEnd()
-	{
-		System.out.println("Front End Simulator Started");		
-		
+	private boolean runState = false;
+
+	/**
+	 * Constructor.
+	 */
+	public SimulateFrontEnd() {
+		super();
+		LOGGER.info("Front End Simulator Started");
 	}
-	
 
-	public void startCommunication()
-	{
+	private void startCommunication() {
 		createNetworkConnections();
-		System.out.println("Created Sockets");
-		
-		receiveCommands();	
+		LOGGER.info("Created Sockets");
+		receiveCommands();
 	}
-	
-	private void createNetworkConnections()
-	{
-		
-		
-		try {// ceate a ports to send and receive
 
+	private void createNetworkConnections() {
+		try {// ceate a ports to send and receive
 			addressHost = InetAddress.getByName(HOST_IP);
-			addressFrontEnd = InetAddress.getByName(FRONTEND_IP);			
-		} catch (UnknownHostException ue) {			
-			System.out.println( "Unknown local host " + FRONTEND_IP);
+			addressFrontEnd = InetAddress.getByName(FRONTEND_IP);
+		} catch (UnknownHostException ue) {
+			LOGGER.info("Unknown local host " + FRONTEND_IP);
 			return;
-		}		
+		}
 		try {
 			socketSend = new DatagramSocket(PORT_SEND, addressHost);
-		} catch (BindException be) {			
-			System.out.println( "Problem binding send socket: "+be.getMessage());			
+		} catch (BindException be) {
+			LOGGER.info("Problem binding send socket: " + be.getMessage());
 		} catch (SocketException se) {
-			System.out.println( "Problem creating send socket "+se.getMessage());			
+			LOGGER.info("Problem creating send socket " + se.getMessage());
 		}
 		try {
 			socketReceive = new DatagramSocket(PORT_RECIEVE, addressHost);
 		} catch (BindException be) {
-			System.out.println( "Problem binding receive socket: "+be.getMessage());			
+			LOGGER.info("Problem binding receive socket: " + be.getMessage());
 		} catch (SocketException se) {
-			System.out.println( "Problem creating receive socket "+se.getMessage());			
-		}		
-		
+			LOGGER.info("Problem creating receive socket " + se.getMessage());
+		}
+
 	}
-	
+
 	/**
 	 * Receive command
 	 */
 	public void receiveCommands() {
-		System.out.println("Setup receive commands");
+		LOGGER.info("Setup receive commands");
 		final byte[] bufferIn = new byte[MAX_PACKET_SIZE];
 		try {
+			final DatagramPacket packetIn = new DatagramPacket(bufferIn,
+					bufferIn.length);
 			while (true) {// loop forever receiving packets
-				final DatagramPacket packetIn = new DatagramPacket(bufferIn,
-						bufferIn.length);
 				socketReceive.receive(packetIn);
-				final ByteBuffer byteBuffer = ByteBuffer.wrap(packetIn.getData());			
-				//final DataInput messageDis = new DataInputStream(messageBais);
-				final int status = byteBuffer.getInt(); 
-				if (status == OK_MESSAGE) {					 
-					final String text=unPackMessage(byteBuffer);		 			
-					System.out.println( "Message: "+text );
+				final ByteBuffer byteBuffer = ByteBuffer.wrap(packetIn
+						.getData());
+				final int status = byteBuffer.getInt();
+				if (status == OK_MESSAGE) {
+					final String text = unPackMessage(byteBuffer);
+					LOGGER.info("Message: " + text);
 					performTask(text);
 				} else if (status == ERROR) {
-					final String text=unPackMessage(byteBuffer);					
-					System.out.println( "Error:"+text);					
+					final String text = unPackMessage(byteBuffer);
+					LOGGER.info("Error:" + text);
 				} else if (status == SCALER) {
-					System.out.println( "Scaler:");	
+					LOGGER.info("Scaler:");
 				} else if (status == CNAF) {
-					System.out.println( "CNAF:");
+					LOGGER.info("CNAF:");
 				} else if (status == COUNTER) {
-					System.out.println( "Counte:r");
-				} else if (status == VME_ADDRESS ){
-					System.out.println( "VME Address:");
+					LOGGER.info("Counte:r");
+				} else if (status == VME_ADDRESS) {
+					LOGGER.info("VME Address:");
 				} else if (status == INTERVAL) {
-					System.out.println( "Interval:");					
+					LOGGER.info("Interval:");
 				} else {
-					System.out.println( "Message Unknown:");
+					LOGGER.info("Message Unknown:");
 				}
 			}// end of receive message forever loop
 		} catch (IOException ioe) {
-			System.out.println( "Error in receiving messages");
+			LOGGER.info("Error in receiving messages");
 		}
 	}
-	
+
 	/**
 	 * Unpack a datagram with a message. Message packets have an ASCII character
 	 * array terminated with \0.
@@ -182,37 +135,36 @@ public final class SimulateFrontEnd extends GoodThread {
 	 * @param buffer
 	 *            packet contents passed in readable form
 	 * @return the string contained in the message
-	 * @throws JamException
-	 *             if there's a problem
 	 */
 	private String unPackMessage(final ByteBuffer buffer) {
 		final StringBuilder rval = new StringBuilder();
 		char next;
 		do {
-			next = (char)buffer.get();
+			next = (char) buffer.get();
 			rval.append(next);
 		} while (next != '\0');
-		final int len = rval.length()-1;
+		final int len = rval.length() - 1;
 		if (len > MAX_MESSAGE_SIZE) {// exclude null
 			final IllegalArgumentException exception = new IllegalArgumentException(
-					"Message length, "+len+", greater than max allowed, "+MAX_MESSAGE_SIZE+".");
+					"Message length, " + len + ", greater than max allowed, "
+							+ MAX_MESSAGE_SIZE + ".");
 			throw exception;
 		}
 		return rval.substring(0, len);
 	}
-	
-	private void performTask(String task) {
+
+	private void performTask(final String task) {
 		if (task.equals("START")) {
 			setRunState(true);
 			replyMessage("Performed Start");
 		} else if (task.equals("STOP")) {
 			setRunState(false);
-		}else if (task.equals("END")) {
+		} else if (task.equals("END")) {
 			setRunState(false);
 		}
 	}
-	private void replyMessage(String message)
-	{
+
+	private void replyMessage(final String message) {
 		final DatagramPacket packetMessage;
 		/* byte arrays initialized with zeros by definition */
 		final byte[] byteMessage = new byte[message.length() + 5];
@@ -223,77 +175,80 @@ public final class SimulateFrontEnd extends GoodThread {
 		byteMessage[byteMessage.length - 1] = STRING_NULL;
 		packetMessage = new DatagramPacket(byteMessage, byteMessage.length,
 				addressHost, PORT_SEND_HOST);
-		try{
-		socketSend.send(packetMessage);
+		try {
+			socketSend.send(packetMessage);
 		} catch (IOException e) {
-			System.out.println("Error sending packet "+e);
+			LOGGER.info("Error sending packet " + e);
 		}
 	}
-	
+
 	/**
 	 * Thread method: is a deamon for sending data packets.
 	 */
 	public void run() {
-		System.out.println("Data Thread Started");
-		DatagramSocket socketData=null; 
-		
+		LOGGER.info("Data Thread Started");
+		DatagramSocket socketData = null;
 		try {
 			socketData = new DatagramSocket(PORT_DATA, addressFrontEnd);
 		} catch (BindException be) {
-			System.out.println( "Problem binding data socket: "+be.getMessage());			
+			LOGGER.info("Problem binding data socket: " + be.getMessage());
 		} catch (SocketException se) {
-			System.out.println( "Problem creating data socket "+se.getMessage());			
-		}		
-		
+			LOGGER.info("Problem creating data socket " + se.getMessage());
+		}
 		final byte[] bufferSend = new byte[MAX_PACKET_SIZE];
 		try {
-
-			while(true) {// loop forever 				
+			final DatagramPacket packetIn = new DatagramPacket(
+					bufferSend, bufferSend.length, addressHost,
+					PORT_DATA_HOST);
+			while (true) {// loop forever
 				try {
-								
 					while (isRunState()) {
-						System.out.println("Data Send");
-						createData(bufferSend)	;
-						final DatagramPacket packetIn = new DatagramPacket(bufferSend, bufferSend.length, addressHost, PORT_DATA_HOST);				
+						LOGGER.info("Data Send");
+						createData(bufferSend);
+						packetIn.setData(bufferSend, 0, bufferSend.length);
 						socketData.send(packetIn);
-						sleep(1000);	//sleep for a second
+						sleep(1000); // sleep for a second
 					}
-				} catch (InterruptedException ie) { 
-					System.out.println ("Thread Interruped");
+				} catch (InterruptedException ie) {
+					LOGGER.info("Thread Interruped");
 				}
-				
+
 			}// end of send data loop
 		} catch (IOException ioe) {
-			System.out.println("Unable to read datagram data Exception:"+ ioe);
-		} 
-	}
-	
-	private void createData(byte [] buffer)	{
-		buffer[0]=(byte)1;
-		buffer[1]=(byte)2;
-		buffer[2]=(byte)3;
-		buffer[3]=(byte)4;		
-	}
-	
-	private synchronized void setRunState(boolean state)
-	{
-		runState=state;
-		
-		if(runState) {
-			notifyAll(); 	
+			LOGGER.info("Unable to read datagram data Exception:" + ioe);
 		}
 	}
-	private synchronized boolean isRunState() {
-		try {		
-			if (!runState) {
-				wait();//wait till start again
-			}
 
-		} catch (InterruptedException ie) {
-			System.out.println ("Thread Interruped");
-		}
-		return runState;		
+	private void createData(byte[] buffer) {
+		buffer[0] = (byte) 1;
+		buffer[1] = (byte) 2;
+		buffer[2] = (byte) 3;
+		buffer[3] = (byte) 4;
 	}
+
+	private void setRunState(final boolean state) {
+		synchronized (this) {
+			runState = state;
+			if (runState) {
+				notifyAll();
+			}
+		}
+	}
+
+	private boolean isRunState() {
+		synchronized (this) {
+			try {
+				if (!runState) {
+					wait();// wait till start again
+				}
+
+			} catch (InterruptedException ie) {
+				LOGGER.info("Thread Interruped");
+			}
+			return runState;
+		}
+	}
+
 	/**
 	 * Main method to run simulator
 	 * 
@@ -301,13 +256,9 @@ public final class SimulateFrontEnd extends GoodThread {
 	 *            not used currently
 	 */
 	public static void main(final String args[]) {
-		SimulateFrontEnd sfe= new SimulateFrontEnd();
-
-		sfe.start();		
+		final SimulateFrontEnd sfe = new SimulateFrontEnd();
+		sfe.start();
 		sfe.startCommunication();
-		
-		
-
 	}
-	
+
 }
