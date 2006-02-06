76eb4580520a6d6249a950ca28fbef30e992bc36
Dale Visser <dwvisser@users.sourceforge.net>
2006-02-06 20:26:22 +0000

    Scale converted to typesafe enum

--- jam/plot/Scale.java
+++ jam/plot/Scale.java
@@ -6,24 +6,14 @@ package jam.plot;
  * @author <a href="mailto:dale@visser.name">Dale W Visser </a>
  * @version 2004-11-08
  */
-public class Scale {
+public enum Scale {
 	/**
 	 * Value representing linear counts scale.
 	 */
-	static public final Scale LINEAR = new Scale(0);
+	LINEAR,
 
 	/**
 	 * Value representing log counts scale.
 	 */
-	static public final Scale LOG = new Scale(1);
-
-	private int type;
-
-	private Scale(int t) {
-		type = t;
-	}
-	
-	public boolean equals(Object o){
-	    return o instanceof Scale ? ((Scale)o).type == type : false;
-	}
+	LOG
 }
