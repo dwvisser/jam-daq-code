843e60791d9816daef796f13a15463e18b072b53
Dale Visser <dwvisser@users.sourceforge.net>
2006-02-06 11:52:08 +0000

    converted to Java 5.0 typesafe enum

--- jam/plot/GateSetMode.java
+++ jam/plot/GateSetMode.java
@@ -6,41 +6,36 @@ package jam.plot;
  * @author <a href="mailto:dale@visser.name">Dale Visser</a>
  * @version Jan 26, 2004
  */
-class GateSetMode {
-	final int value;
-	private final static int [] VALUE_SET={0,1,2,3,4,5};
+enum GateSetMode {
 	
 	/**
 	 * settting a new gate
 	 */
-	final static GateSetMode GATE_NEW = new GateSetMode(0);
+	GATE_NEW,
 
 	/**
 	 * cancel the setting of a gate
 	 */
-	final static GateSetMode GATE_CANCEL = new GateSetMode(1);
+	GATE_CANCEL,
 
 	/**
 	 * continue setting gate
 	 */
-	final static GateSetMode GATE_CONTINUE = new GateSetMode(2);
+	GATE_CONTINUE,
 	
 	/**
 	 * add a point to setting gate
 	 */
-	final static GateSetMode GATE_ADD = new GateSetMode(3);
+	GATE_ADD,
 	
 	/**
 	 * add a point to setting gate
 	 */
-	final static GateSetMode GATE_REMOVE = new GateSetMode(4);
+	GATE_REMOVE,
 
 	/**
 	 * save the gate that is being set
 	 */
-	final static GateSetMode GATE_SAVE = new GateSetMode(5);
-	
-	private GateSetMode(int i){
-		value=GateSetMode.VALUE_SET[i];
-	}
+	GATE_SAVE
 }
+	
