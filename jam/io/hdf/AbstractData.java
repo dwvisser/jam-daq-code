8d0a7cff63f0b62c6ed448a0de97ae8e0a811dba
Dale Visser <dwvisser@users.sourceforge.net>
2006-02-06 22:13:54 +0000

    PMD edits

--- jam/io/hdf/AbstractData.java
+++ jam/io/hdf/AbstractData.java
@@ -95,12 +95,12 @@ public abstract class AbstractData implements Constants {
 	/**
 	 * Before bytes is created, length of bytes.
 	 */
-	protected int length;
+	protected transient int length;
 
 	/**
 	 * Actual bytes stored in HDF file.
 	 */
-	protected ByteBuffer bytes;
+	protected transient ByteBuffer bytes;
 
 	private static final byte[] CLEARBYTES = new byte[0];
 
@@ -135,17 +135,15 @@ public abstract class AbstractData implements Constants {
 	 * 
 	 * @param data
 	 *            data object
-	 * @see #setOffsets()
 	 */
-	static void addDataObjectToList(AbstractData data) {
+	static void addDataObjectToList(final AbstractData data) {
 		final Integer key = calculateKey(data.getTag(), data.getRef());
-		if (!tagRefMap.containsKey(key)) {
-			tagRefMap.put(key, data);
-			objectList.add(data);
-		} else {
+		if (tagRefMap.containsKey(key)) {
 			throw new IllegalArgumentException(
-					"Can't add o list of DataObjects as DataOjbect with the same tag and ref exists.");
+					"Can't add to list of DataObjects as DataOjbect with the same tag and ref exists.");
 		}
+		tagRefMap.put(key, data);
+		objectList.add(data);
 	}
 
 	/**
@@ -155,7 +153,7 @@ public abstract class AbstractData implements Constants {
 	 * @param ref
 	 *            <em>unique</em> reference number in file
 	 */
-	public static AbstractData getObject(short tag, short ref) {
+	public static AbstractData getObject(final short tag, final short ref) {
 		AbstractData match = null;
 		final Integer key = calculateKey(tag, ref);
 		if (tagRefMap.containsKey(key)) {
@@ -172,8 +170,8 @@ public abstract class AbstractData implements Constants {
 	 * @param tagType
 	 *            the type to return
 	 */
-	static List<AbstractData> ofType(Collection<AbstractData> collection,
-			short tagType) {
+	static List<AbstractData> ofType(final Collection<AbstractData> collection,
+			final short tagType) {
 		final Set<AbstractData> ssin = new HashSet<AbstractData>();
 		for (AbstractData dataObject : collection) {
 			if (tagType == dataObject.getTag()) {
@@ -199,12 +197,12 @@ public abstract class AbstractData implements Constants {
 		return rval;
 	}
 
-	static boolean isValidType(short type) {
-		return ALL_TYPES.contains(new Short(type));
+	static boolean isValidType(final short type) {
+		return ALL_TYPES.contains(type);
 	}
 
-	static final AbstractData create(byte[] bytes, short tag, short ref)
-			throws HDFException {
+	static final AbstractData create(final byte[] bytes, final short tag,
+			final short ref) throws HDFException {
 		AbstractData dataObject = null;
 		if (isValidType(tag)) {
 			dataObject = createDataObject(tag);
@@ -217,8 +215,8 @@ public abstract class AbstractData implements Constants {
 		return dataObject;
 	}
 
-	static AbstractData create(short tag, short ref, int offset, int length)
-			throws HDFException {
+	static AbstractData create(final short tag, final short ref,
+			final int offset, final int length) throws HDFException {
 		AbstractData dataObject = null;
 		if (isValidType(tag)) {
 			dataObject = createDataObject(tag);
@@ -231,7 +229,8 @@ public abstract class AbstractData implements Constants {
 		return dataObject;
 	}
 
-	private static AbstractData createDataObject(short tag) throws HDFException {
+	private static AbstractData createDataObject(final short tag)
+			throws HDFException {
 		AbstractData rval = null;
 		final Short shortTag = new Short(tag);
 		if (INITABLE.containsKey(shortTag)) {
@@ -265,7 +264,7 @@ public abstract class AbstractData implements Constants {
 	 * @return a reference number for the given HDF object @param refs the map
 	 * for a given tag type
 	 */
-	static short createUniqueRef(short tag) {
+	static short createUniqueRef(final short tag) {
 		// Add 1 as its a good guess
 		refCount++;
 		// see if a unique object already exists
@@ -280,7 +279,7 @@ public abstract class AbstractData implements Constants {
 	/*
 	 * non-javadoc: Create a unique key given then tag an ref numbers.
 	 */
-	static Integer calculateKey(short tag, short ref) {
+	static Integer calculateKey(final short tag, final short ref) {
 		final int tagInt = tag;
 		final int refInt = ref;
 		final int key = (tagInt << 16) + refInt;
@@ -289,7 +288,7 @@ public abstract class AbstractData implements Constants {
 		return new Integer(key);
 	}
 
-	private final StringUtilities UTIL = StringUtilities.getInstance();
+	private static final StringUtilities UTIL = StringUtilities.getInstance();
 
 	/**
 	 * Creates a new HDF DataObject, belonging to the specified
@@ -298,12 +297,11 @@ public abstract class AbstractData implements Constants {
 	 * handles the bookkeeping of the HDF File Header (see NCSA HDF:
 	 * Specifications and Developer's Guide v3.2).
 	 * 
-	 * @param file
-	 *            The file to contain the new object.
 	 * @param tag
 	 *            The hdf tag of the new object.
 	 */
-	AbstractData(short tag) {
+	AbstractData(final short tag) {
+		super();
 		this.tag = tag;
 		this.ref = createUniqueRef(tag);
 		addDataObjectToList(this); // ref gets set in this call
@@ -319,7 +317,7 @@ public abstract class AbstractData implements Constants {
 	 * of data object. @throws IllegalArgumentException if the data is null or
 	 * empty
 	 */
-	void init(byte[] data, short tagType, short reference) {
+	void init(final byte[] data, final short tagType, final short reference) {
 		if (data == null || data.length == 0) {
 			throw new IllegalArgumentException(
 					"Can't init DataObject with empty data.");
@@ -338,14 +336,15 @@ public abstract class AbstractData implements Constants {
 	 * location in <code>file</code> @param reference The unique value
 	 * specifying the type of data object.
 	 */
-	void init(int byteOffset, int len, short tagType, short reference) {
+	void init(final int byteOffset, final int len, final short tagType,
+			final short reference) {
 		tag = tagType;
 		setRef(reference);
 		offset = byteOffset;
 		length = len;
 	}
 
-	private final void setTag(short newTag) {
+	private final void setTag(final short newTag) {
 		tag = newTag;
 	}
 
@@ -363,7 +362,7 @@ public abstract class AbstractData implements Constants {
 	 * 
 	 * @param newref
 	 */
-	final void setRef(short newref) {
+	final void setRef(final short newref) {
 
 		// Remove object with old ref
 		final Integer key = calculateKey(tag, ref);
@@ -373,10 +372,10 @@ public abstract class AbstractData implements Constants {
 			// Add
 			final Integer keyNew = calculateKey(tag, newref);
 			// Check for collision
-			if (tagRefMap.containsKey(key))
+			if (tagRefMap.containsKey(key)) {
 				throw new IllegalArgumentException(
 						"Can't set reference on DataObject as one the reference already exists.");
-
+			}
 			ref = newref;
 			tagRefMap.put(keyNew, this);
 
@@ -391,7 +390,7 @@ public abstract class AbstractData implements Constants {
 	 * size or writing out. Override when an update of is needed.
 	 */
 	void refreshBytes() {
-		//default is do nothing
+		// default is do nothing
 	}
 
 	/**
@@ -409,7 +408,7 @@ public abstract class AbstractData implements Constants {
 	 * non-javadoc: Called back by <code>HDFile</code> to set the offset
 	 * information.
 	 */
-	void setOffset(int off) {
+	void setOffset(final int off) {
 		offset = off;
 	}
 
@@ -436,7 +435,7 @@ public abstract class AbstractData implements Constants {
 	 * @param string
 	 *            to be converted
 	 */
-	protected final void putString(String string) {
+	protected final void putString(final String string) {
 		bytes.put(UTIL.getASCIIarray(string));
 	}
 
@@ -448,7 +447,7 @@ public abstract class AbstractData implements Constants {
 	 *            length of string
 	 * @return the string
 	 */
-	protected final String getString(int len) {
+	protected final String getString(final int len) {
 		final byte[] rval = new byte[len];
 		bytes.get(rval);
 		return UTIL.getASCIIstring(rval);
