981cdb3bba1507fe42ac5541e803e50b0c79eb74
Dale Visser <dwvisser@users.sourceforge.net>
2006-02-12 16:59:16 +0000

    modified execute() so PMD wouldn't complain about the passed array

--- jam/commands/AddHDF.java
+++ jam/commands/AddHDF.java
@@ -14,37 +14,39 @@ import java.util.Observable;
  * @author Ken Swartz
  */
 final class AddHDF extends AbstractLoaderHDF {
-	
-	AddHDF(){
+
+	AddHDF() {
 		super();
 	}
-	 
-	public void initCommand(){
-		putValue(NAME,"Add Group Counts\u2026");
-		fileOpenMode=FileOpenMode.ADD;
+
+	public void initCommand() {
+		putValue(NAME, "Add Group Counts\u2026");
+		fileOpenMode = FileOpenMode.ADD;
 	}
-	
+
 	protected void execute(final Object[] cmdParams) {
-        File file = null;
-        loadGroup = (Group)STATUS.getCurrentGroup();
-        //Parse commad parameters if given
-        if (cmdParams != null) {
-            if (cmdParams.length > 0) {
-                file = (File) cmdParams[0];
-            }
-            if (cmdParams.length > 1) {
-                loadGroup = (Group) cmdParams[1];
-            }
-        }
-        loadHDFFile(file, loadGroup);
-    }
-	
+		File file = null;
+		loadGroup = (Group) STATUS.getCurrentGroup();
+		// Parse commad parameters if given
+		if (cmdParams != null) {
+			if (cmdParams.length > 0) {
+				final Object param0 = cmdParams[0];
+				file = (File) param0;
+			}
+			if (cmdParams.length > 1) {
+				final Object param1 = cmdParams[1];
+				loadGroup = (Group) param1;
+			}
+		}
+		loadHDFFile(file, loadGroup);
+	}
+
 	public void update(final Observable observe, final Object obj) {
-        final BroadcastEvent event = (BroadcastEvent) obj;
-        final BroadcastEvent.Command command = event.getCommand();
-        if (command == BroadcastEvent.Command.SORT_MODE_CHANGED) {
-            final SortMode mode = STATUS.getSortMode();
-            setEnabled(mode != SortMode.REMOTE);
-        }
-    }
+		final BroadcastEvent event = (BroadcastEvent) obj;
+		final BroadcastEvent.Command command = event.getCommand();
+		if (command == BroadcastEvent.Command.SORT_MODE_CHANGED) {
+			final SortMode mode = STATUS.getSortMode();
+			setEnabled(mode != SortMode.REMOTE);
+		}
+	}
 }
