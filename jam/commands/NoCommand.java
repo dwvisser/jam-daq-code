6d9c2c089a775fe38d68e517bc6f2627bc0465da
Dale Visser <dwvisser@users.sourceforge.net>
2006-02-12 17:55:18 +0000

    null command does nothing...used when no command exists for a request

---
+++ jam/commands/NoCommand.java
@@ -0,0 +1,20 @@
+package jam.commands;
+
+import jam.global.CommandListenerException;
+
+public final class NoCommand extends AbstractCommand {
+
+	@Override
+	protected void execute(Object[] cmdParams) throws CommandException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void executeParse(String[] cmdTokens)
+			throws CommandListenerException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
