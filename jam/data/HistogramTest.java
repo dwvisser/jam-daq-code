e8bddbc288262e41498dfcfccd501aa0a5358524
Dale Visser <dwvisser@users.sourceforge.net>
2006-02-14 15:24:05 +0000

    more tests

--- jam/data/HistogramTest.java
+++ jam/data/HistogramTest.java
@@ -9,13 +9,15 @@ import junit.framework.TestCase;
  * 
  * @author <a href="mailto:dale@visser.name">Dale Visser</a>
  * @see Histogram
- * @see AllTests
  */
 public class HistogramTest extends TestCase {
 
-    Gate gate1, gate2;
+    private transient Gate gate1, gate2;
 
-    Histogram hist1, hist2;
+    private transient HistInt1D hist1;
+    private transient HistDouble1D hist1f;
+    private transient HistInt2D hist2;
+    private transient HistDouble2D hist2f;
 
     /**
      * Constructor for HistogramTest.
@@ -35,10 +37,41 @@ public class HistogramTest extends TestCase {
         super.setUp();
         final Group group = Group.createGroup("TestHistogramGroup",
                 Group.Type.FILE);
-        hist1 = Histogram.createHistogram(group, new int[100], "h1");
-        hist2 = Histogram.createHistogram(group, new int[100][100], "h2");
+        hist1 = (HistInt1D)Histogram.createHistogram(group, new int[100], "h1");
+        hist1f = (HistDouble1D)Histogram.createHistogram(group, new double[100], "h1f");
+        hist2 = (HistInt2D)Histogram.createHistogram(group, new int[100][100], "h2");
+        hist2f = (HistDouble2D)Histogram.createHistogram(group, new double[100][100], "h2f");
+        for (int i=0; i <hist1.getSizeX(); i++){
+        	hist1.setCounts(i,i);
+        	hist1f.setCounts(i,i);
+        }
+        for (int i=0; i< hist2.getSizeX(); i++){
+        	for (int j=0; j<hist2.getSizeY(); j++){
+        		hist2.setCounts(i,j,i+j);
+        		hist2f.setCounts(i,j,i+j);
+        	}
+        }
         gate1 = new Gate("g1", hist1);
         gate2 = new Gate("g2", hist2);
+    }
+    
+    /**
+     * test that add counts gives good results
+     *
+     */
+    public void testAddCounts(){
+    	final double area1before = hist1.getArea();
+    	final double area2before = hist2.getArea();
+    	hist1.addCounts(hist1.getCounts());
+    	hist2.addCounts(hist2.getCounts());
+    	assertEquals("hist1 Should be double before.", hist1.getArea(),2*area1before);
+    	assertEquals("hist2 Should be double before.", hist2.getArea(),2*area2before);
+    	final double area1fbefore = hist1f.getArea();
+    	final double area2fbefore = hist2f.getArea();
+    	hist1f.addCounts(hist1f.getCounts());
+    	hist2f.addCounts(hist2f.getCounts());
+    	assertEquals("hist1f Should be double before.", hist1f.getArea(),2*area1fbefore);
+    	assertEquals("hist2f Should be double before.", hist2f.getArea(),2*area2fbefore);
     }
 
     /**
