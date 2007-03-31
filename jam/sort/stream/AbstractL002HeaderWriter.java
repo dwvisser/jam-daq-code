fd2f8eb39ee3933587f76b2cd5c64d204bbf8214
Dale Visser <dwvisser@users.sourceforge.net>
2007-03-31 19:18:59 +0000

    Added comment to avoid PMD complaint about this class being abstract without defining abstract method. The PMD rule failed to capture that this class must be abstrat because it inherits abstract methods.

--- jam/sort/stream/AbstractL002HeaderWriter.java
+++ jam/sort/stream/AbstractL002HeaderWriter.java
@@ -13,69 +13,77 @@ import java.util.TimeZone;
 
 /**
  * Abstract superclass of all event output streams that write ORNL L002 headers.
- * @author dvk
- *
+ * 
+ * @author Dale Visser
  */
-abstract class AbstractL002HeaderWriter extends AbstractEventOutputStream {
-    
+abstract class AbstractL002HeaderWriter extends AbstractEventOutputStream {// NOPMD
+
+	private static final SimpleDateFormat formatter = new SimpleDateFormat(
+			"MM/dd/yy HH:mm  ", Locale.getDefault());
+
+	static {
+		formatter.setTimeZone(TimeZone.getDefault());
+	}
+
 	/**
 	 * Default constructor.
-	 *
+	 * 
 	 */
-	protected AbstractL002HeaderWriter(){
+	protected AbstractL002HeaderWriter() {
 		super();
 	}
 
 	/**
-	 * @param eventSize number of parmas per event
+	 * @param eventSize
+	 *            number of parmas per event
 	 */
-	protected AbstractL002HeaderWriter(int eventSize){
+	protected AbstractL002HeaderWriter(int eventSize) {
 		super(eventSize);
 	}
-	
-    private static final SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yy HH:mm  ", Locale.getDefault());
-    static {
-        formatter.setTimeZone(TimeZone.getDefault());        
-    }
 
-    /**
-     * Writes the header block.
-     *
-     * @exception   EventException    thrown for errors in the event stream
-     */
-    public void writeHeader() throws EventException {
-		final StringUtilities stringUtilities=StringUtilities.getInstance();
+	/**
+	 * Writes the header block.
+	 * 
+	 * @exception EventException
+	 *                thrown for errors in the event stream
+	 */
+	@Override
+	public void writeHeader() throws EventException {
+		final StringUtilities stringUtilities = StringUtilities.getInstance();
 		String dateString;
 		final RunInfo runInfo = RunInfo.getInstance();
 		synchronized (formatter) {
-			dateString = formatter.format(runInfo.runStartTime);	    //date
+			dateString = formatter.format(runInfo.runStartTime); // date
 		}
-        final String title=runInfo.runTitle;					    //title
-        final int number=runInfo.runNumber;					    //header number
-        final byte [] reserved1=new byte [8];					    //reserved 1
-        final int numSecHead=0;						    //number of secondary headers
-        final int recLen=0;							    //record length
-        final int blckImgRec=0;						    //block line image rec
-        final int paramsPerEvent=runInfo.runEventSize;				    //parameters per event
-        final int dataRecLen=runInfo.runRecordLength;				    //data record length
-        final byte [] reserved2=new byte [92];				    //reserved 2
-        try {
-            dataOutput.writeBytes(HEADER_START);				    //header
-            dataOutput.writeBytes(stringUtilities.makeLength(dateString,16));	    //date				    //date
-            dataOutput.writeBytes(stringUtilities.makeLength(title, TITLE_MAX));    //title
-            dataOutput.writeInt(number);					    //header number
-            dataOutput.write(reserved1, 0, reserved1.length);			    // reserved space
-            dataOutput.writeInt(numSecHead);					    //number secondary headers
-            dataOutput.writeInt(recLen);					    //record length
-            dataOutput.writeInt(blckImgRec);					    //block line image rec
-            dataOutput.writeInt(IMAGE_LENGTH);					    //record length
-            dataOutput.writeInt(paramsPerEvent);					    //parameters / event
-            dataOutput.writeInt(dataRecLen);					    //data record length
-            dataOutput.write(reserved2, 0, reserved2.length);			    //reserved 2
-            dataOutput.flush();
-        } catch (IOException io){
-            throw new EventException("Problem writing header.", io);
-        }
-    }
+		final String title = runInfo.runTitle; // title
+		final int number = runInfo.runNumber; // header number
+		final byte[] reserved1 = new byte[8]; // reserved 1
+		final int numSecHead = 0; // number of secondary headers
+		final int recLen = 0; // record length
+		final int blckImgRec = 0; // block line image rec
+		final int paramsPerEvent = runInfo.runEventSize; // parameters per
+															// event
+		final int dataRecLen = runInfo.runRecordLength; // data record length
+		final byte[] reserved2 = new byte[92]; // reserved 2
+		try {
+			dataOutput.writeBytes(HEADER_START); // header
+			dataOutput.writeBytes(stringUtilities.makeLength(dateString, 16)); // date
+																				// //date
+			dataOutput.writeBytes(stringUtilities.makeLength(title, TITLE_MAX)); // title
+			dataOutput.writeInt(number); // header number
+			dataOutput.write(reserved1, 0, reserved1.length); // reserved
+																// space
+			dataOutput.writeInt(numSecHead); // number secondary headers
+			dataOutput.writeInt(recLen); // record length
+			dataOutput.writeInt(blckImgRec); // block line image rec
+			dataOutput.writeInt(IMAGE_LENGTH); // record length
+			dataOutput.writeInt(paramsPerEvent); // parameters / event
+			dataOutput.writeInt(dataRecLen); // data record length
+			dataOutput.write(reserved2, 0, reserved2.length); // reserved 2
+			dataOutput.flush();
+		} catch (IOException io) {
+			throw new EventException("Problem writing header.", io);
+		}
+	}
 
 }
