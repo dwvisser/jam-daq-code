0f509b1a7f62b8b5022615f6bd63f9ebc9e5c5b1
Dale Visser <dwvisser@users.sourceforge.net>
2007-10-01 23:09:02 +0000

    Refactored for better clarity.

--- test/io/ImpExpASCIITest.java
+++ test/io/ImpExpASCIITest.java
@@ -21,34 +21,65 @@ public final class ImpExpASCIITest {// NOPMD
 
 	private static final String ASCIITEST = "ASCIItest";
 
-	private transient File temp1, temp2;
+	private static final String DATA = "0 8\n1 2\n2 4";
+
+	private static final int HIST_DIMENSION = 1;
+
+	private static final int HIST_SIZE = 3;
+
+	private static final long HIST_SUM = 14L;
+
+	private static final String histName = "oneDtest";
 
 	private transient ImpExpASCII impExp;
 
-	private static final String histName = "oneDtest";
+	private transient File temp1, temp2;
+
+	/**
+	 * @param writer
+	 * @throws IOException
+	 */
+	private void appendDataAndClose(final FileWriter writer) throws IOException {
+		writer.write(DATA);
+		writer.close();
+	}
+
+	/**
+	 * @param hist
+	 */
+	private void assertCorrectHistogramProperties(final Histogram hist) {
+		assertNotNull("Expected to read a histogram.", hist);
+		assertEquals("Expecting a certain number of channels.", HIST_SIZE, hist.getSizeX());
+		assertEquals("Expected one dimension.", HIST_DIMENSION, hist.getDimensionality());
+		assertEquals("Expected a certain sum.", HIST_SUM, Math.round(hist.getArea()));
+	}
+
+	private void readHistDataAndCheck(final File file) throws ImpExpException{
+		impExp.openFile(file);
+		final String groupName = FileUtilities.getInstance()
+				.removeExtensionFileName(file.getName());
+		final Group importGroup = Group.getGroup(groupName);
+		final Histogram hist = importGroup.getHistogramList().get(0);
+		this.assertCorrectHistogramProperties(hist);
+	}
 
 	@Before
 	public void setUp() {
 		Histogram.clearList();
 		try {
 			temp1 = File.createTempFile(ASCIITEST, ".txt");
-			temp2 = File.createTempFile(ASCIITEST, ".txt");
 			final FileWriter writer = new FileWriter(temp1);
-			final FileWriter writer2 = new FileWriter(temp2);
 			writer.write(histName);
 			writer.append('\n');
-			final StringBuilder data = new StringBuilder();
-			data.append("0 8\n").append("1 2\n").append("2 4");
-			writer.write(data.toString());
-			writer2.write(data.toString());
-			writer.close();
-			writer2.close();
+			appendDataAndClose(writer);
+			temp2 = File.createTempFile(ASCIITEST, ".txt");
+			appendDataAndClose(new FileWriter(temp2));
 		} catch (IOException ioe) {
 			fail(ioe.getMessage());
 		}
 		impExp = new ImpExpASCII();
 	}
-
+	
 	@After
 	public void tearDown() {
 		Histogram.clearList();
@@ -57,24 +88,8 @@ public final class ImpExpASCIITest {// NOPMD
 	@Test
 	public void testReadData() {
 		try {
-			impExp.openFile(temp1);
-			String groupName = FileUtilities.getInstance()
-					.removeExtensionFileName(temp1.getName());
-			Group importGroup = Group.getGroup(groupName);
-			Histogram hist = importGroup.getHistogramList().get(0);
-			assertNotNull("Hist got read in.", hist);
-			assertEquals("Testing # channels correct.", 3, hist.getSizeX());
-			assertEquals("1 dimension test.", 1, hist.getDimensionality());
-			assertEquals("Sum test.", 14L, Math.round(hist.getArea()));
-			impExp.openFile(temp2);
-			groupName = FileUtilities.getInstance().removeExtensionFileName(
-					temp2.getName());
-			importGroup = Group.getGroup(groupName);
-			hist = importGroup.getHistogramList().get(0);
-			assertNotNull("Hist got read in.", hist);
-			assertEquals("Testing # channels correct.", 3, hist.getSizeX());
-			assertEquals("1 dimension test.", 1, hist.getDimensionality());
-			assertEquals("Sum test.", 14L, Math.round(hist.getArea()));
+			readHistDataAndCheck(temp1);
+			readHistDataAndCheck(temp2);
 		} catch (ImpExpException iee) {
 			fail(iee.getMessage());
 		}
