bb37ce3ed4bfa3c68c58e1f074c39e07ffde5b23
Dale Visser <dvisser@ida.org>
2012-09-16 08:11:16 -0500

    Fixed classpath in linux launcher.

--- launch/jam.sh
+++ launch/jam.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -classpath @jam.jar@:jh.jar:guice-1.0.jar:aopalliance.jar:javax.inject.jar jam.Main
+java -classpath @jam.jar@:jh.jar:guice-3.0.jar:aopalliance.jar:javax.inject.jar jam.Main
