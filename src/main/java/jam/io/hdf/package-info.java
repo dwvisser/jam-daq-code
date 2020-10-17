d68ee8cf9b32565795730fdc6021be360f549c98
Dale Visser <dale.visser@live.com>
2020-10-17 12:15:31 -0400

    Changed 5 package.html files to package-info.java

---
+++ src/main/java/jam/io/hdf/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Pure Java implementation to store histograms, gates, and scalers in the 
+ * <a href="http://hdf.ncsa.uiuc.edu/hdf4.html">HDF 4&#046;1</a> format.
+ */
+
+package jam.io.hdf;
